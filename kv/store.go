8e4d69b3f406f4d8bcc7f0a757aff8042201c70b
bin.qian <cola.tin.com@gmail.com>
2017-11-30 15:25:44 +0800

    replace ErrNotFound by IsNotFound

--- kv/store.go
+++ kv/store.go
@@ -1,26 +1,24 @@
 package kv
 
-import "github.com/syndtr/goleveldb/leveldb"
-
-// errors
-var (
-	ErrNotFound = leveldb.ErrNotFound
+import (
+	"github.com/syndtr/goleveldb/leveldb"
 )
 
-// Reader wraps the read methods of kv store
+// Reader wraps the read methods of kv store.
 type Reader interface {
-	// Get if no value found for given key, ErrNotFound returned
+	// Get value for given key.
+	// An error returned if key not found. It can be checked via IsNotFound.
 	Get(key []byte) (value []byte, err error)
 	Has(key []byte) (bool, error)
 }
 
-// Writer wraps the write methods of kv store
+// Writer wraps the write methods of kv store.
 type Writer interface {
 	Put(key, value []byte) error
 	Delete(key []byte) error
 }
 
-// Store defines interface of kv store
+// Store defines interface of kv store.
 type Store interface {
 	Reader
 	Writer
@@ -31,14 +29,14 @@ type Store interface {
 	NewIterator(r *Range) Iterator
 }
 
-// Batch defines batch of write ops
+// Batch defines batch of write ops.
 type Batch interface {
 	Writer
 	Len() int
 	Write() error
 }
 
-// Iterator to iterates kvs in store
+// Iterator to iterates kvs in store.
 type Iterator interface {
 	First() bool
 	Last() bool
@@ -54,18 +52,23 @@ type Iterator interface {
 	Value() []byte
 }
 
-// Options to be specified when create store instance
+// Options to be specified when create store instance.
 type Options struct {
 	CacheSize              int
 	OpenFilesCacheCapacity int
 }
 
-// New create persistent store at fs path specified
+// New create persistent store at fs path specified.
 func New(path string, opts Options) (Store, error) {
 	return newPersistentLevelDB(path, opts.CacheSize, opts.OpenFilesCacheCapacity)
 }
 
-// NewMem create in-memory store, for testing purpose
+// NewMem create in-memory store, for testing purpose.
 func NewMem(opts Options) (Store, error) {
 	return newMemLevelDB(opts.CacheSize)
+}
+
+// IsNotFound returns whether an error indicates key not found.
+func IsNotFound(err error) bool {
+	return err == leveldb.ErrNotFound
 }
