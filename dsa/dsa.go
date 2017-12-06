2a5abfbb5e179fbffac19035472fbf91dacafff2
bin.qian <cola.tin.com@gmail.com>
2017-12-07 00:14:55 +0800

    add dsa package

---
+++ dsa/dsa.go
@@ -0,0 +1,29 @@
+package dsa
+
+import (
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/vechain/vecore/acc"
+	"github.com/vechain/vecore/cry"
+)
+
+// TODO: consider adding genesis hash
+
+// Signer extracts signer.
+func Signer(msgHash cry.Hash, sig []byte) (*acc.Address, error) {
+	pub, err := crypto.SigToPub(msgHash[:], sig)
+	if err != nil {
+		return nil, err
+	}
+	addr := crypto.PubkeyToAddress(*pub)
+	return (*acc.Address)(&addr), nil
+}
+
+// Sign sign a signable message.
+func Sign(msgHash cry.Hash, privateKey []byte) ([]byte, error) {
+	priv, err := crypto.ToECDSA(privateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return crypto.Sign(msgHash[:], priv)
+}
