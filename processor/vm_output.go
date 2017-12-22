727dd2cb42e761b56cb4409b1096cc3e1438170a
Qian Bin <cola.tin.com@gmail.com>
2017-12-22 16:20:56 +0800

    add processor.VMOutput

---
+++ processor/vm_output.go
@@ -0,0 +1,60 @@
+package processor
+
+import (
+	"github.com/vechain/thor/acc"
+	"github.com/vechain/thor/cry"
+	"github.com/vechain/thor/vm"
+)
+
+// StateWriter provides state writing methods.
+type StateWriter interface {
+	UpdateAccount(acc.Address, *acc.Account) error
+	Delete(key []byte) error
+}
+
+// StorageWriter provides storage writing methods.
+type StorageWriter interface {
+	UpdateStorage(root cry.Hash, key cry.Hash, value cry.Hash) error
+	Hash(root cry.Hash) cry.Hash
+}
+
+// KVPutter provides kv writing methods.
+type KVPutter interface {
+	Put(key, value []byte) error
+}
+
+// VMOutput alias of vm.Output
+type VMOutput vm.Output
+
+// ApplyState apply account state changes.
+func (o *VMOutput) ApplyState(state StateWriter, storage StorageWriter, kv KVPutter) error {
+	for _, da := range o.DirtiedAccounts {
+		if da.Suicided || da.Data.IsEmpty() {
+			if err := state.Delete(da.Address[:]); err != nil {
+				return err
+			}
+		} else {
+			accCopy := *da.Data
+			// update storage
+			for k, v := range da.Storage {
+				if err := storage.UpdateStorage(accCopy.StorageRoot, k, v); err != nil {
+					return err
+				}
+			}
+
+			if len(da.Code) > 0 {
+				// writing code
+				if err := kv.Put(accCopy.CodeHash[:], da.Code); err != nil {
+					return err
+				}
+			}
+
+			// update account itself
+			accCopy.StorageRoot = storage.Hash(accCopy.StorageRoot)
+			if err := state.UpdateAccount(da.Address, &accCopy); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
