b88c72a71f1c02a60513eefe52be0e6286ec7aa4
Qian Bin <cola.tin.com@gmail.com>
2018-01-09 00:10:49 +0800

    copy Address and Hash type into thor package

---
+++ thor/hash.go
@@ -0,0 +1,54 @@
+package thor
+
+import (
+	"encoding/hex"
+	"errors"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+const (
+	// HashLength length of hash in bytes
+	HashLength = common.HashLength
+)
+
+// Hash main hash type
+type Hash common.Hash
+
+// String implements stringer
+func (h Hash) String() string {
+	return "0x" + hex.EncodeToString(h[:])
+}
+
+// Bytes returns byte slice form of hash.
+func (h Hash) Bytes() []byte {
+	return h[:]
+}
+
+// ParseHash convert string presented hash into Hash type
+func ParseHash(s string) (*Hash, error) {
+	if len(s) == HashLength*2 {
+	} else if len(s) == HashLength*2+2 {
+		if strings.ToLower(s[:2]) != "0x" {
+			return nil, errors.New("invalid prefix")
+		}
+		s = s[2:]
+	} else {
+		return nil, errors.New("invalid length")
+	}
+
+	var h Hash
+	_, err := hex.Decode(h[:], []byte(s))
+	if err != nil {
+		return nil, err
+	}
+	return &h, nil
+}
+
+// BytesToHash converts bytes slice into hash.
+// If b is larger than hash legnth, b will be cropped (from the left).
+// If b is smaller than hash length, b will be extended (from the left).
+func BytesToHash(b []byte) Hash {
+	return Hash(common.BytesToHash(b))
+}
