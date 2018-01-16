0c3703d5eaec87d866dcf5793d71ac65a288e02d
dinn <dinn.ding@vechain.com>
2018-01-16 18:10:34 +0800

    test(api/transaction): 添加transaction测试用例

--- api/transaction.go
+++ api/transaction.go
@@ -2,26 +2,11 @@ package api
 
 import (
 	"errors"
+	"github.com/vechain/thor/api/utils/types"
 	"github.com/vechain/thor/chain"
 	"github.com/vechain/thor/cry"
 	"github.com/vechain/thor/thor"
-	"github.com/vechain/thor/tx"
-	"math/big"
 )
-
-//Transaction transaction
-type Transaction struct {
-	Hash        thor.Hash
-	GasPrice    *big.Int
-	Gas         uint64
-	TimeBarrier uint64
-	From        thor.Address
-
-	Clauses tx.Clauses
-}
-
-//Transactions transactions
-type Transactions []*Transaction
 
 //TransactionInterface for manage block with chain
 type TransactionInterface struct {
@@ -36,42 +21,40 @@ func NewTransactionInterface(chain *chain.Chain) *TransactionInterface {
 }
 
 //GetTransactionByHash return transaction by address
-func (ti *TransactionInterface) GetTransactionByHash(txHash thor.Hash) (*Transaction, error) {
-	tx, location, err := ti.chain.GetTransaction(txHash)
+func (ti *TransactionInterface) GetTransactionByHash(txHash thor.Hash) (*types.Transaction, error) {
+
+	tx, _, err := ti.chain.GetTransaction(txHash)
 	if err != nil {
 		return nil, err
 	}
-	signing := cry.NewSigning(location.BlockHash)
-	return convertTransactionWithSigning(tx, signing), nil
+	genesisHash, err := thor.ParseHash("0x000000006d2958e8510b1503f612894e9223936f1008be2a218c310fa8c11423")
+	if err != nil {
+		return nil, err
+	}
+	signing := cry.NewSigning(genesisHash)
 
+	return types.ConvertTransactionWithSigning(tx, signing), nil
 }
 
 //GetTransactionFromBlock return transaction from block with transaction index
-func (ti *TransactionInterface) GetTransactionFromBlock(blockNumber uint32, index uint64) (*Transaction, error) {
+func (ti *TransactionInterface) GetTransactionFromBlock(blockNumber uint32, index uint64) (*types.Transaction, error) {
+	genesisHash, err := thor.ParseHash("0x000000006d2958e8510b1503f612894e9223936f1008be2a218c310fa8c11423")
+	if err != nil {
+		return nil, err
+	}
+	signing := cry.NewSigning(genesisHash)
+
 	block, err := ti.chain.GetBlockByNumber(blockNumber)
 	if err != nil {
 		return nil, err
 	}
+
 	txs := block.Transactions()
 	if int(index+1) > len(txs) {
 		return nil, errors.New(" Transaction not found! ")
 	}
-	tx := txs[index]
-	signing := cry.NewSigning(block.Hash())
-	return convertTransactionWithSigning(tx, signing), nil
-}
 
-func convertTransactionWithSigning(tx *tx.Transaction, signing *cry.Signing) *Transaction {
-	from, err := signing.Signer(tx)
-	if err != nil {
-		return nil
-	}
-	return &Transaction{
-		From:        from,
-		Hash:        tx.SigningHash(),
-		Clauses:     tx.Clauses(),
-		GasPrice:    tx.GasPrice(),
-		Gas:         tx.Gas(),
-		TimeBarrier: tx.TimeBarrier(),
-	}
+	tx := txs[index]
+
+	return types.ConvertTransactionWithSigning(tx, signing), nil
 }
