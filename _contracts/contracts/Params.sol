fa95d0698c1dd4d2a85aa3152f5e8df60c06ac38
Qian Bin <cola.tin.com@gmail.com>
2018-01-18 01:23:09 +0800

    feat(_contracts): implements Params contract

--- _contracts/contracts/Params.sol
+++ _contracts/contracts/Params.sol
@@ -1,6 +1,27 @@
 pragma solidity ^0.4.18;
 import './Owned.sol';
 
-contract Params is Owned {
-    
+contract Params {
+    // address of voting contract, which controls the params.
+    address public voting;
+
+    mapping(string=>uint256) values;
+
+    function initialize(address _voting) public {
+        require(msg.sender == address(this));
+        voting = _voting;        
+    }
+
+    function get(string _key) public view returns(uint256) {
+        return values[_key];
+    }
+
+    function set(string _key, uint256 _value) public {
+        require(msg.sender == voting);
+        values[_key] = _value;
+        
+        Set(_key, _key, _value);
+    }
+
+    event Set(string indexed _indexedKey, string _key, uint256 _value);
 }
