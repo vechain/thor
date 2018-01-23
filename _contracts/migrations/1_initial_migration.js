e7bd18a2742361b3c1d58a2648456d59bf1190f2
Qian Bin <cola.tin.com@gmail.com>
2018-01-23 14:23:52 +0800

    refactor(contracts): remove truffle project; move sols into contracts/gen/

--- _contracts/migrations/1_initial_migration.js
+++
@@ -1,8 +0,0 @@
-var Authority = artifacts.require("../contracts/Authority.sol");
-var Energy = artifacts.require("../contracts/Energy.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Authority);
-  deployer.deploy(Energy);
-
-};
