845970d7db0e830b723c77a1d53524268fb7caa3
dinn <dinn.ding@vechain.com>
2018-01-24 13:47:06 +0800

    test(energy_test.go): 优化energy 测试用例

--- contracts/energy_test.go
+++ contracts/energy_test.go
@@ -1,9 +1,7 @@
 package contracts_test
 
 import (
-	// "fmt"
 	"github.com/stretchr/testify/assert"
-	// "github.com/stretchr/testify/assert"
 	. "github.com/vechain/thor/contracts"
 	"github.com/vechain/thor/lvldb"
 	"github.com/vechain/thor/runtime"
@@ -22,21 +20,14 @@ type account struct {
 
 func TestEnergy(t *testing.T) {
 	checkChargeAncConsume(t)
-
+	checkBalanceGrowth(t)
+	checkMulBalanceGrowth(t)
 }
 
 func checkChargeAncConsume(t *testing.T) {
 	kv, _ := lvldb.NewMem()
 	st, _ := state.New(thor.Hash{}, kv)
 	st.SetCode(Energy.Address, Energy.RuntimeBytecodes())
-	rt := runtime.New(st,
-		thor.Address{}, 0, 1000000, 1000000,
-		func(uint32) thor.Hash { return thor.Hash{} })
-	call := func(data []byte) *vm.Output {
-		return rt.Execute(
-			tx.NewClause(&Energy.Address).WithData(data),
-			0, 1000000, Energy.Address, &big.Int{}, thor.Hash{})
-	}
 
 	balance := big.NewInt(1e18)
 
@@ -52,17 +43,135 @@ func checkChargeAncConsume(t *testing.T) {
 		},
 	}
 
+	rt := runtime.New(st,
+		thor.Address{}, 0, 1000000, 1000000,
+		func(uint32) thor.Hash { return thor.Hash{} })
+	call := func(data []byte) *vm.Output {
+		return rt.Execute(
+			tx.NewClause(&Energy.Address).WithData(data),
+			0, 100000, Energy.Address, &big.Int{}, thor.Hash{})
+	}
 	for _, a := range accounts {
-		call(Energy.PackCharge(a.addr, a.balance))
-		out := call(Energy.PackBalanceOf(a.addr))
+		//gasused 100000-45336
+		out := call(Energy.PackCharge(a.addr, a.balance))
+		//gasused 100000-65959
+		out = call(Energy.PackUpdateBalance(a.addr))
 		assert.Equal(t, a.balance, new(big.Int).SetBytes(out.Value))
 	}
 
 	for _, a := range accounts {
+		rt := runtime.New(st,
+			thor.Address{}, 0, 1000000, 1000000,
+			func(uint32) thor.Hash { return thor.Hash{} })
+		call := func(data []byte) *vm.Output {
+			return rt.Execute(
+				tx.NewClause(&Energy.Address).WithData(data),
+				0, 100000, Energy.Address, &big.Int{}, thor.Hash{})
+		}
 		consume := new(big.Int).Div(a.balance, big.NewInt(2))
-		call(Energy.PackConsume(a.addr, a.addr, consume))
-		out := call(Energy.PackBalanceOf(a.addr))
+		//gasused 100000-80097
+		out := call(Energy.PackConsume(a.addr, a.addr, consume))
+		out = call(Energy.PackUpdateBalance(a.addr))
 		assert.Equal(t, new(big.Int).Sub(a.balance, consume), new(big.Int).SetBytes(out.Value))
 	}
+
+}
+
+func checkBalanceGrowth(t *testing.T) {
+	kv, _ := lvldb.NewMem()
+	st, _ := state.New(thor.Hash{}, kv)
+	st.SetCode(Energy.Address, Energy.RuntimeBytecodes())
+	st.SetCode(Voting.Address, Voting.RuntimeBytecodes())
+
+	rt := runtime.New(st,
+		thor.Address{}, 0, 1000000, 1000000,
+		func(uint32) thor.Hash { return thor.Hash{} })
+	call := func(data []byte) *vm.Output {
+		return rt.Execute(
+			tx.NewClause(&Energy.Address).WithData(data),
+			0, 1000000, Energy.Address, &big.Int{}, thor.Hash{})
+	}
+	//gasused 100000-978800
+	out := call(Energy.PackInitialize(Voting.Address))
+	vet := big.NewInt(1e18)
+	addr := thor.BytesToAddress([]byte("acc1"))
+
+	st.SetBalance(addr, vet)
+	out = call(Energy.PackUpdateBalance(addr))
+	assert.Equal(t, int64(0), new(big.Int).SetBytes(out.Value).Int64())
+
+	callFromVoting := func(data []byte) *vm.Output {
+		return rt.Execute(
+			tx.NewClause(&Energy.Address).WithData(data),
+			0, 1000000, Voting.Address, &big.Int{}, thor.Hash{})
+	}
+
+	//gasused 100000-936135
+	birth := big.NewInt(100)
+	timeInterval := uint64(10)
+	callFromVoting(Energy.PackSetBalanceBirth(birth))
+
+	rt = runtime.New(st,
+		thor.Address{}, 0, 1000000+timeInterval, 1000001,
+		func(uint32) thor.Hash { return thor.Hash{} })
+	out = call(Energy.PackUpdateBalance(addr))
+	assert.Equal(t, new(big.Int).Mul(big.NewInt(int64(timeInterval)), birth), new(big.Int).SetBytes(out.Value))
+
+}
+
+func checkMulBalanceGrowth(t *testing.T) {
+	kv, _ := lvldb.NewMem()
+	st, _ := state.New(thor.Hash{}, kv)
+	st.SetCode(Energy.Address, Energy.RuntimeBytecodes())
+	st.SetCode(Voting.Address, Voting.RuntimeBytecodes())
+
+	rt := runtime.New(st,
+		thor.Address{}, 0, 1000000, 1000000,
+		func(uint32) thor.Hash { return thor.Hash{} })
+	call := func(data []byte) *vm.Output {
+		return rt.Execute(
+			tx.NewClause(&Energy.Address).WithData(data),
+			0, 1000000, Energy.Address, &big.Int{}, thor.Hash{})
+	}
+	//gasused 100000-978800
+	out := call(Energy.PackInitialize(Voting.Address))
+	vet := big.NewInt(1e18)
+	addr := thor.BytesToAddress([]byte("acc1"))
+
+	st.SetBalance(addr, vet)
+	out = call(Energy.PackUpdateBalance(addr))
+	assert.Equal(t, int64(0), new(big.Int).SetBytes(out.Value).Int64())
+	//initialize birth
+	birth := big.NewInt(100)
+	callFromVoting := func(data []byte) *vm.Output {
+		return rt.Execute(
+			tx.NewClause(&Energy.Address).WithData(data),
+			0, 1000000, Voting.Address, &big.Int{}, thor.Hash{})
+	}
+	callFromVoting(Energy.PackSetBalanceBirth(birth))
+
+	//gasused 100000-936135
+	//set birth
+	time := 0
+	timeInterval := 2000
+	b := 10
+	totalBenefit := 0
+
+	for i := 0; i < 10; i++ {
+		rt = runtime.New(st,
+			thor.Address{}, 0, 1000000+uint64(time), 1000000,
+			func(uint32) thor.Hash { return thor.Hash{} })
+		callFromVoting(Energy.PackSetBalanceBirth(big.NewInt(int64(b))))
+		time += timeInterval
+		totalBenefit += timeInterval * b
+		b += 20
+	}
+
+	rt = runtime.New(st,
+		thor.Address{}, 0, 1000000+uint64(time), 1000000,
+		func(uint32) thor.Hash { return thor.Hash{} })
+	//gasused 100000-927830
+	out = call(Energy.PackUpdateBalance(addr))
+	assert.Equal(t, big.NewInt(int64(totalBenefit)), new(big.Int).SetBytes(out.Value))
 
 }
