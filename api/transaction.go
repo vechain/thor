89fcdad98f065d428d46a50a62c381f112fc15d5
dinn <dinn.ding@vechain.com>
2018-01-25 16:31:06 +0800

    refactor(api/tx): 替换chain为transactionGetter

--- api/transaction.go
+++ api/transaction.go
@@ -3,26 +3,32 @@ package api
 import (
 	"errors"
 	"github.com/vechain/thor/api/utils/types"
-	"github.com/vechain/thor/chain"
+	"github.com/vechain/thor/chain/persist"
 	"github.com/vechain/thor/thor"
+	"github.com/vechain/thor/tx"
 )
+
+type transactionGetter interface {
+	blockGetter
+	GetTransaction(thor.Hash) (*tx.Transaction, *persist.TxLocation, error)
+}
 
 //TransactionInterface for manage block with chain
 type TransactionInterface struct {
-	chain *chain.Chain
+	txGetter transactionGetter
 }
 
 //NewTransactionInterface return a BlockMananger by chain
-func NewTransactionInterface(chain *chain.Chain) *TransactionInterface {
+func NewTransactionInterface(txGetter transactionGetter) *TransactionInterface {
 	return &TransactionInterface{
-		chain: chain,
+		txGetter: txGetter,
 	}
 }
 
 //GetTransactionByHash return transaction by address
 func (ti *TransactionInterface) GetTransactionByHash(txHash thor.Hash) (*types.Transaction, error) {
 
-	tx, location, err := ti.chain.GetTransaction(txHash)
+	tx, location, err := ti.txGetter.GetTransaction(txHash)
 	if err != nil {
 		return nil, err
 	}
@@ -32,7 +38,7 @@ func (ti *TransactionInterface) GetTransactionByHash(txHash thor.Hash) (*types.T
 		return nil, err
 	}
 
-	block, err := ti.chain.GetBlock(location.BlockID)
+	block, err := ti.txGetter.GetBlock(location.BlockID)
 	if err != nil {
 		return nil, err
 	}
@@ -46,7 +52,7 @@ func (ti *TransactionInterface) GetTransactionByHash(txHash thor.Hash) (*types.T
 //GetTransactionFromBlock return transaction from block with transaction index
 func (ti *TransactionInterface) GetTransactionFromBlock(blockNumber uint32, index uint64) (*types.Transaction, error) {
 
-	block, err := ti.chain.GetBlockByNumber(blockNumber)
+	block, err := ti.txGetter.GetBlockByNumber(blockNumber)
 	if err != nil {
 		return nil, err
 	}
