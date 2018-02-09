7e349a5ba7e2cad6e87f876a6bf12c2f7cb024dd
dinn <dinn.ding@vechain.com>
2018-02-09 18:08:37 +0800

    feat(api tx): api sendtransaction with rawtransaction

--- api/transaction.go
+++ api/transaction.go
@@ -2,32 +2,38 @@ package api
 
 import (
 	"github.com/vechain/thor/api/utils/types"
-	"github.com/vechain/thor/chain/persist"
+	"github.com/vechain/thor/chain"
 	"github.com/vechain/thor/thor"
 	"github.com/vechain/thor/tx"
+	"github.com/vechain/thor/txpool"
 )
 
-type transactionGetter interface {
-	blockGetter
-	GetTransaction(thor.Hash) (*tx.Transaction, *persist.TxLocation, error)
+type transactionPool interface {
+	Add(tx *tx.Transaction) error
+	GetTransaction(txID thor.Hash) *tx.Transaction
 }
 
 //TransactionInterface for manage block with chain
 type TransactionInterface struct {
-	txGetter transactionGetter
+	chain  *chain.Chain
+	txPool *txpool.TxPool
 }
 
 //NewTransactionInterface return a BlockMananger by chain
-func NewTransactionInterface(txGetter transactionGetter) *TransactionInterface {
+func NewTransactionInterface(chain *chain.Chain, txPool *txpool.TxPool) *TransactionInterface {
 	return &TransactionInterface{
-		txGetter: txGetter,
+		chain:  chain,
+		txPool: txPool,
 	}
 }
 
 //GetTransactionByID return transaction by transaction id
 func (ti *TransactionInterface) GetTransactionByID(txID thor.Hash) (*types.Transaction, error) {
 
-	tx, location, err := ti.txGetter.GetTransaction(txID)
+	if pengdingTransaction := ti.txPool.GetTransaction(txID); pengdingTransaction != nil {
+		return types.ConvertTransaction(pengdingTransaction)
+	}
+	tx, location, err := ti.chain.GetTransaction(txID)
 	if err != nil {
 		return nil, err
 	}
@@ -37,7 +43,7 @@ func (ti *TransactionInterface) GetTransactionByID(txID thor.Hash) (*types.Trans
 		return nil, err
 	}
 
-	block, err := ti.txGetter.GetBlock(location.BlockID)
+	block, err := ti.chain.GetBlock(location.BlockID)
 	if err != nil {
 		return nil, err
 	}
@@ -46,4 +52,20 @@ func (ti *TransactionInterface) GetTransactionByID(txID thor.Hash) (*types.Trans
 	t.BlockNumber = block.Header().Number()
 	t.Index = location.Index
 	return t, nil
+}
+
+//SendTransaction send a transactoion
+func (ti *TransactionInterface) SendTransaction(raw *types.RawTransaction) error {
+	bestblk, err := ti.chain.GetBestBlock()
+	if err != nil {
+		return err
+	}
+	builder, err := types.BuilcRawTransaction(raw)
+	if err != nil {
+		return err
+	}
+	builder.ChainTag(bestblk.Header().ChainTag())
+	transaction := builder.Build().WithSignature(raw.Sig)
+	return ti.txPool.Add(transaction)
+
 }
