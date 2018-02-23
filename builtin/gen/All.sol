d8761e62e405fe58f675e036d075988058bd5e09
Qian Bin <cola.tin.com@gmail.com>
2018-02-24 00:31:09 +0800

    refactor(builtin): separate contracts into native and non-native

---
+++ builtin/gen/All.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.4.18;
+
+import "./Authority.sol";
+import "./Energy.sol";
+import "./Executor.sol";
+import "./Params.sol";
--- builtin/gen/Params.sol
+++ builtin/gen/Params.sol
@@ -2,25 +2,31 @@ pragma solidity ^0.4.18;
 
 contract Params {
 
+    function n() internal view returns(ParamsNative) {
+        return ParamsNative(this);
+    }
+
     function executor() public view returns(address) {
-        return this.nativeGetExecutor();
+        return n().nativeGetExecutor();
     }
 
     function set(bytes32 _key, uint256 _value) public {
-        require(msg.sender == this.nativeGetExecutor());
+        require(msg.sender == n().nativeGetExecutor());
 
-        this.nativeSet(_key, _value);
+        n().nativeSet(_key, _value);
         Set(_key, _value);
     }
 
     function get(bytes32 _key) public view returns(uint256) {
-        return this.nativeGet(_key);
+        return n().nativeGet(_key);
     }
 
-    function nativeGetExecutor() public view returns(address) {}
-
-    function nativeSet(bytes32 key, uint256 value) public {}
-    function nativeGet(bytes32 key) public view returns(uint256) {}
-
     event Set(bytes32 indexed key, uint256 value);
+}
+
+contract ParamsNative {
+    function nativeGetExecutor() public view returns(address);
+
+    function nativeSet(bytes32 key, uint256 value) public;
+    function nativeGet(bytes32 key) public view returns(uint256);
 }
