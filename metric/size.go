d52d1258ccbcc4e7dd8037da1841a3b7f7ba86c4
Qian Bin <cola.tin.com@gmail.com>
2018-02-25 20:11:37 +0800

    feat(metric): metering stuff

---
+++ metric/size.go
@@ -0,0 +1,35 @@
+package metric
+
+import (
+	"fmt"
+	"io"
+)
+
+// StorageSize describes storage size in bytes.
+type StorageSize int64
+
+func (ss StorageSize) String() string {
+	if ss > 1000000000 {
+		return fmt.Sprintf("%.2f gB", float64(ss)/1000000000)
+	} else if ss > 1000000 {
+		return fmt.Sprintf("%.2f mB", float64(ss)/1000000)
+	} else if ss > 1000 {
+		return fmt.Sprintf("%.2f kB", float64(ss)/1000)
+	}
+	return fmt.Sprintf("%d B", ss)
+}
+
+// Int64 returns int64 value.
+func (ss StorageSize) Int64() int64 {
+	return int64(ss)
+}
+
+// Write implements io.Writer, so it can be passed into function
+// that accepts io.Writer to count written bytes.
+func (ss *StorageSize) Write(b []byte) (int, error) {
+	n := len(b)
+	*ss += StorageSize(n)
+	return n, nil
+}
+
+var _ io.Writer = (*StorageSize)(nil)
