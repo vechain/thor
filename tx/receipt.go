651864a44de3f69456ee2041110cd5c041ac98da
Qian Bin <cola.tin.com@gmail.com>
2018-03-03 00:45:06 +0800

    feat(tx): add GasPayer field to Receipt

--- tx/receipt.go
+++ tx/receipt.go
@@ -1,9 +1,15 @@
 package tx
 
+import (
+	"github.com/vechain/thor/thor"
+)
+
 // Receipt represents the results of a transaction.
 type Receipt struct {
 	// gas used by this tx
 	GasUsed uint64
+	// the one who payed for gas
+	GasPayer thor.Address
 	// outputs of clauses in tx
 	Outputs []*Output
 }
