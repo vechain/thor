12a74319bb71a9e65bf8caf3203fb4a222a1851f
Qian Bin <cola.tin.com@gmail.com>
2018-03-05 20:13:31 +0800

    refactor(packer): break tx adopting loop; add errors

---
+++ packer/errors.go
@@ -0,0 +1,38 @@
+package packer
+
+import "github.com/pkg/errors"
+
+var (
+	errGasLimitReached   = errors.New("gas limit reached")
+	errTxNotAdoptableNow = errors.New("tx not adoptable now")
+	errKnownTx           = errors.New("known tx")
+)
+
+// IsGasLimitReached block if full of txs.
+func IsGasLimitReached(err error) bool {
+	return errors.Cause(err) == errGasLimitReached
+}
+
+// IsTxNotAdoptableNow tx can not be adopted now.
+func IsTxNotAdoptableNow(err error) bool {
+	return errors.Cause(err) == errTxNotAdoptableNow
+}
+
+// IsBadTx not a valid tx.
+func IsBadTx(err error) bool {
+	_, ok := errors.Cause(err).(badTxError)
+	return ok
+}
+
+// IsKnownTx tx is already adopted, or in the chain.
+func IsKnownTx(err error) bool {
+	return errors.Cause(err) == errKnownTx
+}
+
+type badTxError struct {
+	msg string
+}
+
+func (e badTxError) Error() string {
+	return "bad tx: " + e.msg
+}
