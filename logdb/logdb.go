bc23cca2371ccc0c68a36e8619a1aa5ea5f28c34
dinn <dinn.ding@vechain.com>
2018-03-06 18:29:31 +0800

    refactor(logdb): refactor logdb

--- logdb/logdb.go
+++ logdb/logdb.go
@@ -3,6 +3,7 @@ package logdb
 import (
 	"database/sql"
 	"fmt"
+
 	sqlite3 "github.com/mattn/go-sqlite3"
 	"github.com/vechain/thor/thor"
 	"sync"
@@ -12,8 +13,8 @@ import (
 type FilterOption struct {
 	FromBlock uint32
 	ToBlock   uint32
-	Address   thor.Address // always a contract address
-	Topics    [5]*thor.Hash
+	Address   *thor.Address // always a contract address
+	TopicSet  [][5]*thor.Hash
 }
 
 //LDB manages all logs
@@ -24,21 +25,23 @@ type LDB struct {
 	m             sync.RWMutex
 }
 
-//OpenDB open a logdb
-func OpenDB(path string) (*LDB, error) {
+//New open a logdb
+func New(path string) (*LDB, error) {
 	db, err := sql.Open("sqlite3", path)
 	if err != nil {
 		return nil, err
 	}
 	s, _, _ := sqlite3.Version()
-	if err != nil {
-		return nil, err
-	}
-	return &LDB{
+	ldb := &LDB{
 		path:          path,
 		db:            db,
 		sqliteVersion: s,
-	}, nil
+	}
+	err = ldb.execInTransaction(LogSQL)
+	if err != nil {
+		return nil, err
+	}
+	return ldb, nil
 }
 
 //Insert insert logs into db
@@ -51,42 +54,71 @@ func (db *LDB) Insert(logs []*Log) error {
 
 	stmt := ""
 	for _, log := range logs {
-		stmt += "insert into log(blockID ,blockNumber ,txID ,txOrigin ,address ,data ,topic0 ,topic1 ,topic2 ,topic3 ,topic4) values " + fmt.Sprintf(" ('%v',%v,'%v','%v','%v','%s','%v','%v','%v','%v','%v'); ", log.blockID.String(),
-			log.blockNumber,
-			log.txID,
-			log.txOrigin,
-			log.address,
-			string(log.data),
-			formatHash(log.topic0),
-			formatHash(log.topic1),
-			formatHash(log.topic2),
-			formatHash(log.topic3),
-			formatHash(log.topic4))
+		stmt += "insert into log(blockID ,blockNumber ,txID ,txOrigin ,address ,data ,topic0 ,topic1 ,topic2 ,topic3 ,topic4) values " + fmt.Sprintf(" ('%v',%v,'%v','%v','%v','%s','%v','%v','%v','%v','%v'); ",
+			log.BlockID,
+			log.BlockNumber,
+			log.TxID,
+			log.TxOrigin,
+			log.Address,
+			string(log.Data),
+			formatHash(log.Topics[0]),
+			formatHash(log.Topics[1]),
+			formatHash(log.Topics[2]),
+			formatHash(log.Topics[3]),
+			formatHash(log.Topics[4]))
 	}
-	return db.ExecInTransaction(stmt)
+	return db.execInTransaction(stmt)
 }
 
 //Filter return logs with options
-func (db *LDB) Filter(options []*FilterOption) ([]*Log, error) {
-	stmt := "select * from log where 1 "
-	if len(options) != 0 {
-		for _, op := range options {
-			stmt += fmt.Sprintf(" or ( blockNumber >= %v and blockNumber <= %v and address = %v and topic0 = %v and topic1 = %v and topic2 = %v and topic3 = %v and topic4 = %v ) ",
-				op.FromBlock,
-				op.ToBlock,
-				op.Address,
-				formatHash(op.Topics[0]),
-				formatHash(op.Topics[1]),
-				formatHash(op.Topics[2]),
-				formatHash(op.Topics[3]),
-				formatHash(op.Topics[4]))
+func (db *LDB) Filter(option *FilterOption) ([]*Log, error) {
+	if option == nil {
+		return db.Query("select * from log")
+	}
+	stmt := "select * from log where ( 1"
+	stmt += fmt.Sprintf(" and blockNumber >= %v ", option.FromBlock)
+	if option.ToBlock >= option.FromBlock {
+		stmt += fmt.Sprintf(" and blockNumber <= %v ", option.ToBlock)
+	}
+	if option.Address != nil {
+		stmt += fmt.Sprintf(" and address = '%v' ", option.Address)
+	}
+	stmt += " ) "
+	length := len(option.TopicSet)
+	if length > 0 {
+		for i, topics := range option.TopicSet {
+			if i == 0 {
+				stmt += " and (( 1 "
+			} else {
+				stmt += " or ( 1 "
+			}
+			if topics[0] != nil {
+				stmt += fmt.Sprintf(" and topic0 = '%v' ", topics[0])
+			}
+			if topics[1] != nil {
+				stmt += fmt.Sprintf(" and topic1 = '%v' ", topics[1])
+			}
+			if topics[2] != nil {
+				stmt += fmt.Sprintf(" and topic2 = '%v' ", topics[2])
+			}
+			if topics[3] != nil {
+				stmt += fmt.Sprintf(" and topic3 = '%v' ", topics[3])
+			}
+			if topics[4] != nil {
+				stmt += fmt.Sprintf(" and topic4 = '%v' ", topics[4])
+			}
+			if i == length-1 {
+				stmt += " )) "
+			} else {
+				stmt += " ) "
+			}
 		}
 	}
 	return db.Query(stmt)
 }
 
-//ExecInTransaction execute sql in a transaction
-func (db *LDB) ExecInTransaction(sqlStmt string, args ...interface{}) error {
+//execInTransaction execute sql in a transaction
+func (db *LDB) execInTransaction(sqlStmt string, args ...interface{}) error {
 	tx, err := db.db.Begin()
 	if err != nil {
 		return err
