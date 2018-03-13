869c368f8338807509627256f51924a9805fb36c
Han Xiao <hanxiao@vechain.com>
2018-03-13 18:58:39 +0800

    feat(comm.sync): 同步客户端逻辑

--- comm/proto/protocol.go
+++ comm/proto/protocol.go
@@ -24,6 +24,7 @@ const (
 	MsgNewBlock
 	MsgNewTx
 	MsgGetBlockIDByNumber
+	MsgGetBlocksByNumber // 获取某个 Num 之后的部分块 (不包含 num 所在的块)
 )
 
 // RespStatus response payload of MsgStatus.
@@ -79,3 +80,20 @@ func (req ReqGetBlockIDByNumber) Do(ctx context.Context, session *p2psrv.Session
 
 // RespGetBlockIDByNumber response payload of MsgGetBlockIDByNumber.
 type RespGetBlockIDByNumber thor.Hash
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// ReqGetBlocksByNumber request payload of MsgGetBlocksByNumber.
+type ReqGetBlocksByNumber uint32
+
+// Do make request to session.
+func (req ReqGetBlocksByNumber) Do(ctx context.Context, session *p2psrv.Session) (RespGetBlocksByNumber, error) {
+	var resp RespGetBlocksByNumber
+	if err := session.Request(ctx, MsgGetBlockIDByNumber, &req, &resp); err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+// RespGetBlocksByNumber response payload of MsgGetBlocksByNumber.
+type RespGetBlocksByNumber []*block.Block
