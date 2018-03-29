3f81f1a3a9f91794aac9285750f48f21d60462b2
Qian Bin <cola.tin.com@gmail.com>
2018-03-29 16:03:13 +0800

    refactor(txpool): reorg codes

--- txpool/txobject.go
+++ txpool/txobject.go
@@ -1,45 +1,46 @@
 package txpool
 
 import (
-	"github.com/vechain/thor/tx"
 	"math/big"
+
+	"github.com/vechain/thor/tx"
 )
 
-//TxObject wrap transaction
-type TxObject struct {
-	transaction  *tx.Transaction
+//txObject wrap transaction
+type txObject struct {
+	tx           *tx.Transaction
 	overallGP    *big.Int
 	creationTime int64
 }
 
 //TxObjects array of TxObject
-type TxObjects []*TxObject
+type txObjects []*txObject
 
-//NewTxObject NewTxObject
-func NewTxObject(transaction *tx.Transaction, creationTime int64) *TxObject {
-	return &TxObject{
-		transaction:  transaction,
+//newTxObject NewTxObject
+func newTxObject(tx *tx.Transaction, creationTime int64) *txObject {
+	return &txObject{
+		tx:           tx,
 		overallGP:    new(big.Int),
 		creationTime: creationTime,
 	}
 }
 
 //SetOverallGP set overallGP of txObejct
-func (obj *TxObject) SetOverallGP(overallGP *big.Int) {
+func (obj *txObject) SetOverallGP(overallGP *big.Int) {
 	obj.overallGP = overallGP
 }
 
 //CreationTime returns obj's CreationTime
-func (obj *TxObject) CreationTime() int64 {
+func (obj *txObject) CreationTime() int64 {
 	return obj.creationTime
 }
 
 //OverallGP returns obj's overallGP
-func (obj *TxObject) OverallGP() *big.Int {
+func (obj *txObject) OverallGP() *big.Int {
 	return obj.overallGP
 }
 
-//Transaction returns obj's transaction
-func (obj *TxObject) Transaction() *tx.Transaction {
-	return obj.transaction
+//Tx returns obj's transaction
+func (obj *txObject) Tx() *tx.Transaction {
+	return obj.tx
 }
