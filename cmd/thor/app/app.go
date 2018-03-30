3d770379f5f5a33df1b6eabf1144893fc3df083b
Qian Bin <cola.tin.com@gmail.com>
2018-03-30 13:54:43 +0800

    feat(thor): replace boot node

--- cmd/thor/app/app.go
+++ cmd/thor/app/app.go
@@ -27,8 +27,7 @@ import (
 	"github.com/vechain/thor/txpool"
 )
 
-var boot1 = "enode://ec0ccfaeefa53c6a7ec73ca36940c911902d1f6f9da7567d05c44d1aa841b309260f7b228008331b61c8890ece0297eb0c3541af1a51fd5fcc749bee9104e64a@192.168.31.182:55555"
-var boot2 = "enode://0cc5f5ffb5d9098c8b8c62325f3797f56509bff942704687b6530992ac706e2cb946b90a34f1f19548cd3c7baccbcaea354531e5983c7d1bc0dee16ce4b6440b@40.118.3.223:30305"
+var boot1 = "enode://b788e1d863aaea4fecef4aba4be50e59344d64f2db002160309a415ab508977b8bffb7bac3364728f9cdeab00ebdd30e8d02648371faacd0819edc27c18b2aad@106.15.4.191:55555"
 
 type newBlockEvent struct {
 	Blk      *block.Block
@@ -94,7 +93,7 @@ func New(
 			PrivateKey:     nodeKey,
 			MaxPeers:       25,
 			ListenAddr:     port,
-			BootstrapNodes: []*discover.Node{discover.MustParseNode(boot1), discover.MustParseNode(boot2)},
+			BootstrapNodes: []*discover.Node{discover.MustParseNode(boot1)},
 		})
 
 	return app, nil
