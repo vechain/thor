bd53135c534da99182cec51ca66aab5e04fb19e5
dinn <dinn.ding@vechain.com>
2018-04-04 16:10:26 +0800

    refactor(api): refactor api

--- api/accounts/accounts.go
+++ api/accounts/accounts.go
@@ -1,6 +1,8 @@
 package accounts
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"math/big"
 	"net/http"
 	"strconv"
@@ -12,73 +14,46 @@ import (
 	"github.com/vechain/thor/api/utils"
 	"github.com/vechain/thor/block"
 	"github.com/vechain/thor/chain"
+	"github.com/vechain/thor/logdb"
+	"github.com/vechain/thor/runtime"
 	"github.com/vechain/thor/state"
 	"github.com/vechain/thor/thor"
+	"github.com/vechain/thor/tx"
+	"github.com/vechain/thor/vm"
 )
 
 type Accounts struct {
 	chain        *chain.Chain
 	stateCreator *state.Creator
+	logDB        *logdb.LogDB
 }
 
-func New(chain *chain.Chain, stateCreator *state.Creator) *Accounts {
+func New(chain *chain.Chain, stateCreator *state.Creator, logDB *logdb.LogDB) *Accounts {
 	return &Accounts{
 		chain,
 		stateCreator,
+		logDB,
 	}
 }
 
-func (a *Accounts) getStateRoot(blockNum uint32) (thor.Bytes32, error) {
-	var (
-		block *block.Block
-		err   error
-	)
-
-	if blockNum == math.MaxUint32 {
-		block, err = a.chain.GetBestBlock()
-	} else {
-		block, err = a.chain.GetBlockByNumber(blockNum)
-	}
-	if err != nil {
-		return thor.Bytes32{}, err
-	}
-	return block.Header().StateRoot(), nil
-}
-
-func (a *Accounts) getState(blockNum uint32) (*state.State, error) {
-	root, err := a.getStateRoot(blockNum)
+func (a *Accounts) getAccount(addr thor.Address, stateRoot thor.Bytes32) (*Account, error) {
+	state, err := a.stateCreator.NewState(stateRoot)
 	if err != nil {
 		return nil, err
 	}
-	return a.stateCreator.NewState(root)
-}
-
-func (a *Accounts) getBalance(addr thor.Address, blockNum uint32) (*big.Int, error) {
-	state, err := a.getState(blockNum)
-	if err != nil {
-		return nil, err
-	}
-	bal := state.GetBalance(addr)
-	if err := state.Error(); err != nil {
-		return nil, err
-	}
-	return bal, nil
-}
-
-func (a *Accounts) getCode(addr thor.Address, blockNum uint32) ([]byte, error) {
-	state, err := a.getState(blockNum)
-	if err != nil {
-		return nil, err
-	}
+	b := state.GetBalance(addr)
 	code := state.GetCode(addr)
 	if err := state.Error(); err != nil {
 		return nil, err
 	}
-	return code, nil
+	return &Account{
+		Balance: math.HexOrDecimal256(*b),
+		Code:    hexutil.Encode(code),
+	}, nil
 }
 
-func (a *Accounts) getStorage(addr thor.Address, key thor.Bytes32, blockNum uint32) (thor.Bytes32, error) {
-	state, err := a.getState(blockNum)
+func (a *Accounts) getStorage(addr thor.Address, key thor.Bytes32, stateRoot thor.Bytes32) (thor.Bytes32, error) {
+	state, err := a.stateCreator.NewState(stateRoot)
 	if err != nil {
 		return thor.Bytes32{}, err
 	}
@@ -89,38 +64,125 @@ func (a *Accounts) getStorage(addr thor.Address, key thor.Bytes32, blockNum uint
 	return storage, nil
 }
 
-func (a *Accounts) handleGetBalance(w http.ResponseWriter, req *http.Request) error {
-	hexAddr := mux.Vars(req)["address"]
-	addr, err := thor.ParseAddress(hexAddr)
-	if err != nil {
-		return utils.HTTPError(errors.Wrap(err, "address"), http.StatusBadRequest)
+func (a *Accounts) defaultContractCallOptions() *ContractCallOptions {
+	gp := big.NewInt(2106)
+	gasPrice := math.HexOrDecimal256(*gp)
+	v := big.NewInt(0)
+	value := math.HexOrDecimal256(*v)
+	return &ContractCallOptions{
+		ClauseIndex: 0,
+		Gas:         21000,
+		From:        &thor.Address{},
+		GasPrice:    &gasPrice,
+		TxID:        &thor.Bytes32{},
+		Value:       &value,
 	}
-	blockNum, err := a.parseBlockNum(req.URL.Query().Get("blockNumber"))
-	if err != nil {
-		return utils.HTTPError(errors.Wrap(err, "blockNumber"), http.StatusBadRequest)
-	}
-	balance, err := a.getBalance(addr, blockNum)
-	if err != nil {
-		return err
-	}
-	return utils.WriteJSON(w, (*math.HexOrDecimal256)(balance))
 }
 
-func (a *Accounts) handleGetCode(w http.ResponseWriter, req *http.Request) error {
+func (a *Accounts) santinizeOptions(options *ContractCallOptions) {
+	defaultOptions := a.defaultContractCallOptions()
+
+	if options.ClauseIndex < defaultOptions.ClauseIndex {
+		options.ClauseIndex = defaultOptions.ClauseIndex
+	}
+	if options.Gas < defaultOptions.Gas {
+		options.Gas = defaultOptions.Gas
+	}
+
+	if options.GasPrice == nil {
+		options.GasPrice = defaultOptions.GasPrice
+	} else {
+		gp := big.Int(*options.GasPrice)
+		gpDefault := big.Int(*defaultOptions.GasPrice)
+		if (&gp).Cmp(&gpDefault) < 0 {
+			options.GasPrice = defaultOptions.GasPrice
+		}
+	}
+
+	if options.Value == nil {
+		options.Value = defaultOptions.Value
+	}
+
+	if options.From == nil {
+		options.From = defaultOptions.From
+	}
+
+	if options.TxID == nil {
+		options.TxID = defaultOptions.TxID
+	}
+}
+
+//Call a contract with input
+func (a *Accounts) Call(to *thor.Address, body *ContractCallBody, block *block.Block) (output []byte, err error) {
+	a.santinizeOptions(&body.Options)
+	state, err := a.stateCreator.NewState(block.Header().StateRoot())
+	if err != nil {
+		return nil, err
+	}
+	header := block.Header()
+	rt := runtime.New(state, header.Beneficiary(), header.Number(), header.Timestamp(), header.GasLimit(), nil)
+	v := big.Int(*body.Options.Value)
+	data, err := hexutil.Decode(body.Input)
+	if err != nil {
+		return nil, err
+	}
+	clause := tx.NewClause(to).WithData(data).WithValue(&v)
+	var vmout *vm.Output
+	gp := big.Int(*body.Options.GasPrice)
+	vmout = rt.Call(clause, body.Options.ClauseIndex, body.Options.Gas, *body.Options.From, &gp, *body.Options.TxID)
+	if err := state.Error(); err != nil {
+		return nil, err
+	}
+	if vmout.VMErr != nil {
+		return nil, vmout.VMErr
+	}
+	return vmout.Value, nil
+
+}
+
+func (a *Accounts) handleCallContract(w http.ResponseWriter, req *http.Request) error {
+	addr, err := thor.ParseAddress(mux.Vars(req)["address"])
+	if err != nil {
+		return utils.HTTPError(err, http.StatusBadRequest)
+	}
+
+	res, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return utils.HTTPError(err, http.StatusBadRequest)
+	}
+	req.Body.Close()
+	callBody := &ContractCallBody{}
+	if err := json.Unmarshal(res, &callBody); err != nil {
+		return utils.HTTPError(err, http.StatusBadRequest)
+	}
+
+	b, err := a.getBlock(req.URL.Query().Get("revision"))
+	if err != nil {
+		return utils.HTTPError(errors.Wrap(err, "revision"), http.StatusBadRequest)
+	}
+
+	output, err := a.Call(&addr, callBody, b)
+	if err != nil {
+		return utils.HTTPError(err, http.StatusBadRequest)
+	}
+	return utils.WriteJSON(w, hexutil.Encode(output))
+}
+
+func (a *Accounts) handleGetAccount(w http.ResponseWriter, req *http.Request) error {
 	hexAddr := mux.Vars(req)["address"]
 	addr, err := thor.ParseAddress(hexAddr)
 	if err != nil {
 		return utils.HTTPError(errors.Wrap(err, "address"), http.StatusBadRequest)
 	}
-	blockNum, err := a.parseBlockNum(req.URL.Query().Get("blockNumber"))
+	b, err := a.getBlock(req.URL.Query().Get("revision"))
 	if err != nil {
-		return utils.HTTPError(errors.Wrap(err, "blockNumber"), http.StatusBadRequest)
+		return utils.HTTPError(errors.Wrap(err, "revision"), http.StatusBadRequest)
 	}
-	code, err := a.getCode(addr, blockNum)
+	acc, err := a.getAccount(addr, b.Header().StateRoot())
 	if err != nil {
 		return err
 	}
-	return utils.WriteJSON(w, hexutil.Encode(code))
+	return utils.WriteJSON(w, acc)
 }
 
 func (a *Accounts) handleGetStorage(w http.ResponseWriter, req *http.Request) error {
@@ -133,12 +195,12 @@ func (a *Accounts) handleGetStorage(w http.ResponseWriter, req *http.Request) er
 	if err != nil {
 		return utils.HTTPError(errors.Wrap(err, "key"), http.StatusBadRequest)
 	}
-	blockNum, err := a.parseBlockNum(req.URL.Query().Get("blockNumber"))
+	b, err := a.getBlock(req.URL.Query().Get("revision"))
 	if err != nil {
-		return utils.HTTPError(errors.Wrap(err, "blockNumber"), http.StatusBadRequest)
+		return utils.HTTPError(errors.Wrap(err, "revision"), http.StatusBadRequest)
 	}
 
-	storage, err := a.getStorage(addr, key, blockNum)
+	storage, err := a.getStorage(addr, key, b.Header().StateRoot())
 	if err != nil {
 		return err
 	}
@@ -159,15 +221,149 @@ func (a *Accounts) parseBlockNum(blkNum string) (uint32, error) {
 	return uint32(n), nil
 }
 
+//Filter query logs with option
+func (a *Accounts) filter(option *logdb.FilterOption) ([]Log, error) {
+	logs, err := a.logDB.Filter(option)
+	if err != nil {
+		return nil, err
+	}
+	lgs := make([]Log, len(logs))
+	for i, log := range logs {
+		lgs[i] = convertLog(log)
+	}
+	return lgs, nil
+}
+
+func (a *Accounts) handleFilterLogs(w http.ResponseWriter, req *http.Request) error {
+	res, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return utils.HTTPError(err, http.StatusBadRequest)
+	}
+	req.Body.Close()
+	var f *FilterTopics
+	if len(res) != 0 {
+		if err := json.Unmarshal(res, &f); err != nil {
+			return utils.HTTPError(err, http.StatusBadRequest)
+		}
+	}
+	params := mux.Vars(req)
+	addr, err := thor.ParseAddress(params["address"])
+	if err != nil {
+		return utils.HTTPError(errors.Wrap(err, "address"), http.StatusBadRequest)
+	}
+	fromBlock, err := a.parseFromBlock(req.URL.Query().Get("fromBlock"))
+	if err != nil {
+		return utils.HTTPError(errors.Wrap(err, "fromBlock"), http.StatusBadRequest)
+	}
+	toBlock, err := a.parseToBlock(req.URL.Query().Get("toBlock"))
+	if err != nil {
+		return utils.HTTPError(errors.Wrap(err, "toBlock"), http.StatusBadRequest)
+	}
+	offset, err := a.parseOffset(req.URL.Query().Get("offset"))
+	if err != nil {
+		return utils.HTTPError(errors.Wrap(err, "offset"), http.StatusBadRequest)
+	}
+	limit, err := a.parseLimit(req.URL.Query().Get("limit"))
+	if err != nil {
+		return utils.HTTPError(errors.Wrap(err, "limit"), http.StatusBadRequest)
+	}
+	options := &logdb.FilterOption{
+		FromBlock: fromBlock,
+		ToBlock:   toBlock,
+		Address:   &addr,
+		TopicSet:  f.TopicSet,
+		Offset:    offset,
+		Limit:     limit,
+	}
+	logs, err := a.filter(options)
+	if err != nil {
+		return utils.HTTPError(err, http.StatusBadRequest)
+	}
+	return utils.WriteJSON(w, logs)
+}
+
+func (a *Accounts) parseOffset(offset string) (uint64, error) {
+	if offset == "" {
+		return math.MaxUint64, nil
+	}
+	n, err := strconv.ParseUint(offset, 0, 0)
+	if err != nil {
+		return math.MaxUint64, err
+	}
+	return uint64(n), nil
+}
+
+func (a *Accounts) parseLimit(limit string) (uint32, error) {
+	if limit == "" {
+		return math.MaxUint32, nil
+	}
+	n, err := strconv.ParseUint(limit, 0, 0)
+	if err != nil {
+		return math.MaxUint32, err
+	}
+	if n > math.MaxUint32 {
+		return math.MaxUint32, errors.New("block number exceeded")
+	}
+	return uint32(n), nil
+}
+
+func (a *Accounts) parseFromBlock(blkNum string) (uint32, error) {
+	if blkNum == "" {
+		return 0, nil
+	}
+	n, err := strconv.ParseUint(blkNum, 0, 0)
+	if err != nil {
+		return math.MaxUint32, err
+	}
+	if n > math.MaxUint32 {
+		return math.MaxUint32, errors.New("block number exceeded")
+	}
+	return uint32(n), nil
+}
+
+func (a *Accounts) parseToBlock(blkNum string) (uint32, error) {
+	if blkNum == "" {
+		return math.MaxUint32, nil
+	}
+	n, err := strconv.ParseUint(blkNum, 0, 0)
+	if err != nil {
+		return math.MaxUint32, err
+	}
+	if n > math.MaxUint32 {
+		return math.MaxUint32, errors.New("block number exceeded")
+	}
+	return uint32(n), nil
+}
+
+func (a *Accounts) getBlock(revision string) (*block.Block, error) {
+	if revision == "" || revision == "best" {
+		return a.chain.GetBestBlock()
+	}
+	blkID, err := thor.ParseBytes32(revision)
+	if err != nil {
+		n, err := strconv.ParseUint(revision, 0, 0)
+		if err != nil {
+			return nil, err
+		}
+		if n > math.MaxUint32 {
+			return nil, errors.New("block number exceeded")
+		}
+		return a.chain.GetBlockByNumber(uint32(n))
+	}
+	return a.chain.GetBlock(blkID)
+}
+
 func (a *Accounts) Mount(root *mux.Router, pathPrefix string) {
 	sub := root.PathPrefix(pathPrefix).Subrouter()
 
-	sub.Path("/{address}/balance").Methods(http.MethodGet).HandlerFunc(utils.WrapHandlerFunc(a.handleGetBalance))
-	sub.Path("/{address}/balance").Queries("blockNumber", "{blockNumber}").Methods(http.MethodGet).HandlerFunc(utils.WrapHandlerFunc(a.handleGetBalance))
-
-	sub.Path("/{address}/code").Methods(http.MethodGet).HandlerFunc(utils.WrapHandlerFunc(a.handleGetCode))
-	sub.Path("/{address}/code").Queries("blockNumber", "{blockNumber}").Methods(http.MethodGet).HandlerFunc(utils.WrapHandlerFunc(a.handleGetCode))
+	sub.Path("/{address}").Methods(http.MethodGet).HandlerFunc(utils.WrapHandlerFunc(a.handleGetAccount))
+	sub.Path("/{address}").Queries("revision", "{revision}").Methods(http.MethodGet).HandlerFunc(utils.WrapHandlerFunc(a.handleGetAccount))
 
 	sub.Path("/{address}/storage").Queries("key", "{key}").Methods("GET").HandlerFunc(utils.WrapHandlerFunc(a.handleGetStorage))
-	sub.Path("/{address}/storage").Queries("key", "{key}", "blockNumber", "{blockNumber}").Methods("GET").HandlerFunc(utils.WrapHandlerFunc(a.handleGetStorage))
+	sub.Path("/{address}/storage").Queries("key", "{key}", "revision", "{revision}").Methods("GET").HandlerFunc(utils.WrapHandlerFunc(a.handleGetStorage))
+
+	sub.Path("/{address}").Methods("POST").HandlerFunc(utils.WrapHandlerFunc(a.handleCallContract))
+	sub.Path("/{address}").Queries("revision", "{revision}").Methods("POST").HandlerFunc(utils.WrapHandlerFunc(a.handleCallContract))
+
+	sub.Path("/{address}/logs").Methods("POST").HandlerFunc(utils.WrapHandlerFunc(a.handleFilterLogs))
 }
--- api/transactions/transactions_test.go
+++ api/transactions/transactions_test.go
@@ -9,79 +9,67 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/vechain/thor/packer"
+	"github.com/vechain/thor/txpool"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/common/math"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 	"github.com/vechain/thor/api/transactions"
-	"github.com/vechain/thor/block"
 	"github.com/vechain/thor/chain"
 	"github.com/vechain/thor/genesis"
 	"github.com/vechain/thor/lvldb"
 	"github.com/vechain/thor/state"
 	"github.com/vechain/thor/thor"
 	"github.com/vechain/thor/tx"
-	"github.com/vechain/thor/txpool"
 )
-
-var testPrivHex = "efa321f290811731036e5eccd373114e5186d9fe419081f5a607231279d5ef01"
 
 func TestTransaction(t *testing.T) {
 
-	ntx, ts := initTransactionServer(t)
-	raw, err := transactions.ConvertTransaction(ntx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	transaction, ts := initTransactionServer(t)
 	defer ts.Close()
-
-	r, err := httpGet(ts, ts.URL+fmt.Sprintf("/transactions/%v", ntx.ID().String()))
+	raw, err := transactions.ConvertTransaction(transaction)
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	res := httpGet(t, ts.URL+fmt.Sprintf("/transactions/%v", transaction.ID()))
 	var rtx *transactions.Transaction
-	if err := json.Unmarshal(r, &rtx); err != nil {
+	if err := json.Unmarshal(res, &rtx); err != nil {
 		t.Fatal(err)
 	}
 	checkTx(t, raw, rtx)
 
-	r, err = httpGet(ts, ts.URL+fmt.Sprintf("/transactions/%v/receipts", ntx.ID().String()))
-	if err != nil {
-		t.Fatal(err)
-	}
+	r := httpGet(t, ts.URL+fmt.Sprintf("/transactions/%v/receipts", transaction.ID().String()))
 	var receipt *transactions.Receipt
 	if err := json.Unmarshal(r, &receipt); err != nil {
 		t.Fatal(err)
 	}
-
-	key, err := crypto.HexToECDSA(testPrivHex)
-	if err != nil {
-		t.Fatal(err)
-	}
-	sig, err := crypto.Sign(ntx.SigningHash().Bytes(), key)
-
+	assert.Equal(t, uint64(receipt.GasUsed), transaction.Gas(), "gas should be equal")
+	sig, err := crypto.Sign(transaction.SigningHash().Bytes(), genesis.Dev.Accounts()[0].PrivateKey)
 	if err != nil {
 		t.Errorf("Sign error: %s", err)
 	}
-	to := thor.BytesToAddress([]byte("acc1"))
-	hash := thor.BytesToHash([]byte("DependsOn"))
+
+	to := thor.BytesToAddress([]byte("to"))
 	v := big.NewInt(10000)
-	m := math.HexOrDecimal256(*v)
-	blockRef := tx.NewBlockRef(20)
+	blockRef := tx.NewBlockRef(0)
 	rawTransaction := &transactions.RawTransaction{
 		Nonce:        1,
+		ChainTag:     transaction.ChainTag(),
 		GasPriceCoef: 1,
-		Gas:          30000,
-		DependsOn:    &hash,
+		Gas:          21000,
 		Sig:          hexutil.Encode(sig),
 		BlockRef:     hexutil.Encode(blockRef[:]),
 		Clauses: transactions.Clauses{
 			transactions.Clause{
 				To:    &to,
-				Value: &m,
-				Data:  hexutil.Encode([]byte{0x00, 0x00}),
+				Value: math.HexOrDecimal256(*v),
+				Data:  hexutil.Encode(nil),
 			},
 		},
 	}
@@ -89,24 +77,26 @@ func TestTransaction(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	r, err = httpPost(ts, ts.URL+"/transactions", txData)
-	if err != nil {
+	res = httpPost(t, ts.URL+"/transactions", txData)
+	var txID string
+	if err = json.Unmarshal(res, &txID); err != nil {
 		t.Fatal(err)
 	}
+	assert.Equal(t, transaction.ID().String(), txID, "shoudl be the same transaction")
 
 }
 
-func httpPost(ts *httptest.Server, url string, data []byte) ([]byte, error) {
+func httpPost(t *testing.T, url string, data []byte) []byte {
 	res, err := http.Post(url, "application/x-www-form-urlencoded", bytes.NewReader(data))
 	if err != nil {
-		return nil, err
+		t.Fatal(err)
 	}
 	r, err := ioutil.ReadAll(res.Body)
 	res.Body.Close()
 	if err != nil {
-		return nil, err
+		t.Fatal(err)
 	}
-	return r, nil
+	return r
 }
 
 func initTransactionServer(t *testing.T) (*tx.Transaction, *httptest.Server) {
@@ -117,47 +107,41 @@ func initTransactionServer(t *testing.T) (*tx.Transaction, *httptest.Server) {
 		t.Fatal(err)
 	}
 	chain, _ := chain.New(db, b)
+	addr := thor.BytesToAddress([]byte("to"))
+	cla := tx.NewClause(&addr).WithValue(big.NewInt(10000))
+	tx := new(tx.Builder).
+		ChainTag(chain.Tag()).
+		GasPriceCoef(1).
+		Gas(21000).
+		Nonce(1).
+		Clause(cla).
+		BlockRef(tx.NewBlockRef(0)).
+		Build()
 
+	sig, err := crypto.Sign(tx.SigningHash().Bytes(), genesis.Dev.Accounts()[0].PrivateKey)
+	if err != nil {
+		t.Errorf("Sign error: %s", err)
+	}
+	tx = tx.WithSignature(sig)
+	pack := packer.New(chain, stateC, genesis.Dev.Accounts()[0].Address, genesis.Dev.Accounts()[0].Address)
+	_, adopt, commit, err := pack.Prepare(b.Header(), uint64(time.Now().Unix()))
+	err = adopt(tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	b, receipts, err := commit(genesis.Dev.Accounts()[0].PrivateKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := chain.AddBlock(b, receipts, true); err != nil {
+		t.Fatal(err)
+	}
 	router := mux.NewRouter()
 	pool := txpool.New(chain, stateC)
 	defer pool.Stop()
 	transactions.New(chain, pool).Mount(router, "/transactions")
 	ts := httptest.NewServer(router)
-
-	addr := thor.BytesToAddress([]byte("to"))
-	cla := tx.NewClause(&addr).WithValue(big.NewInt(1000))
-	tx := new(tx.Builder).
-		ChainTag(0).
-		GasPriceCoef(1).
-		Gas(1000).
-		Nonce(1).
-		Clause(cla).
-		BlockRef(tx.NewBlockRef(0)).
-		Build()
-	key, err := crypto.HexToECDSA(testPrivHex)
-	if err != nil {
-		t.Fatal(err)
-	}
-	sig, err := crypto.Sign(tx.SigningHash().Bytes(), key)
-	if err != nil {
-		t.Errorf("Sign error: %s", err)
-	}
-	tx = tx.WithSignature(sig)
-
-	best, _ := chain.GetBestBlock()
-	bl := new(block.Builder).
-		ParentID(best.Header().ID()).
-		Transaction(tx).
-		Build()
-	stat, err := state.New(bl.Header().StateRoot(), db)
-	if err != nil {
-		t.Fatal(err)
-	}
-	stat.SetBalance(thor.BytesToAddress([]byte("acc1")), big.NewInt(10000000000000))
-	stat.Stage().Commit()
-	if _, err := chain.AddBlock(bl, nil, true); err != nil {
-		t.Fatal(err)
-	}
 	return tx, ts
 }
 
@@ -175,15 +159,15 @@ func checkTx(t *testing.T, expectedTx *transactions.Transaction, actualTx *trans
 
 }
 
-func httpGet(ts *httptest.Server, url string) ([]byte, error) {
+func httpGet(t *testing.T, url string) []byte {
 	res, err := http.Get(url)
 	if err != nil {
-		return nil, err
+		t.Fatal(err)
 	}
 	r, err := ioutil.ReadAll(res.Body)
 	res.Body.Close()
 	if err != nil {
-		return nil, err
+		t.Fatal(err)
 	}
-	return r, nil
+	return r
 }
