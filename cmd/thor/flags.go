1d1a973956bf3ba0a03ec291ae747347d9d0d470
Han Xiao <hanxiao@vechain.com>
2018-04-12 17:11:36 +0800

    refactor(thor.main): 抽取 flags

---
+++ cmd/thor/flags.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/inconshreveable/log15"
+	cli "gopkg.in/urfave/cli.v1"
+)
+
+var appFlags = []cli.Flag{
+	cli.StringFlag{
+		Name:  "p2paddr",
+		Value: ":11235",
+		Usage: "p2p listen addr",
+	},
+	cli.StringFlag{
+		Name:  "apiaddr",
+		Value: "127.0.0.1:8669",
+		Usage: "restful addr",
+	},
+	cli.StringFlag{
+		Name:  "datadir",
+		Value: "/tmp/thor-data",
+		Usage: "chain data path",
+	},
+	cli.IntFlag{
+		Name:  "verbosity",
+		Value: int(log15.LvlInfo),
+		Usage: "log verbosity (0-9)",
+	},
+	cli.BoolFlag{
+		Name:  "devnet",
+		Usage: "develop network",
+	},
+	cli.StringFlag{
+		Name:  "beneficiary",
+		Usage: "address of beneficiary",
+	},
+	cli.IntFlag{
+		Name:  "maxpeers",
+		Usage: "maximum number of network peers (network disabled if set to 0)",
+		Value: 10,
+	},
+}
