bd638b46d127edb405d66a487e8850fd2991d2ce
dinn <dinn.ding@vechain.com>
2018-04-13 16:04:03 +0800

    test(txpool): refator txpool test

--- txpool/txpool_test.go
+++ txpool/txpool_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/vechain/thor/block"
 	"github.com/vechain/thor/chain"
@@ -24,35 +26,37 @@ const (
 )
 
 func TestTxPool(t *testing.T) {
-	db, _ := lvldb.NewMem()
-	c := state.NewCreator(db)
-	bl, _, err := genesis.Dev.Build(c)
-	if err != nil {
-		t.Fatal(err)
-	}
-	chain, _ := chain.New(db, bl)
-	best, _ := chain.GetBestBlock()
-	blk := new(block.Builder).
-		ParentID(best.Header().ID()).
-		StateRoot(best.Header().StateRoot()).
-		Build()
-	if _, err := chain.AddBlock(blk, nil, true); err != nil {
-		t.Fatal(err)
-	}
-	address, _ := thor.ParseAddress(testAddress)
-	pool := txpool.New(chain, c)
+	pool := initPool(t)
+	defer pool.Stop()
 	count := 10
+	addTx(t, pool, count)
+	pending(t, pool, count)
+	dump(t, pool, count)
+}
+
+func pending(t *testing.T, pool *txpool.TxPool, count int) {
+	txs := pool.Pending()
+	assert.Equal(t, len(txs), count)
+}
+
+func dump(t *testing.T, pool *txpool.TxPool, count int) {
+	txs := pool.Dump()
+	assert.Equal(t, len(txs), count)
+}
+
+func addTx(t *testing.T, pool *txpool.TxPool, count int) {
+
 	ch := make(chan *tx.Transaction, count)
 	sub := pool.SubscribeNewTransaction(ch)
 	defer sub.Unsubscribe()
-	dependsOn := thor.BytesToBytes32([]byte("depend"))
+	address := thor.BytesToAddress([]byte("addr"))
 	for i := 0; i < count; i++ {
 		cla := tx.NewClause(&address).WithValue(big.NewInt(10 + int64(i))).WithData(nil)
 		tx := new(tx.Builder).
 			GasPriceCoef(1).
 			Gas(1000000).
+			Expiration(100).
 			Clause(cla).
-			DependsOn(&dependsOn).
 			Nonce(1).
 			Build()
 		key, err := crypto.HexToECDSA(testPrivHex)
@@ -61,7 +65,7 @@ func TestTxPool(t *testing.T) {
 		}
 		sig, err := crypto.Sign(tx.SigningHash().Bytes(), key)
 		if err != nil {
-			t.Errorf("Sign error: %s", err)
+			t.Fatal(err)
 		}
 		tx = tx.WithSignature(sig)
 		if err := pool.Add(tx); err != nil {
@@ -74,7 +78,29 @@ func TestTxPool(t *testing.T) {
 		case t1 := <-ch:
 			fmt.Println(i, t1)
 		case <-time.After(time.Second):
-			fmt.Println("event not fired", i)
+			t.Errorf("event not fired")
 		}
 	}
+}
+func initPool(t *testing.T) *txpool.TxPool {
+	db, _ := lvldb.NewMem()
+	stateC := state.NewCreator(db)
+	gen, err := genesis.NewDevnet()
+	if err != nil {
+		t.Fatal(err)
+	}
+	b, _, err := gen.Build(stateC)
+	if err != nil {
+		t.Fatal(err)
+	}
+	c, _ := chain.New(db, b)
+	best, _ := c.GetBestBlock()
+	blk := new(block.Builder).
+		ParentID(best.Header().ID()).
+		StateRoot(best.Header().StateRoot()).
+		Build()
+	if _, err := c.AddBlock(blk, nil, true); err != nil {
+		t.Fatal(err)
+	}
+	return txpool.New(c, stateC)
 }
