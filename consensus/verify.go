857b9273799ed4079117a2b36a7db7c36c800b6e
Han Xiao <hanxiao@vechain.com>
2018-04-23 20:08:34 +0800

    refactor(consensus): 修改 DependsOn 的逻辑为依赖交易执行结果

--- consensus/verify.go
+++ consensus/verify.go
@@ -10,7 +10,10 @@ import (
 )
 
 func (c *Consensus) verifyBlock(blk *block.Block, state *state.State) (*state.Stage, tx.Receipts, error) {
-
+	var totalGasUsed uint64
+	txs := blk.Transactions()
+	receipts := make(tx.Receipts, 0, len(txs))
+	passedTxs := make(map[thor.Bytes32]struct{})
 	header := blk.Header()
 	traverser := c.chain.NewTraverser(blk.Header().ParentID())
 	rt := runtime.New(state,
@@ -19,19 +22,39 @@ func (c *Consensus) verifyBlock(blk *block.Block, state *state.State) (*state.St
 		header.Timestamp(),
 		header.GasLimit(),
 		func(num uint32) thor.Bytes32 { return traverser.Get(num).ID() })
-	var (
-		txs          = blk.Transactions()
-		totalGasUsed uint64
-		receipts     = make(tx.Receipts, 0, len(txs))
-	)
 
 	for _, tx := range txs {
+		// check depended tx
+		if dep := tx.DependsOn(); dep != nil {
+			found, isReverted, err := c.hasTx(passedTxs, header.ParentID(), *dep)
+			if err != nil {
+				return nil, nil, err
+			}
+			if !found {
+				return nil, nil, errors.New("bad tx: dep not found")
+			}
+
+			reverted, err := isReverted()
+			if err != nil {
+				return nil, nil, err
+			}
+			if reverted {
+				return nil, nil, errors.New("bad tx: dep reverted")
+			}
+
+			passedTxs[*dep] = struct{}{} // cache
+		}
+
 		receipt, _, _, err := rt.ExecuteTransaction(tx)
 		if err != nil {
 			return nil, nil, err
 		}
+
 		totalGasUsed += receipt.GasUsed
 		receipts = append(receipts, receipt)
+		if !receipt.Reverted {
+			passedTxs[tx.ID()] = struct{}{}
+		}
 	}
 
 	if header.GasUsed() != totalGasUsed {
