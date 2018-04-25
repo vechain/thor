a6a63c867254004366c9f3e814387cee59a5ddf1
Qian Bin <cola.tin.com@gmail.com>
2018-04-25 23:39:40 +0800

    feat(api): add swagger-ui handler

--- api/api.go
+++ api/api.go
@@ -1,14 +1,13 @@
 package api
 
-//go:generate go-bindata -nometadata -pkg gen -o gen/bindata.go swagger-ui/... thor.yaml
-
 import (
-	"fmt"
 	"net/http"
 
+	assetfs "github.com/elazarl/go-bindata-assetfs"
 	"github.com/gorilla/mux"
 	"github.com/vechain/thor/api/accounts"
 	"github.com/vechain/thor/api/blocks"
+	"github.com/vechain/thor/api/doc"
 	"github.com/vechain/thor/api/logs"
 	"github.com/vechain/thor/api/node"
 	"github.com/vechain/thor/api/transactions"
@@ -23,21 +22,37 @@ import (
 //New return api router
 func New(chain *chain.Chain, stateCreator *state.Creator, txPool *txpool.TxPool, logDB *logdb.LogDB, transferDB *transferdb.TransferDB, nw node.Network) http.HandlerFunc {
 	router := mux.NewRouter()
-	serveStatic(router, "/static")
-	accounts.New(chain, stateCreator).Mount(router, "/accounts")
-	logs.New(logDB).Mount(router, "/logs")
-	transfers.New(transferDB).Mount(router, "/transfers")
-	blocks.New(chain).Mount(router, "/blocks")
-	transactions.New(chain, txPool, transferDB).Mount(router, "/transactions")
-	node.New(nw).Mount(router, "/node")
+
+	// to serve api doc and swagger-ui
+	router.PathPrefix("/doc").Handler(
+		http.StripPrefix("/doc/", http.FileServer(
+			&assetfs.AssetFS{
+				Asset:     doc.Asset,
+				AssetDir:  doc.AssetDir,
+				AssetInfo: doc.AssetInfo})))
+
+	// redirect swagger-ui
+	router.Path("/").HandlerFunc(
+		func(w http.ResponseWriter, req *http.Request) {
+			http.Redirect(w, req, "doc/swagger-ui/", http.StatusTemporaryRedirect)
+		})
+
+	accounts.New(chain, stateCreator).
+		Mount(router, "/accounts")
+	logs.New(logDB).
+		Mount(router, "/logs")
+	transfers.New(transferDB).
+		Mount(router, "/transfers")
+	blocks.New(chain).
+		Mount(router, "/blocks")
+	transactions.New(chain, txPool, transferDB).
+		Mount(router, "/transactions")
+	node.New(nw).
+		Mount(router, "/node")
+
 	return func(w http.ResponseWriter, req *http.Request) {
+		// TODO make CORS setting according to cmd flags
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		router.ServeHTTP(w, req)
-		fmt.Println(w)
 	}
-}
-
-func serveStatic(root *mux.Router, pathPrefix string) {
-	sub := root.PathPrefix(pathPrefix).Subrouter()
-	sub.Path("/static").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(""))))
 }
