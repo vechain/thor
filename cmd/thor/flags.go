f493120a89abc366a23c333b0a0ef944fe2c9815
Qian Bin <cola.tin.com@gmail.com>
2018-05-04 14:33:08 +0800

    feat(thor-cli): add nat flag; rename some flags

--- cmd/thor/flags.go
+++ cmd/thor/flags.go
@@ -6,31 +6,10 @@ import (
 )
 
 var (
-	p2pPortFlag = cli.IntFlag{
-		Name:  "p2pport",
-		Value: 11235,
-		Usage: "P2P network listening port",
-	}
-	apiAddrFlag = cli.StringFlag{
-		Name:  "apiaddr",
-		Value: "localhost:8669",
-		Usage: "API service listening address",
-	}
-	apiCorsFlag = cli.StringFlag{
-		Name:  "apicors",
-		Value: "",
-		Usage: "Comma separated list of domains from which to accept cross origin requests to API",
-	}
 	dirFlag = cli.StringFlag{
 		Name:  "dir",
 		Value: defaultMainDir(),
 		Usage: "Main directory for configs and databases",
-	}
-
-	verbosityFlag = cli.IntFlag{
-		Name:  "verbosity",
-		Value: int(log15.LvlInfo),
-		Usage: "log verbosity (0-9)",
 	}
 	devFlag = cli.BoolFlag{
 		Name:  "dev",
@@ -40,9 +19,35 @@ var (
 		Name:  "beneficiary",
 		Usage: "address for block rewards",
 	}
+	apiAddrFlag = cli.StringFlag{
+		Name:  "api-addr",
+		Value: "localhost:8669",
+		Usage: "API service listening address",
+	}
+	apiCorsFlag = cli.StringFlag{
+		Name:  "api-cors",
+		Value: "",
+		Usage: "Comma separated list of domains from which to accept cross origin requests to API",
+	}
+	verbosityFlag = cli.IntFlag{
+		Name:  "verbosity",
+		Value: int(log15.LvlInfo),
+		Usage: "log verbosity (0-9)",
+	}
+
 	maxPeersFlag = cli.IntFlag{
-		Name:  "maxpeers",
+		Name:  "max-peers",
 		Usage: "maximum number of P2P network peers (P2P network disabled if set to 0)",
 		Value: 10,
+	}
+	p2pPortFlag = cli.IntFlag{
+		Name:  "p2p-port",
+		Value: 11235,
+		Usage: "P2P network listening port",
+	}
+	natFlag = cli.StringFlag{
+		Name:  "nat",
+		Value: "none",
+		Usage: "port mapping mechanism (any|none|upnp|pmp|extip:<IP>)",
 	}
 )
