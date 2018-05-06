902d96b17eced2aae2f2941e6555e75b5622eb59
cola tin <cola.tin.com@gmail.com>
2018-05-06 16:40:51 +0800

    feat(genesis): add testnet genesis

--- genesis/testnet.go
+++ genesis/testnet.go
@@ -1,4 +1,56 @@
 package genesis
 
-// func NewTestnet() {
-// }
+import (
+	"math/big"
+
+	"github.com/vechain/thor/builtin"
+	"github.com/vechain/thor/state"
+	"github.com/vechain/thor/thor"
+	"github.com/vechain/thor/tx"
+	"github.com/vechain/thor/vm/evm"
+)
+
+// NetTestnet create genesis for testnet.
+func NewTestnet() (*Genesis, error) {
+	launchTime := uint64(1526054400000)                                // Sat May 12 2018 00:00:00 GMT+0800 (CST)
+	tokenSupply := new(big.Int).Mul(big.NewInt(1e9), big.NewInt(1e18)) // VET 1 billion
+	acccount0, _ := thor.ParseAddress("0xe59d475abe695c7f67a8a2321f33a856b0b4c71d")
+
+	builder := new(Builder).
+		Timestamp(launchTime).
+		GasLimit(thor.InitialGasLimit).
+		State(func(state *state.State) error {
+			// alloc precompiled contracts
+			for addr := range evm.PrecompiledContractsByzantium {
+				state.SetCode(thor.Address(addr), emptyRuntimeBytecode)
+			}
+			state.SetCode(builtin.Authority.Address, builtin.Authority.RuntimeBytecodes())
+			state.SetCode(builtin.Energy.Address, builtin.Energy.RuntimeBytecodes())
+			state.SetCode(builtin.Params.Address, builtin.Params.RuntimeBytecodes())
+			state.SetCode(builtin.Prototype.Address, builtin.Prototype.RuntimeBytecodes())
+			state.SetCode(builtin.Executor.Address, builtin.Executor.RuntimeBytecodes())
+			state.SetCode(builtin.Extension.Address, builtin.Extension.RuntimeBytecodes())
+
+			builtin.Energy.Native(state).SetInitialSupply(tokenSupply, &big.Int{})
+			state.SetBalance(acccount0, tokenSupply)
+			return nil
+		}).
+		Call(
+			tx.NewClause(&builtin.Params.Address).
+				WithData(mustEncodeInput(builtin.Params.ABI, "set", thor.KeyRewardRatio, thor.InitialRewardRatio)),
+			builtin.Executor.Address).
+		Call(
+			tx.NewClause(&builtin.Params.Address).
+				WithData(mustEncodeInput(builtin.Params.ABI, "set", thor.KeyBaseGasPrice, thor.InitialBaseGasPrice)),
+			builtin.Executor.Address).
+		Call(
+			tx.NewClause(&builtin.Params.Address).
+				WithData(mustEncodeInput(builtin.Params.ABI, "set", thor.KeyProposerEndorsement, thor.InitialProposerEndorsement)),
+			builtin.Executor.Address)
+
+	id, err := builder.ComputeID()
+	if err != nil {
+		return nil, err
+	}
+	return &Genesis{builder, id, "testnet"}, nil
+}
