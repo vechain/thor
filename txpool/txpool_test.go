f66e09cbe0b354c3000b6c39b194845cd2bcd6e0
Han Xiao <hanxiao@vechain.com>
2018-05-07 20:47:34 +0800

    test(txpool): fix txpool_test

--- txpool/txpool_test.go
+++ txpool/txpool_test.go
@@ -6,9 +6,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/stretchr/testify/assert"
 	"github.com/vechain/thor/block"
 	"github.com/vechain/thor/chain"
 	"github.com/vechain/thor/genesis"
