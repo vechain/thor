fbb3f9887c010d956679a3e332e5f7e094e867ea
cola tin <cola.tin.com@gmail.com>
2018-05-08 12:22:26 +0800

    feat(co): specilize the condition that cpu num is 1

--- co/parallel.go
+++ co/parallel.go
@@ -4,16 +4,24 @@ import (
 	"runtime"
 )
 
+var numCPU = runtime.NumCPU()
+
 // Enqueue function to enqueue parallel works.
 type Enqueue func(work func())
 
 // Parallel to run a batch of work using as many CPU as it can.
 func Parallel(cb func(Enqueue)) {
+	if numCPU < 2 {
+		cb(func(work func()) {
+			work()
+		})
+	}
+
 	var goes Goes
 	defer goes.Wait()
-	ch := make(chan func(), runtime.NumCPU()*2)
+	ch := make(chan func(), numCPU*2)
 	defer close(ch)
-	for i := 0; i < runtime.NumCPU(); i++ {
+	for i := 0; i < numCPU; i++ {
 		goes.Go(func() {
 			for {
 				select {
