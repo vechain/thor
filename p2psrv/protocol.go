520e7947d198fc5fbc7768047d52c75562559d39
cola tin <cola.tin.com@gmail.com>
2018-05-11 10:39:11 +0800

    refactor(p2psrv): simplify p2p servre; remoev request-reponse style peer

--- p2psrv/protocol.go
+++ p2psrv/protocol.go
@@ -1,21 +1,10 @@
 package p2psrv
 
+import "github.com/ethereum/go-ethereum/p2p"
+
 // Protocol represents a P2P subprotocol implementation.
 type Protocol struct {
-	// Name should contain the official protocol name,
-	// often a three-letter word.
-	Name string
-
-	// Version should contain the version number of the protocol.
-	Version uint32
-
-	// Length should contain the number of message codes used
-	// by the protocol.
-	Length uint64
-
-	MaxMsgSize uint32
+	p2p.Protocol
 
 	DiscTopic string
-
-	HandlePeer func(peer *Peer) HandleRequest
 }
