8835c2109781158887229b4153ba123219ef6273
linglong <linglong.yan@vechain.com>
2018-05-16 18:41:39 +0800

    add block test case

--- block/block_test.go
+++ block/block_test.go
@@ -3,7 +3,11 @@ package block_test
 import (
 	"fmt"
 	"testing"
+	"time"
 
+	"github.com/stretchr/testify/assert"
+
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/rlp"
 	. "github.com/vechain/thor/block"
 	"github.com/vechain/thor/thor"
@@ -14,21 +18,65 @@ func TestBlock(t *testing.T) {
 
 	tx1 := new(tx.Builder).Clause(tx.NewClause(&thor.Address{})).Clause(tx.NewClause(&thor.Address{})).Build()
 	tx2 := new(tx.Builder).Clause(tx.NewClause(nil)).Build()
+
+	privKey := string("dce1443bd2ef0c2631adc1c67e5c93f13dc23a41c18b536effbbdcbcdb96fb65")
+
+	now := uint64(time.Now().UnixNano())
+
+	var (
+		gasUsed     uint64       = 1000
+		gasLimit    uint64       = 14000
+		totalScore  uint64       = 101
+		emptyRoot   thor.Bytes32 = thor.BytesToBytes32([]byte("0"))
+		beneficiary thor.Address = thor.BytesToAddress([]byte("abc"))
+	)
+
 	block := new(Builder).
-		GasUsed(1000).
+		GasUsed(gasUsed).
 		Transaction(tx1).
 		Transaction(tx2).
+		GasLimit(gasLimit).
+		TotalScore(totalScore).
+		StateRoot(emptyRoot).
+		ReceiptsRoot(emptyRoot).
+		Timestamp(now).
+		ParentID(emptyRoot).
+		Beneficiary(beneficiary).
 		Build()
+
 	h := block.Header()
+
+	txs := block.Transactions()
+	body := block.Body()
+	txsRootHash := txs.RootHash()
+
 	fmt.Println(h.ID())
+
+	assert.Equal(t, body.Txs, txs)
+	assert.Equal(t, Compose(h, txs), block)
+	assert.Equal(t, gasLimit, h.GasLimit())
+	assert.Equal(t, gasUsed, h.GasUsed())
+	assert.Equal(t, totalScore, h.TotalScore())
+	assert.Equal(t, emptyRoot, h.StateRoot())
+	assert.Equal(t, emptyRoot, h.ReceiptsRoot())
+	assert.Equal(t, now, h.Timestamp())
+	assert.Equal(t, emptyRoot, h.ParentID())
+	assert.Equal(t, beneficiary, h.Beneficiary())
+	assert.Equal(t, txsRootHash, h.TxsRoot())
+
+	key, _ := crypto.HexToECDSA(privKey)
+	sig, _ := crypto.Sign(block.Header().SigningHash().Bytes(), key)
+
+	block = block.WithSignature(sig)
 
 	data, _ := rlp.EncodeToBytes(block)
 	fmt.Println(Raw(data).DecodeHeader())
 	fmt.Println(Raw(data).DecodeBody())
 
+	fmt.Println(block.Size())
+
 	b := Block{}
 	rlp.DecodeBytes(data, &b)
 	fmt.Println(b.Header().ID())
-	fmt.Println(&b)
 	fmt.Println(&b)
 }
