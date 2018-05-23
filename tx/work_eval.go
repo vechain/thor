271e6f9f198dcf952332ada6c4f947d9b1168bed
Qian Bin <cola.tin.com@gmail.com>
2018-05-23 23:15:20 +0800

    refactor(tx): increase work per gas to 1000

--- tx/work_eval.go
+++ tx/work_eval.go
@@ -13,7 +13,7 @@ import (
 )
 
 var (
-	workPerGas = big.NewInt(100)
+	workPerGas = big.NewInt(1000)
 	big100     = big.NewInt(100)
 	big104     = big.NewInt(104) // Moore's law monthly rate (percentage)
 	bigE18     = big.NewInt(1e18)
