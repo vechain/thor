a31cf170cc96e690e3d9141d030a390b81286dec
Qian Bin <cola.tin.com@gmail.com>
2018-06-06 00:40:04 +0800

    refactor(genesis): redefine testnet and devnet genesis; remove mainnet

--- genesis/genesis_test.go
+++ genesis/genesis_test.go
@@ -6,45 +6,22 @@
 package genesis_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/vechain/thor/builtin"
 	"github.com/vechain/thor/genesis"
 	"github.com/vechain/thor/lvldb"
 	"github.com/vechain/thor/state"
 )
 
-func TestGenesis(t *testing.T) {
-	assert := assert.New(t)
+func TestTestnetGenesis(t *testing.T) {
 	kv, _ := lvldb.NewMem()
-	defer kv.Close()
+	gene, err := genesis.NewTestnet()
+	assert.Nil(t, err)
 
-	g, err := genesis.NewMainnet()
-	if err != nil {
-		t.Fatal(err)
-	}
+	b0, _, err := gene.Build(state.NewCreator(kv))
+	assert.Nil(t, err)
 
-	b0, _, err := g.Build(state.NewCreator(kv))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	st, _ := state.New(b0.Header().StateRoot(), kv)
-	assert.True(len(st.GetCode(builtin.Authority.Address)) > 0)
-}
-func TestDevGenesis(t *testing.T) {
-	assert := assert.New(t)
-	kv, _ := lvldb.NewMem()
-	defer kv.Close()
-	g, err := genesis.NewDevnet()
-	if err != nil {
-		t.Fatal(err)
-	}
-	b0, logs, err := g.Build(state.NewCreator(kv))
-
-	st, _ := state.New(b0.Header().StateRoot(), kv)
-	assert.True(len(st.GetCode(builtin.Authority.Address)) > 0)
-	fmt.Println(b0, logs)
+	_, err = state.New(b0.Header().StateRoot(), kv)
+	assert.Nil(t, err)
 }
