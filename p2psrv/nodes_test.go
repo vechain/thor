9668109b8eaf6c354b71c6a4288e36bf814184c6
hariexcel <hari.excel@gmail.com>
2018-07-13 09:09:45 +0530

    Nodes_test file added for p2p

---
+++ p2psrv/nodes_test.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2018 The VeChainThor developers
+
+// Distributed under the GNU Lesser General Public License v3.0 software license, see the accompanying
+// file LICENSE or <https://www.gnu.org/licenses/lgpl-3.0.html>
+
+package p2psrv_test
