6b5f421770fd3413a435db00de8c9352b7d551dd
dinn <dinn.ding@vechain.com>
2018-12-11 17:26:04 +0800

    feat: export doc version

--- api/doc/doc.go
+++ api/doc/doc.go
@@ -6,3 +6,28 @@
 package doc
 
 //go:generate go-bindata -nometadata -ignore=.DS_Store -pkg doc -o bindata.go swagger-ui/... thor.yaml
+
+import (
+	yaml "gopkg.in/yaml.v2"
+)
+
+//Version open api version
+func Version() string {
+	return version
+}
+
+var version string
+
+type openAPIInfo struct {
+	Info struct {
+		Version string
+	}
+}
+
+func init() {
+	var oai openAPIInfo
+	if err := yaml.Unmarshal(MustAsset("thor.yaml"), &oai); err != nil {
+		panic(err)
+	}
+	version = oai.Info.Version
+}
