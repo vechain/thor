0a0ff615464ca4fd5759c50cc2ac2c71737200f9
qianbin <cola.tin.com@gmail.com>
2019-02-12 13:20:36 +0800

    fix: 'database is locked' on querying sqlite

--- logdb/logdb.go
+++ logdb/logdb.go
@@ -34,6 +34,10 @@ func New(path string) (logDB *LogDB, err error) {
 			db.Close()
 		}
 	}()
+
+	// to avoid 'database is locked' error
+	db.SetMaxOpenConns(1)
+
 	if _, err := db.Exec(eventTableSchema + transferTableSchema); err != nil {
 		return nil, err
 	}
