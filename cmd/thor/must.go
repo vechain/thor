3f5b14fd481aa16f456d307681d75551d6d6c72f
tony <tony.li@vechain.com>
2019-05-20 11:10:12 +0800

    customnet: add forkconfig support

--- cmd/thor/must.go
+++ cmd/thor/must.go
@@ -48,15 +48,17 @@ func initLogger(ctx *cli.Context) {
 	ethlog.Root().SetHandler(ethLogHandler)
 }
 
-func selectGenesis(ctx *cli.Context) *genesis.Genesis {
+func selectGenesis(ctx *cli.Context) (*genesis.Genesis, thor.ForkConfig) {
 	network := ctx.String(networkFlag.Name)
 
 	if network != "" {
 		switch network {
 		case "test":
-			return genesis.NewTestnet()
+			gene := genesis.NewTestnet()
+			return gene, thor.GetForkConfig(gene.ID())
 		case "main":
-			return genesis.NewMainnet()
+			gene := genesis.NewMainnet()
+			return gene, thor.GetForkConfig(gene.ID())
 		default:
 			file, err := os.Open(network)
 			if err != nil {
@@ -77,14 +79,14 @@ func selectGenesis(ctx *cli.Context) *genesis.Genesis {
 				fatal(fmt.Sprintf("build genesis: %v", err))
 			}
 
-			return customGen
+			return customGen, gen.ForkConfig
 		}
 	}
 
 	cli.ShowAppHelp(ctx)
 	fmt.Println("network flag not specified")
 	os.Exit(1)
-	return nil
+	return nil, thor.ForkConfig{}
 }
 
 func makeConfigDir(ctx *cli.Context) string {
@@ -323,6 +325,7 @@ func printStartupMessage1(
 	chain *chain.Chain,
 	master *node.Master,
 	dataDir string,
+	forkConfig thor.ForkConfig,
 ) {
 	bestBlock := chain.BestBlock()
 
@@ -337,7 +340,7 @@ func printStartupMessage1(
 		common.MakeName("Thor", fullVersion()),
 		gene.ID(), gene.Name(),
 		bestBlock.Header().ID(), bestBlock.Header().Number(), time.Unix(int64(bestBlock.Header().Timestamp()), 0),
-		thor.GetForkConfig(gene.ID()),
+		forkConfig,
 		master.Address(),
 		func() string {
 			if master.Beneficiary == nil {
@@ -380,6 +383,7 @@ func printSoloStartupMessage(
 	chain *chain.Chain,
 	dataDir string,
 	apiURL string,
+	forkConfig thor.ForkConfig,
 ) {
 	tableHead := `
 ┌────────────────────────────────────────────┬────────────────────────────────────────────────────────────────────┐
@@ -401,7 +405,7 @@ func printSoloStartupMessage(
 		common.MakeName("Thor solo", fullVersion()),
 		gene.ID(), gene.Name(),
 		bestBlock.Header().ID(), bestBlock.Header().Number(), time.Unix(int64(bestBlock.Header().Timestamp()), 0),
-		thor.GetForkConfig(gene.ID()),
+		forkConfig,
 		dataDir,
 		apiURL)
 
