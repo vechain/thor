3366e76a5760831ff0c9942100ab51e47fdb98c2
qianbin <cola.tin.com@gmail.com>
2019-12-28 09:29:41 +0800

    logdb: re-design table schemas
    - add ref table to map id <-> data which significantly reduces disk space useage
    - optmize indexes to cover most cases
    - remove time range query options
    - add log Writer

--- logdb/logdb.go
+++ logdb/logdb.go
@@ -9,6 +9,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"math"
 	"math/big"
 
 	sqlite3 "github.com/mattn/go-sqlite3"
@@ -18,7 +19,10 @@ import (
 )
 
 // the key to last written block id.
-var configBlockIDKey = "blockID"
+const (
+	configBlockIDKey = "blockID"
+	refIDQuery       = "(SELECT id FROM ref WHERE data=?)"
+)
 
 type LogDB struct {
 	path          string
@@ -33,15 +37,12 @@ func New(path string) (logDB *LogDB, err error) {
 		return nil, err
 	}
 	defer func() {
-		if logDB == nil {
-			db.Close()
+		if err != nil {
+			_ = db.Close()
 		}
 	}()
 
-	// to avoid 'database is locked' error
-	db.SetMaxOpenConns(1)
-
-	if _, err := db.Exec(configTableSchema + eventTableSchema + transferTableSchema); err != nil {
+	if _, err := db.Exec(configTableSchema + refTableScheme + eventTableSchema + transferTableSchema); err != nil {
 		return nil, err
 	}
 
@@ -59,134 +60,130 @@ func NewMem() (*LogDB, error) {
 }
 
 // Close close the log db.
-func (db *LogDB) Close() {
-	db.db.Close()
+func (db *LogDB) Close() error {
+	return db.db.Close()
 }
 
 func (db *LogDB) Path() string {
 	return db.path
 }
 
-// NewTask create a new task to perform transactional operations of
-// writing logs.
-func (db *LogDB) NewTask() *Task {
-	return &Task{db: db.db}
-}
-
 func (db *LogDB) FilterEvents(ctx context.Context, filter *EventFilter) ([]*Event, error) {
+
+	const query = `SELECT e.seq, r0.data, e.blockTime, r1.data, r2.data, e.clauseIndex, r3.data, r4.data, r5.data, r6.data, r7.data, r8.data, e.data
+FROM (%v) e
+	LEFT JOIN ref r0 ON e.blockID = r0.id
+	LEFT JOIN ref r1 ON e.txID = r1.id
+	LEFT JOIN ref r2 ON e.txOrigin = r2.id
+	LEFT JOIN ref r3 ON e.address = r3.id
+	LEFT JOIN ref r4 ON e.topic0 = r4.id
+	LEFT JOIN ref r5 ON e.topic1 = r5.id
+	LEFT JOIN ref r6 ON e.topic2 = r6.id
+	LEFT JOIN ref r7 ON e.topic3 = r7.id
+	LEFT JOIN ref r8 ON e.topic4 = r8.id`
+
 	if filter == nil {
-		return db.queryEvents(ctx, "SELECT * FROM event")
+		return db.queryEvents(ctx, fmt.Sprintf(query, "event"))
 	}
-	var args []interface{}
-	stmt := "SELECT * FROM event WHERE 1"
-	condition := "blockNumber"
+
+	var (
+		subQuery = "SELECT seq FROM event WHERE 1"
+		args     []interface{}
+	)
+
 	if filter.Range != nil {
-		if filter.Range.Unit == Time {
-			condition = "blockTime"
-		}
-		args = append(args, filter.Range.From)
-		stmt += " AND " + condition + " >= ? "
+		subQuery += " AND seq >= ?"
+		args = append(args, newSequence(filter.Range.From, 0))
 		if filter.Range.To >= filter.Range.From {
-			args = append(args, filter.Range.To)
-			stmt += " AND " + condition + " <= ? "
+			subQuery += " AND seq <= ?"
+			args = append(args, newSequence(filter.Range.To, uint32(math.MaxInt32)))
 		}
 	}
-	if length := len(filter.CriteriaSet); length > 0 {
-		for i, criteria := range filter.CriteriaSet {
-			if i == 0 {
-				stmt += " AND (( 1"
-			} else {
-				stmt += " OR ( 1"
+
+	if len(filter.CriteriaSet) > 0 {
+		subQuery += " AND ("
+
+		for i, c := range filter.CriteriaSet {
+			cond, cargs := c.toWhereCondition()
+			if i > 0 {
+				subQuery += " OR"
 			}
-			if criteria.Address != nil {
-				args = append(args, criteria.Address.Bytes())
-				stmt += " AND address = ? "
-			}
-			for j, topic := range criteria.Topics {
-				if topic != nil {
-					args = append(args, topic.Bytes())
-					stmt += fmt.Sprintf(" AND topic%v = ?", j)
-				}
-			}
-			if i == length-1 {
-				stmt += " )) "
-			} else {
-				stmt += " ) "
-			}
+			subQuery += " (" + cond + ")"
+			args = append(args, cargs...)
 		}
+		subQuery += ")"
 	}
 
 	if filter.Order == DESC {
-		stmt += " ORDER BY blockNumber DESC,eventIndex DESC "
+		subQuery += " ORDER BY seq DESC "
 	} else {
-		stmt += " ORDER BY blockNumber ASC,eventIndex ASC "
+		subQuery += " ORDER BY seq ASC "
 	}
 
 	if filter.Options != nil {
-		stmt += " limit ?, ? "
+		subQuery += " LIMIT ?, ?"
 		args = append(args, filter.Options.Offset, filter.Options.Limit)
 	}
-	return db.queryEvents(ctx, stmt, args...)
+
+	subQuery = "SELECT e.* FROM (" + subQuery + ") s LEFT JOIN event e ON s.seq = e.seq"
+
+	return db.queryEvents(ctx, fmt.Sprintf(query, subQuery), args...)
 }
 
 func (db *LogDB) FilterTransfers(ctx context.Context, filter *TransferFilter) ([]*Transfer, error) {
+
+	const query = `SELECT t.seq, r0.data, t.blockTime, r1.data, r2.data, t.clauseIndex, r3.data, r4.data, t.amount
+FROM (%v) t 
+	LEFT JOIN ref r0 ON t.blockID = r0.id
+	LEFT JOIN ref r1 ON t.txID = r1.id
+	LEFT JOIN ref r2 ON t.txOrigin = r2.id
+	LEFT JOIN ref r3 ON t.sender = r3.id
+	LEFT JOIN ref r4 ON t.recipient = r4.id`
+
 	if filter == nil {
-		return db.queryTransfers(ctx, "SELECT * FROM transfer")
+		return db.queryTransfers(ctx, fmt.Sprintf(query, "transfer"))
 	}
-	var args []interface{}
-	stmt := "SELECT * FROM transfer WHERE 1"
-	condition := "blockNumber"
+
+	var (
+		subQuery = "SELECT seq FROM transfer WHERE 1"
+		args     []interface{}
+	)
+
 	if filter.Range != nil {
-		if filter.Range.Unit == Time {
-			condition = "blockTime"
-		}
-		args = append(args, filter.Range.From)
-		stmt += " AND " + condition + " >= ? "
+		subQuery += " AND seq >= ?"
+		args = append(args, newSequence(filter.Range.From, 0))
 		if filter.Range.To >= filter.Range.From {
-			args = append(args, filter.Range.To)
-			stmt += " AND " + condition + " <= ? "
+			subQuery += " AND seq <= ?"
+			args = append(args, newSequence(filter.Range.To, uint32(math.MaxInt32)))
 		}
 	}
-	if filter.TxID != nil {
-		args = append(args, filter.TxID.Bytes())
-		stmt += " AND txID = ? "
-	}
-	if length := len(filter.CriteriaSet); length > 0 {
-		for i, criteria := range filter.CriteriaSet {
-			if i == 0 {
-				stmt += " AND (( 1 "
-			} else {
-				stmt += " OR ( 1 "
+
+	if len(filter.CriteriaSet) > 0 {
+		subQuery += " AND ("
+		for i, c := range filter.CriteriaSet {
+			cond, cargs := c.toWhereCondition()
+			if i > 0 {
+				subQuery += " OR"
 			}
-			if criteria.TxOrigin != nil {
-				args = append(args, criteria.TxOrigin.Bytes())
-				stmt += " AND txOrigin = ? "
-			}
-			if criteria.Sender != nil {
-				args = append(args, criteria.Sender.Bytes())
-				stmt += " AND sender = ? "
-			}
-			if criteria.Recipient != nil {
-				args = append(args, criteria.Recipient.Bytes())
-				stmt += " AND recipient = ? "
-			}
-			if i == length-1 {
-				stmt += " )) "
-			} else {
-				stmt += " ) "
-			}
+			subQuery += " (" + cond + ")"
+			args = append(args, cargs...)
 		}
+		subQuery += ")"
 	}
+
 	if filter.Order == DESC {
-		stmt += " ORDER BY blockNumber DESC,transferIndex DESC "
+		subQuery += " ORDER BY seq DESC"
 	} else {
-		stmt += " ORDER BY blockNumber ASC,transferIndex ASC "
+		subQuery += " ORDER BY seq ASC"
 	}
+
 	if filter.Options != nil {
-		stmt += " limit ?, ? "
+		subQuery += " LIMIT ?, ?"
 		args = append(args, filter.Options.Offset, filter.Options.Limit)
 	}
-	return db.queryTransfers(ctx, stmt, args...)
+
+	subQuery = "SELECT e.* FROM (" + subQuery + ") s LEFT JOIN transfer e ON s.seq = e.seq"
+	return db.queryTransfers(ctx, fmt.Sprintf(query, subQuery), args...)
 }
 
 func (db *LogDB) queryEvents(ctx context.Context, stmt string, args ...interface{}) ([]*Event, error) {
@@ -194,7 +191,7 @@ func (db *LogDB) queryEvents(ctx context.Context, stmt string, args ...interface
 	if err != nil {
 		return nil, err
 	}
-	defer rows.Close()
+	defer func() { _ = rows.Close() }()
 
 	var events []*Event
 	for rows.Next() {
@@ -204,8 +201,7 @@ func (db *LogDB) queryEvents(ctx context.Context, stmt string, args ...interface
 		default:
 		}
 		var (
-			blockNumber uint32
-			index       uint32
+			seq         sequence
 			blockID     []byte
 			blockTime   uint64
 			txID        []byte
@@ -216,8 +212,7 @@ func (db *LogDB) queryEvents(ctx context.Context, stmt string, args ...interface
 			data        []byte
 		)
 		if err := rows.Scan(
-			&blockNumber,
-			&index,
+			&seq,
 			&blockID,
 			&blockTime,
 			&txID,
@@ -234,8 +229,8 @@ func (db *LogDB) queryEvents(ctx context.Context, stmt string, args ...interface
 			return nil, err
 		}
 		event := &Event{
-			BlockNumber: blockNumber,
-			Index:       index,
+			BlockNumber: seq.BlockNumber(),
+			Index:       seq.Index(),
 			BlockID:     thor.BytesToBytes32(blockID),
 			BlockTime:   blockTime,
 			TxID:        thor.BytesToBytes32(txID),
@@ -263,7 +258,7 @@ func (db *LogDB) queryTransfers(ctx context.Context, stmt string, args ...interf
 	if err != nil {
 		return nil, err
 	}
-	defer rows.Close()
+	defer func() { _ = rows.Close() }()
 	var transfers []*Transfer
 	for rows.Next() {
 		select {
@@ -272,8 +267,7 @@ func (db *LogDB) queryTransfers(ctx context.Context, stmt string, args ...interf
 		default:
 		}
 		var (
-			blockNumber uint32
-			index       uint32
+			seq         sequence
 			blockID     []byte
 			blockTime   uint64
 			txID        []byte
@@ -284,8 +278,7 @@ func (db *LogDB) queryTransfers(ctx context.Context, stmt string, args ...interf
 			amount      []byte
 		)
 		if err := rows.Scan(
-			&blockNumber,
-			&index,
+			&seq,
 			&blockID,
 			&blockTime,
 			&txID,
@@ -298,8 +291,8 @@ func (db *LogDB) queryTransfers(ctx context.Context, stmt string, args ...interf
 			return nil, err
 		}
 		trans := &Transfer{
-			BlockNumber: blockNumber,
-			Index:       index,
+			BlockNumber: seq.BlockNumber(),
+			Index:       seq.Index(),
 			BlockID:     thor.BytesToBytes32(blockID),
 			BlockTime:   blockTime,
 			TxID:        thor.BytesToBytes32(txID),
@@ -329,10 +322,10 @@ func (db *LogDB) NewestBlockID() (thor.Bytes32, error) {
 
 		// no config, query newest block ID from existing records
 		row = db.db.QueryRow(
-			`SELECT MAX(blockId) FROM (
-SELECT * FROM (SELECT blockId FROM transfer ORDER BY blockNumber DESC LIMIT 1)
+			`SELECT MAX(data) FROM (
+SELECT data FROM ref WHERE id=(SELECT blockId FROM transfer ORDER BY seq DESC LIMIT 1)
 UNION
-SELECT * FROM (SELECT blockId FROM event ORDER BY blockNumber DESC LIMIT 1))`)
+SELECT data FROM ref WHERE id=(SELECT blockId FROM event ORDER BY seq DESC LIMIT 1))`)
 
 		if err := row.Scan(&data); err != nil {
 			if sql.ErrNoRows != err {
@@ -345,18 +338,30 @@ SELECT * FROM (SELECT blockId FROM event ORDER BY blockNumber DESC LIMIT 1))`)
 
 // HasBlockID query whether given block id related logs were written.
 func (db *LogDB) HasBlockID(id thor.Bytes32) (bool, error) {
-	num := block.Number(id)
-	row := db.db.QueryRow(`SELECT COUNT(*) FROM (
-SELECT * FROM (SELECT blockNumber FROM transfer WHERE blockNumber=? AND blockID=? LIMIT 1) 
+	seq := newSequence(block.Number(id), 0)
+	row := db.db.QueryRow(fmt.Sprintf(`SELECT COUNT(*) FROM (
+SELECT * FROM (SELECT seq FROM transfer WHERE seq=? AND blockID=%v LIMIT 1) 
 UNION
-SELECT * FROM (SELECT blockNumber FROM event WHERE blockNumber=? AND blockID=? LIMIT 1))`,
-		num, id.Bytes(), num, id.Bytes())
+SELECT * FROM (SELECT seq FROM event WHERE seq=? AND blockID=%v LIMIT 1))`, refIDQuery, refIDQuery),
+		seq, id.Bytes(), seq, id.Bytes())
 	var count int
 	if err := row.Scan(&count); err != nil {
 		// no need to check ErrNoRows
 		return false, err
 	}
 	return count > 0, nil
+}
+
+// Log write logs.
+func (db *LogDB) Log(f func(*Writer) error) error {
+	w := &Writer{db: db.db}
+	if err := f(w); err != nil {
+		if w.tx != nil {
+			_ = w.tx.Rollback()
+		}
+		return err
+	}
+	return w.Flush()
 }
 
 func topicValue(topics []thor.Bytes32, i int) []byte {
@@ -366,115 +371,167 @@ func topicValue(topics []thor.Bytes32, i int) []byte {
 	return nil
 }
 
-// Task to transactionally perform logs writting.
-type Task struct {
-	db      *sql.DB
-	queries []func(tx *sql.Tx) error
-	ctx     *taskContext
+// Writer is the transactional log writer.
+type Writer struct {
+	db          *sql.DB
+	tx          *sql.Tx
+	len         int
+	lastBlockID thor.Bytes32
 }
 
-type taskContext struct {
-	blockNumber uint32
-	blockID     thor.Bytes32
-	blockTime   uint64
+// Write writes all logs of the given block.
+func (w *Writer) Write(b *block.Block, receipts tx.Receipts) error {
 
-	// counters
-	eventCount    uint32
-	transferCount uint32
-}
+	var (
+		num                       = b.Header().Number()
+		id                        = b.Header().ID()
+		ts                        = b.Header().Timestamp()
+		txs                       = b.Transactions()
+		eventCount, transferCount uint32
+	)
+	w.lastBlockID = id
 
-func (t *Task) add(op func(tx *sql.Tx) error) {
-	t.queries = append(t.queries, op)
-}
-
-// ForBlock set context to given block.
-func (t *Task) ForBlock(b *block.Header) *Task {
-	if b.Number() > 0 {
-		t.add(func(tx *sql.Tx) error {
-			if _, err := tx.Exec("DELETE from event where blockNumber >= ?", b.Number()); err != nil {
-				return err
-			}
-			if _, err := tx.Exec("DELETE from transfer where blockNumber >= ?", b.Number()); err != nil {
-				return err
-			}
-
-			if _, err := tx.Exec("INSERT OR REPLACE INTO config(key, value) VALUES(?,?)",
-				configBlockIDKey,
-				b.ID().Bytes()); err != nil {
-				return err
-			}
-			return nil
-		})
-	}
-	t.ctx = &taskContext{
-		blockNumber: b.Number(),
-		blockID:     b.ID(),
-		blockTime:   b.Timestamp(),
-	}
-	return t
-}
-
-// Write write all outputs of a tx.
-func (t *Task) Write(txID thor.Bytes32, txOrigin thor.Address, outputs []*tx.Output) *Task {
-	// necessary to assign since it's used in closure
-	ctx := t.ctx
-	t.add(func(tx *sql.Tx) error {
-		for clauseIndex, output := range outputs {
-			for _, ev := range output.Events {
-				if _, err := tx.Exec("INSERT OR REPLACE INTO event(blockNumber, eventIndex, blockID, blockTime, txID, txOrigin, clauseIndex, address, topic0, topic1, topic2, topic3, topic4, data) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
-					ctx.blockNumber,
-					ctx.eventCount,
-					ctx.blockID.Bytes(),
-					ctx.blockTime,
-					txID.Bytes(),
-					txOrigin.Bytes(),
-					clauseIndex,
-					ev.Address.Bytes(),
-					topicValue(ev.Topics, 0),
-					topicValue(ev.Topics, 1),
-					topicValue(ev.Topics, 2),
-					topicValue(ev.Topics, 3),
-					topicValue(ev.Topics, 4),
-					ev.Data,
-				); err != nil {
-					return err
-				}
-				ctx.eventCount++
-			}
-			for _, tr := range output.Transfers {
-				if _, err := tx.Exec("INSERT OR REPLACE INTO transfer(blockNumber, transferIndex, blockID, blockTime, txID, txOrigin, clauseIndex, sender, recipient, amount) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
-					ctx.blockNumber,
-					ctx.transferCount,
-					ctx.blockID.Bytes(),
-					ctx.blockTime,
-					txID.Bytes(),
-					txOrigin.Bytes(),
-					clauseIndex,
-					tr.Sender.Bytes(),
-					tr.Recipient.Bytes(),
-					tr.Amount.Bytes(),
-				); err != nil {
-					return err
-				}
-				ctx.transferCount++
-			}
-		}
-		return nil
-	})
-	return t
-}
-
-// Commit commit task.
-func (t *Task) Commit() error {
-	tx, err := t.db.Begin()
-	if err != nil {
-		return err
-	}
-	for _, q := range t.queries {
-		if err := q(tx); err != nil {
-			tx.Rollback()
+	if num > 0 && w.len == 0 {
+		seq := newSequence(num, 0)
+		if err := w.exec("DELETE from event where seq >= ?;DELETE from transfer where seq >= ?", seq, seq); err != nil {
 			return err
 		}
 	}
-	return tx.Commit()
+
+	if len(receipts) > 0 {
+		if err := w.exec(
+			"INSERT OR IGNORE INTO ref(data) VALUES(?)",
+			id.Bytes()); err != nil {
+			return err
+		}
+
+		for txIndex, receipt := range receipts {
+			if len(receipt.Outputs) > 0 {
+				var (
+					txID     thor.Bytes32
+					txOrigin thor.Address
+				)
+				if num != 0 {
+					txID = txs[txIndex].ID()
+					txOrigin, _ = txs[txIndex].Origin()
+				}
+
+				if err := w.exec(
+					"INSERT OR IGNORE INTO ref(data) VALUES(?),(?)",
+					txID.Bytes(), txOrigin.Bytes()); err != nil {
+					return err
+				}
+
+				for clauseIndex, output := range receipt.Outputs {
+					for _, ev := range output.Events {
+						if err := w.exec(
+							"INSERT OR IGNORE INTO ref (data) VALUES(?),(?),(?),(?),(?),(?)",
+							ev.Address.Bytes(),
+							topicValue(ev.Topics, 0),
+							topicValue(ev.Topics, 1),
+							topicValue(ev.Topics, 2),
+							topicValue(ev.Topics, 3),
+							topicValue(ev.Topics, 4)); err != nil {
+							return err
+						}
+
+						if err := w.exec(
+							fmt.Sprintf(
+								"INSERT OR REPLACE INTO event VALUES(?,%v,?,%v,%v,?,%v,%v,%v,%v,%v,%v,?)",
+								refIDQuery, refIDQuery, refIDQuery, refIDQuery, refIDQuery, refIDQuery, refIDQuery, refIDQuery, refIDQuery),
+							newSequence(num, eventCount),
+							id.Bytes(),
+							ts,
+							txID.Bytes(),
+							txOrigin.Bytes(),
+							clauseIndex,
+							ev.Address.Bytes(),
+							topicValue(ev.Topics, 0),
+							topicValue(ev.Topics, 1),
+							topicValue(ev.Topics, 2),
+							topicValue(ev.Topics, 3),
+							topicValue(ev.Topics, 4),
+							ev.Data); err != nil {
+							return err
+						}
+
+						eventCount++
+					}
+
+					for _, tr := range output.Transfers {
+						if err := w.exec(
+							"INSERT OR IGNORE INTO ref (data) VALUES(?),(?)",
+							tr.Sender.Bytes(),
+							tr.Recipient.Bytes()); err != nil {
+							return err
+						}
+						if err := w.exec(
+							fmt.Sprintf(
+								"INSERT OR REPLACE INTO transfer VALUES(?,%v,?,%v,%v,?,%v,%v,?)",
+								refIDQuery, refIDQuery, refIDQuery, refIDQuery, refIDQuery),
+							newSequence(num, transferCount),
+							id.Bytes(),
+							ts,
+							txID.Bytes(),
+							txOrigin.Bytes(),
+							clauseIndex,
+							tr.Sender.Bytes(),
+							tr.Recipient.Bytes(),
+							tr.Amount.Bytes()); err != nil {
+							return err
+						}
+
+						transferCount++
+					}
+				}
+			}
+		}
+	}
+	return nil
+}
+
+// Flush commits accumulated logs.
+func (w *Writer) Flush() (err error) {
+	if w.tx == nil {
+		return nil
+	}
+
+	defer func() {
+		if err != nil {
+			_ = w.tx.Rollback()
+		}
+		w.tx = nil
+		w.lastBlockID = thor.Bytes32{}
+		w.len = 0
+	}()
+
+	if block.Number(w.lastBlockID) > 0 {
+		if err := w.exec(
+			"INSERT OR REPLACE INTO config(key, value) VALUES(?,?)",
+			configBlockIDKey, w.lastBlockID.Bytes()); err != nil {
+			return err
+		}
+	}
+	return w.tx.Commit()
+}
+
+// Len returns count of pending logs.
+func (w *Writer) Len() int {
+	return w.len
+}
+
+func (w *Writer) exec(query string, args ...interface{}) error {
+	if w.tx == nil {
+		tx, err := w.db.Begin()
+		if err != nil {
+			return err
+		}
+		w.tx = tx
+	}
+
+	if _, err := w.tx.Exec(query, args...); err != nil {
+		return err
+	}
+	w.len++
+	return nil
 }
