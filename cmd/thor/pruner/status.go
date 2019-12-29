6c54bbed700eef7ec453e42de1f56f381cf3eecc
qianbin <cola.tin.com@gmail.com>
2019-12-30 00:33:51 +0800

    pruner: implements pruner

---
+++ cmd/thor/pruner/status.go
@@ -0,0 +1,37 @@
+// Copyright (c) 2019 The VeChainThor developers
+
+// Distributed under the GNU Lesser General Public License v3.0 software license, see the accompanying
+// file LICENSE or <https://www.gnu.org/licenses/lgpl-3.0.html>
+
+package pruner
+
+import (
+	"encoding/json"
+
+	"github.com/vechain/thor/muxdb"
+)
+
+type status struct {
+	N1, N2 uint32
+	Step   string
+	Cycles uint32
+}
+
+func (s *status) Load(db *muxdb.MuxDB) error {
+	data, err := db.NewStore(propsStoreName).Get([]byte(statusKey))
+	if err != nil && !db.IsNotFound(err) {
+		return err
+	}
+	if len(data) == 0 {
+		return nil
+	}
+	return json.Unmarshal(data, s)
+}
+
+func (s *status) Save(db *muxdb.MuxDB) error {
+	data, err := json.Marshal(s)
+	if err != nil {
+		return err
+	}
+	return db.NewStore(propsStoreName).Put([]byte(statusKey), data)
+}
