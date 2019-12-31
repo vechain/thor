c7118b213d8669dcc39d61ba2cd69a0c1a34257d
qianbin <cola.tin.com@gmail.com>
2019-12-31 14:07:57 +0800

    thor: set block number for BLOCKLIST fork

--- thor/fork_config.go
+++ thor/fork_config.go
@@ -41,7 +41,7 @@ var forkConfigs = map[Bytes32]ForkConfig{
 	MustParseBytes32("0x00000000851caf3cfdb6e899cf5958bfb1ac3413d346d43539627e6be7ec1b4a"): {
 		VIP191:    3337300,
 		ETH_CONST: 3337300,
-		BLOCKLIST: math.MaxUint32,
+		BLOCKLIST: 4817300,
 	},
 	// testnet
 	MustParseBytes32("0x000000000b2bce3c70bc649a02749e8687721b09ed2e15997f466536b20bb127"): {
