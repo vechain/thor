56f1010b2d4d66a44d29f6947ce6b9b3d4b3d755
peterzhou <peterzhou@git>
2020-01-22 11:43:35 +0800

    solo loop

--- cmd/thor/solo/solo.go
+++ cmd/thor/solo/solo.go
@@ -96,8 +96,8 @@ func (s *Solo) Run(ctx context.Context) error {
 }
 
 func (s *Solo) loop(ctx context.Context) {
-	tickerBegin := time.NewTicker(time.Duration(10) * time.Second)
-	defer tickerBegin.Stop()
+	ticker := time.NewTicker(time.Duration(10) * time.Second)
+	defer ticker.Stop()
 
 	for {
 		select {
@@ -105,7 +105,7 @@ func (s *Solo) loop(ctx context.Context) {
 			log.Info("stopping packing service......")
 			return
 
-		case <-tickerBegin.C:
+		case <-ticker.C:
 			log.Debug("START")
 			startTime := mclock.Now()
 
@@ -114,6 +114,7 @@ func (s *Solo) loop(ctx context.Context) {
 				ts   *block.TxSet
 				err  error
 				flow *packer.Flow
+				done chan struct{}
 
 				header   *block.Header
 				stage    *state.Stage
@@ -127,12 +128,13 @@ func (s *Solo) loop(ctx context.Context) {
 				log.Error("packer.Mock", "error", err)
 			}
 
-			doneCh1 := make(chan struct{})
+			done = make(chan struct{})
 			go func() {
 				time.Sleep(time.Duration(3) * time.Second)
-				close(doneCh1)
+				// close(done)
+				done <- struct{}{}
 			}()
-			bs, ts, err = s.packTxSetAndBlockSummary(doneCh1, flow, txs)
+			bs, ts, err = s.packTxSetAndBlockSummary(done, flow, txs)
 			if err != nil {
 				log.Debug("packTxSetAndBlockSummary", "error", err)
 			}
@@ -140,16 +142,17 @@ func (s *Solo) loop(ctx context.Context) {
 			prepareElapsed := mclock.Now() - startTime
 
 			// log.Debug("Endorsing starts")
-			doneCh2 := make(chan struct{})
+			done = make(chan struct{})
 			edCh := make(chan *block.Endorsement, thor.CommitteeSize)
 			for i := uint64(0); i < thor.CommitteeSize*2; i++ {
-				go s.endorse(doneCh2, edCh, bs)
+				go s.endorse(done, edCh, bs)
 			}
 
 			var eds block.Endorsements
 			for i := uint64(0); i < thor.CommitteeSize*2; i++ {
 				if eds.Len() >= int(thor.CommitteeSize) {
-					close(doneCh2)
+					// close(done)
+					done <- struct{}{}
 					break
 				}
 				select {
