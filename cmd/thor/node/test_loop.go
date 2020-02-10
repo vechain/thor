72cb55869a4ccb2f5f6cba576b4828219425dd58
zzGHzz <ziheng.zhou@gmail.com>
2020-02-10 09:27:02 +0800

    routine

--- cmd/thor/node/test_loop.go
+++ cmd/thor/node/test_loop.go
@@ -7,10 +7,13 @@ import (
 	"encoding/hex"
 	"time"
 
+	"github.com/ethereum/go-ethereum/common/mclock"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/event"
 	"github.com/vechain/thor/block"
 	"github.com/vechain/thor/comm"
 	"github.com/vechain/thor/thor"
+	"github.com/vechain/thor/vrf"
 )
 
 func randByte32() (b thor.Bytes32) {
@@ -19,43 +22,72 @@ func randByte32() (b thor.Bytes32) {
 }
 
 func (n *Node) sendNewStructObj(ctx context.Context) {
-	addr, _ := hex.DecodeString("475f483a6cba6b0eba8ff269aa9d4fcd5b5275e3")
-	if bytes.Compare(n.master.Address().Bytes(), addr) != 0 {
-		log.Debug("exit sending loop, key not matched")
-		return
-	}
-
-	log.Info("starting sending loop")
-	defer func() { log.Info("existing sending loop") }()
-
-	ticker := time.NewTicker(time.Second * 5)
+	ticker := time.NewTicker(time.Second * 20)
 	defer ticker.Stop()
 
 	<-ticker.C
-	// b := new(block.Builder).GasLimit(10000).Timestamp(1231090).Build()
-	// log.Info("sent block", "id", b.Header().ID())
-	// n.comm.BroadcastBlock(b)
 
 	bs := block.RandBlockSummary()
-	log.Info("sent block summary", "id", bs.ID())
+	log.Debug("sending new block summary", "id", bs.ID())
 	n.comm.BroadcastBlockSummary(bs)
 
 	ed := block.RandEndorsement(block.RandBlockSummary())
-	log.Info("send endoresement", "id", ed.ID())
+	log.Debug("sending new endoresement", "id", ed.ID())
 	n.comm.BroadcastEndorsement(ed)
 
 	ts := block.RandTxSet(10)
-	log.Info("sent tx set", "id", ts.ID())
+	log.Debug("sending new tx set", "id", ts.ID())
 	n.comm.BroadcastTxSet(ts)
 
 	header := block.RandBlockHeader()
-	log.Info("sending block header", "id", header.ID())
+	log.Debug("sending new block header", "id", header.ID())
 	n.comm.BroadcastBlockHeader(header)
 }
 
+func (n *Node) sendBlockComponets() {
+	addr, _ := hex.DecodeString("")
+	if bytes.Compare(n.master.Address().Bytes(), addr) != 0 {
+		return
+	}
+
+	best := n.chain.BestBlock()
+
+	flow, err := n.packer.Schedule(best.Header(), uint64(time.Now().Unix()))
+	if err != nil {
+		log.Error("Schedule", "err", err)
+		return
+	}
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+loop:
+	for {
+		select {
+		case <-ticker.C:
+			now := uint64(time.Now().Unix())
+			if now < flow.When() {
+				continue
+			} else {
+				break loop
+			}
+		}
+	}
+
+	log.Debug("begin to pack")
+	t1 := mclock.Now()
+
+	bs, _, err := flow.PackTxSetAndBlockSummary(n.master.PrivateKey)
+	log.Debug("packed block summary", "id", bs.ID())
+
+	ethsk, _ := crypto.HexToECDSA("")
+	_, vrfsk := vrf.GenKeyPairFromSeed(ethsk.D.Bytes())
+
+}
+
 func (n *Node) simpleHouseKeeping(ctx context.Context) {
-	log.Info("enter test house keeping")
-	defer log.Info("leave test house keeping")
+	log.Debug("entering simple house-keeping loop")
+	defer log.Info("leaving simple house-keeping loop")
 
 	var scope event.SubscriptionScope
 	defer scope.Close()
@@ -81,19 +113,19 @@ func (n *Node) simpleHouseKeeping(ctx context.Context) {
 			return
 		case <-ticker.C:
 		case dat := <-newBlockCh:
-			log.Info("received block", "id", dat.Header().ID())
+			log.Debug("received new block", "id", dat.Header().ID())
 			n.comm.BroadcastBlock(dat.Block)
 		case dat := <-newBlockSummaryCh:
-			log.Info("received block summary", "id", dat.ID())
+			log.Debug("received new block summary", "id", dat.ID())
 			n.comm.BroadcastBlockSummary(dat.Summary)
 		case dat := <-newTxSetCh:
-			log.Info("received tx set", "id", dat.ID())
+			log.Debug("received new tx set", "id", dat.ID())
 			n.comm.BroadcastTxSet(dat.TxSet)
 		case dat := <-newEndorsementCh:
-			log.Info("received endorsement", "id", dat.ID())
+			log.Debug("received new endorsement", "id", dat.ID())
 			n.comm.BroadcastEndorsement(dat.Endorsement)
 		case dat := <-newBlockHeaderCh:
-			log.Info("received block header", "id", dat.ID())
+			log.Debug("received new block header", "id", dat.ID())
 			n.comm.BroadcastBlockHeader(dat.Header)
 		}
 	}
