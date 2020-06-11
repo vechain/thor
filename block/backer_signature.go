9128b53c63a3db6737d84d37eb5771290814c251
tony <liboliqi@gmail.com>
2020-06-11 17:27:45 +0800

    add backer signature and root

---
+++ block/backer_signature.go
@@ -0,0 +1,160 @@
+// Copyright (c) 2020 The VeChainThor developers
+
+// Distributed under the GNU Lesser General Public License v3.0 software license, see the accompanying
+// file LICENSE or <https://www.gnu.org/licenses/lgpl-3.0.html>
+
+package block
+
+import (
+	"io"
+	"sync/atomic"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/vechain/go-ecvrf"
+	"github.com/vechain/thor/thor"
+	"github.com/vechain/thor/trie"
+)
+
+var (
+	emptyRoot = trie.DeriveRoot(&derivableBackerSignatures{})
+)
+
+// BackerSignature is the backer's signature of a block proposal by VRF.
+// Composed by [ Compressed Public Key(33bytes) + Proof(81bytes) ]
+type BackerSignature struct {
+	body  [33 + 81]byte
+	cache struct {
+		hash   atomic.Value
+		signer atomic.Value
+		beta   atomic.Value
+	}
+}
+
+// NewBackerSignature creates a new backer signature.
+func NewBackerSignature(pub, proof []byte) *BackerSignature {
+	var bs BackerSignature
+
+	copy(bs.body[:], pub)
+	copy(bs.body[33:], proof)
+
+	return &bs
+}
+
+// Hash is the hash of RLP encoded backer signature.
+func (bs *BackerSignature) Hash() (hash thor.Bytes32) {
+	if cached := bs.cache.hash.Load(); cached != nil {
+		return cached.(thor.Bytes32)
+	}
+	defer func() { bs.cache.hash.Store(hash) }()
+
+	hw := thor.NewBlake2b()
+	rlp.Encode(hw, bs)
+	hw.Sum(hash[:0])
+	return
+}
+
+// Validate validates backer's proof and returns the VRF output.
+func (bs *BackerSignature) Validate(alpha []byte) (beta []byte, err error) {
+	if cached := bs.cache.beta.Load(); cached != nil {
+		return cached.([]byte), nil
+	}
+	defer func() { bs.cache.beta.Store(beta) }()
+
+	pub := make([]byte, 33)
+	proof := make([]byte, 81)
+
+	copy(pub[:], bs.body[:])
+	copy(proof[:], bs.body[33:])
+
+	vrf := ecvrf.NewSecp256k1Sha256Tai()
+	pubkey, err := crypto.DecompressPubkey(pub)
+	if err != nil {
+		return []byte{}, err
+	}
+	beta, err = vrf.Verify(pubkey, alpha, proof)
+	return
+}
+
+// Signer computes the address from the public key.
+func (bs *BackerSignature) Signer() (signer thor.Address, err error) {
+	if cached := bs.cache.signer.Load(); cached != nil {
+		return cached.(thor.Address), nil
+	}
+	defer func() { bs.cache.signer.Store(signer) }()
+
+	pub := make([]byte, 33)
+	copy(pub[:], bs.body[:])
+
+	pubkey, err := crypto.DecompressPubkey(pub)
+	if err != nil {
+		return thor.Address{}, err
+	}
+
+	signer = thor.Address(crypto.PubkeyToAddress(*pubkey))
+	return
+}
+
+// EncodeRLP implements rlp.Encoder.
+func (bs *BackerSignature) EncodeRLP(w io.Writer) error {
+	return rlp.Encode(w, &bs.body)
+}
+
+// DecodeRLP implements rlp.Decoder.
+func (bs *BackerSignature) DecodeRLP(s *rlp.Stream) error {
+	var body [33 + 81]byte
+
+	if err := s.Decode(&body); err != nil {
+		return err
+	}
+	*bs = BackerSignature{body: body}
+	return nil
+}
+
+// BackerSignatures is the list of backer signature.
+type BackerSignatures []*BackerSignature
+
+// RootHash computes merkle root hash of backers.
+func (bss BackerSignatures) RootHash() thor.Bytes32 {
+	if len(bss) == 0 {
+		// optimized
+		return emptyRoot
+	}
+	return trie.DeriveRoot(derivableBackerSignatures(bss))
+}
+
+// implements DerivableList.
+type derivableBackerSignatures BackerSignatures
+
+func (d derivableBackerSignatures) Len() int {
+	return len(d)
+}
+func (d derivableBackerSignatures) GetRlp(i int) []byte {
+	data, err := rlp.EncodeToBytes(d[i])
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+// FullBackerSignature is the backer signature with proposal hash, for p2p propgation.
+type FullBackerSignature struct {
+	ProposalHash thor.Bytes32
+	Signature    *BackerSignatures
+	cache        struct {
+		hash atomic.Value
+	}
+}
+
+// Hash is the hash of RLP encoded full backer signature.
+func (bs *FullBackerSignature) Hash() (hash thor.Bytes32) {
+	if cached := bs.cache.hash.Load(); cached != nil {
+		return cached.(thor.Bytes32)
+	}
+	defer func() { bs.cache.hash.Store(hash) }()
+
+	hw := thor.NewBlake2b()
+	rlp.Encode(hw, bs)
+	hw.Sum(hash[:0])
+	return
+}
---
+++ block/backer_signatures_root.go
@@ -0,0 +1,36 @@
+// Copyright (c) 2020 The VeChainThor developers
+
+// Distributed under the GNU Lesser General Public License v3.0 software license, see the accompanying
+// file LICENSE or <https://www.gnu.org/licenses/lgpl-3.0.html>
+
+package block
+
+import (
+	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/vechain/thor/thor"
+)
+
+type backerSignaturesRoot struct {
+	Root              thor.Bytes32
+	TotalBackersCount uint64
+}
+
+type _backerSignaturesRoot backerSignaturesRoot
+
+// DecodeRLP implements rlp.Decoder.
+func (br *backerSignaturesRoot) DecodeRLP(s *rlp.Stream) error {
+	k, _, _ := s.Kind()
+	if k == rlp.List {
+		var obj _backerSignaturesRoot
+		if err := s.Decode(&obj); err != nil {
+			return err
+		}
+		*br = backerSignaturesRoot(obj)
+	} else {
+		*br = backerSignaturesRoot{
+			emptyRoot,
+			0,
+		}
+	}
+	return nil
+}
