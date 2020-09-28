af6d44519ace9a328b8b9371ac95a5aaca65652f
tony <liboliqi@gmail.com>
2020-09-28 11:44:12 +0800

    add seeder to generate seed for consensus round

--- block/proposal.go
+++ block/proposal.go
@@ -13,7 +13,7 @@ import (
 	"github.com/vechain/thor/thor"
 )
 
-// Proposal is block Proposal.
+// Proposal is block proposal.
 type Proposal struct {
 	ParentID  thor.Bytes32
 	TxsRoot   thor.Bytes32
@@ -26,7 +26,7 @@ type Proposal struct {
 	}
 }
 
-// NewProposal creates a new Proposal.
+// NewProposal creates a new proposal.
 func NewProposal(parentID, txsRoot thor.Bytes32, gasLimit, timestamp uint64) *Proposal {
 	return &Proposal{
 		ParentID:  parentID,
@@ -36,34 +36,34 @@ func NewProposal(parentID, txsRoot thor.Bytes32, gasLimit, timestamp uint64) *Pr
 	}
 }
 
-// Number returns number of the Proposal.
-func (d *Proposal) Number() uint32 {
-	return Number(d.ParentID) + 1
+// Number returns number of the proposal.
+func (p *Proposal) Number() uint32 {
+	return Number(p.ParentID) + 1
 }
 
-// SigningHash returns the hash of the Proposal body without signature.
-func (d *Proposal) SigningHash() (hash thor.Bytes32) {
+// SigningHash returns the hash of the proposal body without signature.
+func (p *Proposal) SigningHash() (hash thor.Bytes32) {
 	b := make([]byte, 16)
-	binary.BigEndian.PutUint64(b, d.GasLimit)
-	binary.BigEndian.PutUint64(b[8:], d.Timestamp)
+	binary.BigEndian.PutUint64(b, p.GasLimit)
+	binary.BigEndian.PutUint64(b[8:], p.Timestamp)
 
 	// [parentID + txsRoot + Gaslimit + Timestamp]
-	hash = thor.Blake2b(d.ParentID.Bytes(), d.TxsRoot.Bytes(), b)
+	hash = thor.Blake2b(p.ParentID.Bytes(), p.TxsRoot.Bytes(), b)
 	return
 }
 
-// Signer extract signer of the Proposal from signature.
-func (d *Proposal) Signer() (signer thor.Address, err error) {
-	if cached := d.cache.signer.Load(); cached != nil {
+// Signer extract signer of the proposal from signature.
+func (p *Proposal) Signer() (signer thor.Address, err error) {
+	if cached := p.cache.signer.Load(); cached != nil {
 		return cached.(thor.Address), nil
 	}
 	defer func() {
 		if err == nil {
-			d.cache.signer.Store(signer)
+			p.cache.signer.Store(signer)
 		}
 	}()
 
-	pub, err := crypto.SigToPub(d.SigningHash().Bytes(), d.Signature)
+	pub, err := crypto.SigToPub(p.SigningHash().Bytes(), p.Signature)
 	if err != nil {
 		return thor.Address{}, err
 	}
@@ -72,29 +72,27 @@ func (d *Proposal) Signer() (signer thor.Address, err error) {
 	return
 }
 
-// WithSignature create a new Proposal with signature set.
-func (d *Proposal) WithSignature(sig []byte) *Proposal {
-	return &Proposal{
-		ParentID:  d.ParentID,
-		TxsRoot:   d.TxsRoot,
-		GasLimit:  d.GasLimit,
-		Timestamp: d.Timestamp,
-		Signature: append([]byte(nil), sig...),
-	}
+// AsMessage returns the bytes of proposal body for being a message, which is parentID+txsRoot+gaslimit+timestamp+signer.
+// Mostly used for computing backer signature.
+func (p *Proposal) AsMessage(signer thor.Address) []byte {
+	// [parentID + txsRoot + gaslimit + timestamp + signer]
+	msg := make([]byte, 100)
+	copy(msg[:], p.ParentID.Bytes())
+	copy(msg[32:], p.TxsRoot.Bytes())
+	binary.BigEndian.PutUint64(msg[64:], p.GasLimit)
+	binary.BigEndian.PutUint64(msg[72:], p.Timestamp)
+	copy(msg[80:], signer.Bytes())
+
+	return msg
 }
 
-// Hash is the hash of Proposal body.
-func (d *Proposal) Hash() (hash thor.Bytes32) {
-	if cached := d.cache.hash.Load(); cached != nil {
-		return cached.(thor.Bytes32)
+// WithSignature create a new proposal with signature set.
+func (p *Proposal) WithSignature(sig []byte) *Proposal {
+	return &Proposal{
+		ParentID:  p.ParentID,
+		TxsRoot:   p.TxsRoot,
+		GasLimit:  p.GasLimit,
+		Timestamp: p.Timestamp,
+		Signature: append([]byte(nil), sig...),
 	}
-	defer func() { d.cache.hash.Store(hash) }()
-
-	b := make([]byte, 16)
-	binary.BigEndian.PutUint64(b, d.GasLimit)
-	binary.BigEndian.PutUint64(b[8:], d.Timestamp)
-
-	// [parentID + txsRoot + gaslimit + timestamp + signature]
-	hash = thor.Blake2b(d.ParentID.Bytes(), d.TxsRoot.Bytes(), b, d.Signature)
-	return
 }
