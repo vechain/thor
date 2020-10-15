4da6b3d48f5e2394ac512b76e16641309e3576d4
tony <liboliqi@gmail.com>
2020-10-15 15:22:54 +0800

    consensus:validate vip 193 blocks

--- consensus/consensus.go
+++ consensus/consensus.go
@@ -57,7 +57,7 @@ func (c *Consensus) Process(blk *block.Block, nowTimestamp uint64) (*state.Stage
 		return nil, nil, errKnownBlock
 	}
 
-	parentSummary, err := c.repo.GetBlockSummary(header.ParentID())
+	parent, err := c.repo.GetBlock(header.ParentID())
 	if err != nil {
 		if !c.repo.IsNotFound(err) {
 			return nil, nil, err
@@ -65,7 +65,7 @@ func (c *Consensus) Process(blk *block.Block, nowTimestamp uint64) (*state.Stage
 		return nil, nil, errParentMissing
 	}
 
-	state := c.stater.NewState(parentSummary.Header.StateRoot())
+	state := c.stater.NewState(parent.Header().StateRoot())
 
 	vip191 := c.forkConfig.VIP191
 	if vip191 == 0 {
@@ -87,7 +87,7 @@ func (c *Consensus) Process(blk *block.Block, nowTimestamp uint64) (*state.Stage
 		return nil, nil, consensusError(fmt.Sprintf("block txs features invalid: want %v, have %v", features, header.TxsFeatures()))
 	}
 
-	stage, receipts, err := c.validate(state, blk, parentSummary.Header, nowTimestamp)
+	stage, receipts, err := c.validate(state, blk, parent, nowTimestamp)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -100,16 +100,16 @@ func (c *Consensus) NewRuntimeForReplay(header *block.Header, skipPoA bool) (*ru
 	if err != nil {
 		return nil, err
 	}
-	parentSummary, err := c.repo.GetBlockSummary(header.ParentID())
+	parent, err := c.repo.GetBlock(header.ParentID())
 	if err != nil {
 		if !c.repo.IsNotFound(err) {
 			return nil, err
 		}
 		return nil, errParentMissing
 	}
-	state := c.stater.NewState(parentSummary.Header.StateRoot())
+	state := c.stater.NewState(parent.Header().StateRoot())
 	if !skipPoA {
-		if _, err := c.validateProposer(header, parentSummary.Header, state); err != nil {
+		if _, _, err := c.validateProposer(header, parent, state); err != nil {
 			return nil, err
 		}
 	}
