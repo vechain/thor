2dae057dcdf3a797f78b7e171b67830adaca353a
qianbin <cola.tin.com@gmail.com>
2021-08-06 14:20:12 +0800

    kv: remove PutFlusher interface

--- kv/f.go
+++ kv/f.go
@@ -12,9 +12,8 @@ type (
 	HasFunc        func(key []byte) (bool, error)
 	PutFunc        func(key, val []byte) error
 	DeleteFunc     func(key []byte) error
-	FlushFunc      func() error
 	SnapshotFunc   func(fn func(Getter) error) error
-	BatchFunc      func(fn func(PutFlusher) error) error
+	BatchFunc      func(fn func(Putter) error) error
 	IterateFunc    func(rgn Range, fn func(Pair) bool) error
 	IsNotFoundFunc func(err error) bool
 	KeyFunc        func() []byte
@@ -25,9 +24,8 @@ func (f GetFunc) Get(key []byte) ([]byte, error)                  { return f(key
 func (f HasFunc) Has(key []byte) (bool, error)                    { return f(key) }
 func (f PutFunc) Put(key, val []byte) error                       { return f(key, val) }
 func (f DeleteFunc) Delete(key []byte) error                      { return f(key) }
-func (f FlushFunc) Flush() error                                  { return f() }
 func (f SnapshotFunc) Snapshot(fn func(Getter) error) error       { return f(fn) }
-func (f BatchFunc) Batch(fn func(PutFlusher) error) error         { return f(fn) }
+func (f BatchFunc) Batch(fn func(Putter) error) error             { return f(fn) }
 func (f IterateFunc) Iterate(rng Range, fn func(Pair) bool) error { return f(rng, fn) }
 func (f IsNotFoundFunc) IsNotFound(err error) bool                { return f(err) }
 func (f KeyFunc) Key() []byte                                     { return f() }
