e90e66988e95149cfb7b166f1a9cf1896d59011e
qianbin <cola.tin.com@gmail.com>
2021-09-23 13:15:05 +0800

    muxdb/internal/trie: rewrite trie * big improvement of cache, using path as key * implement vip-212 * trie node partition * new pruning algorithm

--- muxdb/internal/trie/cache.go
+++ muxdb/internal/trie/cache.go
@@ -6,6 +6,12 @@
 package trie
 
 import (
+	"bytes"
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+
 	"github.com/coocood/freecache"
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/vechain/thor/thor"
@@ -14,89 +20,154 @@ import (
 
 // Cache is the cache layer for trie.
 type Cache struct {
-	nodes  [7]*freecache.Cache // slots to cache node blobs.
-	pfkeys *freecache.Cache    // caches prefilter keys.
-	roots  *lru.Cache          // caches root nodes.
+	nodes       *freecache.Cache // caches node blobs.
+	roots       *lru.ARCCache    // caches root nodes.
+	nodeStats   cacheStats
+	rootStats   cacheStats
+	lastLogTime int64
+	logMu       sync.Mutex
 }
 
 // NewCache creates a cache object with the given cache size.
 func NewCache(sizeMB int, rootCap int) *Cache {
 	var cache Cache
-	if sizeMB > 0 {
-		nSlot := len(cache.nodes) + 1
-		size := sizeMB * 1024 * 1024 / nSlot
-		for i := 0; i < len(cache.nodes); i++ {
-			cache.nodes[i] = freecache.NewCache(size)
-		}
-		cache.pfkeys = freecache.NewCache(size)
-	}
-	if rootCap > 0 {
-		cache.roots, _ = lru.New(rootCap)
-	}
+	cache.nodes = freecache.NewCache(sizeMB * 1024 * 1024)
+	cache.roots, _ = lru.NewARC(rootCap)
+	cache.lastLogTime = time.Now().UnixNano()
 	return &cache
 }
 
-func (c *Cache) nodeSlot(pathLen int) *freecache.Cache {
-	if n := len(c.nodes); pathLen >= n {
-		pathLen = n - 1
-	}
-	return c.nodes[pathLen]
-}
+func (c *Cache) log() {
+	c.logMu.Lock()
+	defer c.logMu.Unlock()
 
-// AddNodeBlob adds node into the cache.
-func (c *Cache) AddNodeBlob(key, node []byte, pathLen int) {
-	if s := c.nodeSlot(pathLen); s != nil {
-		_ = s.Set(key, node, 0)
+	if now := time.Now().UnixNano(); now-c.lastLogTime > int64(time.Second*20) {
+		c.lastLogTime = now
+		c.nodeStats.Log("node cache stats")
+		c.rootStats.Log("root cache stats")
 	}
 }
 
-// GetNodeBlob returns the cached node.
-func (c *Cache) GetNodeBlob(key []byte, pathLen int, peek bool) (node []byte) {
-	if s := c.nodeSlot(pathLen); s != nil {
-		f := s.Get
-		if peek {
-			f = s.Peek
+// AddNodeBlob adds node blob into the cache.
+func (c *Cache) AddNodeBlob(name string, key HistNodeKey, node []byte) {
+	if c == nil {
+		return
+	}
+
+	k := bufferPool.Get().(*buffer)
+	defer bufferPool.Put(k)
+
+	// concat name with path as cache key
+	k.b = append(k.b[:0], name...)
+	k.b = append(k.b, key.PathBlob()...)
+
+	v := bufferPool.Get().(*buffer)
+	defer bufferPool.Put(v)
+
+	// concat hash pointer with blob as cache value
+	v.b = append(v.b[:0], key.HashPointer()...)
+	v.b = append(v.b, node...)
+
+	_ = c.nodes.Set(k.b, v.b, 0)
+}
+
+// GetNodeBlob returns the cached node blob.
+func (c *Cache) GetNodeBlob(name string, key HistNodeKey, peek bool) (node []byte) {
+	if c == nil {
+		return
+	}
+
+	get := c.nodes.Get
+	if peek {
+		get = c.nodes.Peek
+	}
+
+	buf := bufferPool.Get().(*buffer)
+	defer bufferPool.Put(buf)
+
+	// concat name with path as cache key
+	buf.b = append(buf.b[:0], name...)
+	buf.b = append(buf.b, key.PathBlob()...)
+
+	if val, _ := get(buf.b); len(val) > 0 {
+		// compare the hash pointer
+		hp := key.HashPointer()
+		if bytes.Equal(hp, val[:len(hp)]) {
+			if !peek {
+				c.nodeStats.Hit()
+			}
+			return val[len(hp):]
 		}
-		node, _ = f(key)
+	}
+	if !peek {
+		c.nodeStats.Miss()
 	}
 	return
 }
 
-// AddPrefilterKey add prefilter key into the cache.
-func (c *Cache) AddPrefilterKey(key []byte) {
-	if s := c.pfkeys; s != nil {
-		s.Set(key, nil, 0)
-	}
-}
-
-// HasPrefilterKey check if the given key is in the cache.
-func (c *Cache) HasPrefilterKey(key []byte) bool {
-	if s := c.pfkeys; s != nil {
-		_, err := s.Get(key)
-		return err == nil
-	}
-	return false
-}
-
 type rootNodeKey struct {
-	name      string
 	root      thor.Bytes32
 	commitNum uint32
 }
 
-// AddRootNode add the root node into the cache
-func (c *Cache) AddRootNode(name string, root thor.Bytes32, commitNum uint32, n *trie.Node) {
-	if r := c.roots; r != nil {
-		r.Add(rootNodeKey{name, root, commitNum}, n)
+// AddRootNode add the root node into the cache.
+func (c *Cache) AddRootNode(name string, n *trie.Node) bool {
+	if c == nil {
+		return false
 	}
+	if n.Dirty() {
+		return false
+	}
+	var sub *lru.Cache
+	if q, has := c.roots.Get(name); has {
+		sub = q.(*lru.Cache)
+	} else {
+		sub, _ = lru.New(4)
+		c.roots.Add(name, sub)
+	}
+	sub.Add(rootNodeKey{n.Hash(), n.CommitNum()}, n)
+	return true
 }
 
 // GetRootNode returns the cached root node.
 func (c *Cache) GetRootNode(name string, root thor.Bytes32, commitNum uint32) *trie.Node {
-	if r := c.roots; r != nil {
-		if cached, has := r.Get(rootNodeKey{name, root, commitNum}); has {
+	if c == nil {
+		return nil
+	}
+
+	if sub, has := c.roots.Get(name); has {
+		if cached, has := sub.(*lru.Cache).Get(rootNodeKey{root, commitNum}); has {
+			if c.rootStats.Hit()%1000 == 0 {
+				c.log()
+			}
 			return cached.(*trie.Node)
 		}
 	}
+	c.rootStats.Miss()
 	return nil
+}
+
+type cacheStats struct {
+	hit, miss int64
+}
+
+func (cs *cacheStats) Hit() int64  { return atomic.AddInt64(&cs.hit, 1) }
+func (cs *cacheStats) Miss() int64 { return atomic.AddInt64(&cs.miss, 1) }
+
+func (cs *cacheStats) Log(msg string) {
+	hit := atomic.LoadInt64(&cs.hit)
+	miss := atomic.LoadInt64(&cs.miss)
+	lookups := hit + miss
+
+	var hitrate string
+	if lookups > 0 {
+		hitrate = fmt.Sprintf("%.3f", float64(hit)/float64(lookups))
+	} else {
+		hitrate = "n/a"
+	}
+
+	log.Info(msg,
+		"lookups", lookups,
+		"hitrate", hitrate,
+	)
 }
--- muxdb/internal/trie/node_key.go
+++ muxdb/internal/trie/node_key.go
@@ -7,58 +7,102 @@ package trie
 
 import (
 	"encoding/binary"
-	"math"
 )
 
-// nodeKey helps encode/decode trie node key.
-// A trie node key is composed in (space, name, path, commitNum, hash).
-// space - 1 byte
-// name - var len
-// path - 8 bytes
-// commitNum - 4 bytes
-// hash - 32 bytes
-type nodeKey []byte
+// HistNodeKey provides the buffer and helps to encode historical-node key.
+// The historical-node key consists of partition id, path, commitNum, and hash.
+// partition id: 4bytes
+// path: >= 8 bytes
+// commitNum: 4 bytes
+// hash: 32 bytes
+type HistNodeKey []byte
 
-// newNodeKey create a node key with the given trie name.
-func newNodeKey(name string) nodeKey {
-	buf := make([]byte, 1+len(name)+8+4+32)
-	copy(buf[1:], name)
-	return buf
-}
+// Encode returns the encoded historical-node key.
+func (k *HistNodeKey) Encode(factor PartitionFactor, hash []byte, commitNum uint32, path []byte) []byte {
+	// partition id
+	pid := factor.Which(commitNum)
+	*k = appendUint32((*k)[:0], pid)
 
-// Encode returns the encoded key for saving trie node.
-func (k nodeKey) Encode(hash []byte, commitNum uint32, path []byte) []byte {
-	if len(path) <= 15 {
-		k[0] = NodeSpace       // space
-		p := k[len(k)-8-4-32:] // path
-		c := p[8:]             // commit num
-		h := c[4:]             // hash
-
-		binary.BigEndian.PutUint64(p, uint64(newPath64(path)))
-		binary.BigEndian.PutUint32(c, commitNum)
-		copy(h, hash)
-		return k
+	// path
+	for i := 0; i < len(path); i += 7 {
+		*k = appendUint32(*k, encodePath32(path[i:]))
 	}
 
-	// for a path-len overflowed node, the key is encoded as (space, name, commitNum, hash)
-	k[0] = OverflowNodeSpace // space
-	c := k[len(k)-8-4-32:]   // commit num
-	h := c[4:]               // hash
-
-	binary.BigEndian.PutUint32(c, commitNum)
-	copy(h, hash)
-	return k[:len(k)-8]
+	// commit number
+	*k = appendUint32(*k, commitNum)
+	// hash
+	*k = append(*k, hash...)
+	return *k
 }
 
-// Path returns the path part.
-func (k nodeKey) Path() path64 {
-	if k[0] == NodeSpace {
-		return path64(binary.BigEndian.Uint64(k[len(k)-8-4-32:]))
-	}
-	return math.MaxUint64
+// PathBlob returns the path blob.
+func (k HistNodeKey) PathBlob() []byte {
+	return k[4 : len(k)-4-32]
 }
 
-// CommitNum returns the commit num part.
-func (k nodeKey) CommitNum() uint32 {
+// Hash returns the hash.
+func (k HistNodeKey) Hash() []byte {
+	return k[len(k)-32:]
+}
+
+// HashPointer returns the hash pointer(hash with commit number).
+func (k HistNodeKey) HashPointer() []byte {
+	return k[len(k)-4-32:]
+}
+
+// CommitNum returns the commit number of the node.
+func (k HistNodeKey) CommitNum() uint32 {
 	return binary.BigEndian.Uint32(k[len(k)-4-32:])
+}
+
+// DedupedNodeKey provide the buffer and helps to encode deduped-node key.
+// The deduped-node key consists of partition id and path.
+type DedupedNodeKey []byte
+
+// Encode returns the encoded deduped-node key.
+func (k *DedupedNodeKey) Encode(factor PartitionFactor, commitNum uint32, path []byte) []byte {
+	// partition id
+	pid := factor.Which(commitNum)
+	*k = appendUint32((*k)[:0], pid)
+
+	// path
+	for i := 0; i < len(path); i += 7 {
+		*k = appendUint32(*k, encodePath32(path[i:]))
+	}
+	return *k
+}
+
+// FromHistKey convert the historical-node key into deduped-node key.
+func (k *DedupedNodeKey) FromHistKey(factor PartitionFactor, hnk HistNodeKey) []byte {
+	// partition id
+	pid := factor.Which(hnk.CommitNum())
+	*k = appendUint32((*k)[:0], pid)
+	// path
+	*k = append(*k, hnk.PathBlob()...)
+	return *k
+}
+
+func encodePath32(path []byte) uint32 {
+	n := len(path)
+	if n > 7 {
+		n = 7
+	}
+
+	var v uint32
+	for i := 0; i < 7; i++ {
+		if i < n {
+			v |= uint32(path[i])
+		}
+		v <<= 4
+	}
+	return v | uint32(n)
+}
+
+func appendUint32(b []byte, v uint32) []byte {
+	return append(b,
+		byte(v>>24),
+		byte(v>>16),
+		byte(v>>8),
+		byte(v),
+	)
 }
