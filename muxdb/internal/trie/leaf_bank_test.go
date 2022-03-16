9245706dd09df606f7aa4f2bf48028f58af831f3
qianbin <cola.tin.com@gmail.com>
2022-03-17 01:43:10 +0800

    muxdb/internal/trie: improve leafbank

--- muxdb/internal/trie/leaf_bank_test.go
+++ muxdb/internal/trie/leaf_bank_test.go
@@ -15,22 +15,23 @@ import (
 
 func TestLeafbank(t *testing.T) {
 	engine := newEngine()
-	lb := NewLeafBank(engine, 2, 100)
+	space := byte(2)
+	slotCap := 10
+	lb := NewLeafBank(engine, space, slotCap)
 	name := "the trie"
 
 	t.Run("empty state", func(t *testing.T) {
 		key := []byte("key")
 		rec, err := lb.Lookup(name, key)
-		if err != nil {
-			t.Fatal(err)
-		}
+		assert.NoError(t, err)
 		assert.Equal(t, &LeafRecord{}, rec)
 	})
 
 	t.Run("update and lookup", func(t *testing.T) {
-		u := lb.NewUpdater(name, 100)
+		u, err := lb.NewUpdater(name, 0, 100)
+		assert.Nil(t, err)
 		for i := 0; i < 10; i++ {
-			if err := u.Update([]byte(strconv.Itoa(i)), &trie.Leaf{Value: []byte(strconv.Itoa(i))}); err != nil {
+			if err := u.Update([]byte(strconv.Itoa(i)), &trie.Leaf{Value: []byte(strconv.Itoa(i))}, 10); err != nil {
 				t.Fatal(err)
 			}
 		}
@@ -40,40 +41,38 @@ func TestLeafbank(t *testing.T) {
 
 		for i := 0; i < 10; i++ {
 			rec, err := lb.Lookup(name, []byte(strconv.Itoa(i)))
-			if err != nil {
-				t.Fatal(err)
-			}
+			assert.NoError(t, err)
 			assert.Equal(t, &LeafRecord{
-				Value:     []byte(strconv.Itoa(i)),
-				CommitNum: 100,
+				Leaf:          &trie.Leaf{Value: []byte(strconv.Itoa(i))},
+				CommitNum:     10,
+				SlotCommitNum: 100,
 			}, rec)
 		}
 	})
 
 	t.Run("lookup never seen", func(t *testing.T) {
 		rec, err := lb.Lookup(name, []byte(strconv.Itoa(11)))
-		if err != nil {
-			t.Fatal(err)
-		}
+		assert.NoError(t, err)
 
-		assert.Equal(t, &LeafRecord{
-			CommitNum: 100,
-		}, rec)
+		assert.Equal(t, &LeafRecord{Leaf: &trie.Leaf{}, SlotCommitNum: 100}, rec)
 	})
 
-	t.Run("lookup touched", func(t *testing.T) {
+	t.Run("lookup deleted", func(t *testing.T) {
 		// mark
-		if err := lb.Touch(engine, name, []byte(strconv.Itoa(1))); err != nil {
-			t.Fatal(err)
-		}
+		err := lb.LogDeletions(engine, name, []string{strconv.Itoa(1)}, 101)
+		assert.Nil(t, err)
+
+		u, err := lb.NewUpdater(name, 100, 101)
+		assert.Nil(t, err)
+
+		err = u.Commit()
+		assert.Nil(t, err)
+
 		// recreate to drop cache
-		lb = NewLeafBank(engine, 2, 100)
+		lb = NewLeafBank(engine, space, slotCap)
 
 		rec, err := lb.Lookup(name, []byte(strconv.Itoa(1)))
-		if err != nil {
-			t.Fatal(err)
-		}
-
+		assert.NoError(t, err)
 		assert.Equal(t, &LeafRecord{}, rec)
 	})
 }
