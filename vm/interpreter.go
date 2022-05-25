5e6e55fc243828a35945a7dd5a756c0e3ede65f2
tony <liboliqi@gmail.com>
2022-05-25 22:03:56 +0800

    vm(tracer):  do not report post-op memory

--- vm/interpreter.go
+++ vm/interpreter.go
@@ -202,13 +202,15 @@ func (in *Interpreter) Run(contract *Contract, input []byte) (ret []byte, err er
 		if err != nil || !contract.UseGas(cost) {
 			return nil, ErrOutOfGas
 		}
-		if memorySize > 0 {
-			mem.Resize(memorySize)
-		}
 
+		// Do tracing before memory expansion
 		if in.cfg.Debug {
 			in.cfg.Tracer.CaptureState(pc, op, gasCopy, cost, mem, stack, contract, in.returnData, in.evm.depth, err)
 			logged = true
+		}
+
+		if memorySize > 0 {
+			mem.Resize(memorySize)
 		}
 
 		// execute the operation
