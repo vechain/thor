103ffeb12e93ffe8f39dba4d3b9325a509bddf4b
Makis Christou <michalis.christou@protonmail.com>
2024-01-10 13:38:24 +0200

    Add genesis tests

--- genesis/devnet.go
+++ genesis/devnet.go
@@ -123,3 +123,71 @@ func NewDevnet() *Genesis {
 
 	return &Genesis{builder, id, "devnet"}
 }
+
+// NewDevnet create genesis for solo mode.
+func NewDevnetCustomTimestamp(timestamp uint64) *Genesis {
+	launchTime := timestamp
+
+	executor := DevAccounts()[0].Address
+	soloBlockSigner := DevAccounts()[0]
+
+	builder := new(Builder).
+		GasLimit(thor.InitialGasLimit).
+		Timestamp(launchTime).
+		State(func(state *state.State) error {
+
+			// setup builtin contracts
+			if err := state.SetCode(builtin.Authority.Address, builtin.Authority.RuntimeBytecodes()); err != nil {
+				return err
+			}
+			if err := state.SetCode(builtin.Energy.Address, builtin.Energy.RuntimeBytecodes()); err != nil {
+				return err
+			}
+			if err := state.SetCode(builtin.Params.Address, builtin.Params.RuntimeBytecodes()); err != nil {
+				return err
+			}
+			if err := state.SetCode(builtin.Prototype.Address, builtin.Prototype.RuntimeBytecodes()); err != nil {
+				return err
+			}
+			if err := state.SetCode(builtin.Extension.Address, builtin.Extension.RuntimeBytecodes()); err != nil {
+				return err
+			}
+
+			tokenSupply := &big.Int{}
+			energySupply := &big.Int{}
+			for _, a := range DevAccounts() {
+				bal, _ := new(big.Int).SetString("1000000000000000000000000000", 10)
+				if err := state.SetBalance(a.Address, bal); err != nil {
+					return err
+				}
+				if err := state.SetEnergy(a.Address, bal, launchTime); err != nil {
+					return err
+				}
+				tokenSupply.Add(tokenSupply, bal)
+				energySupply.Add(energySupply, bal)
+			}
+			return builtin.Energy.Native(state, launchTime).SetInitialSupply(tokenSupply, energySupply)
+		}).
+		Call(
+			tx.NewClause(&builtin.Params.Address).WithData(mustEncodeInput(builtin.Params.ABI, "set", thor.KeyExecutorAddress, new(big.Int).SetBytes(executor[:]))),
+			thor.Address{}).
+		Call(
+			tx.NewClause(&builtin.Params.Address).WithData(mustEncodeInput(builtin.Params.ABI, "set", thor.KeyRewardRatio, thor.InitialRewardRatio)),
+			executor).
+		Call(
+			tx.NewClause(&builtin.Params.Address).WithData(mustEncodeInput(builtin.Params.ABI, "set", thor.KeyBaseGasPrice, thor.InitialBaseGasPrice)),
+			executor).
+		Call(
+			tx.NewClause(&builtin.Params.Address).WithData(mustEncodeInput(builtin.Params.ABI, "set", thor.KeyProposerEndorsement, thor.InitialProposerEndorsement)),
+			executor).
+		Call(
+			tx.NewClause(&builtin.Authority.Address).WithData(mustEncodeInput(builtin.Authority.ABI, "add", soloBlockSigner.Address, soloBlockSigner.Address, thor.BytesToBytes32([]byte("Solo Block Signer")))),
+			executor)
+
+	id, err := builder.ComputeID()
+	if err != nil {
+		panic(err)
+	}
+
+	return &Genesis{builder, id, "devnet"}
+}
