4cc13e755df97bd584f037788a7cabe434cce363
Makis Christou <michalis.christou@protonmail.com>
2024-01-12 09:15:05 +0200

    Add genesis tests to increase coverage

---
+++ genesis/devnet_test.go
@@ -0,0 +1,32 @@
+package genesis_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vechain/thor/v2/genesis"
+	"github.com/vechain/thor/v2/thor"
+)
+
+// TestDevAccounts checks if DevAccounts function returns the expected number of accounts and initializes them correctly
+func TestDevAccounts(t *testing.T) {
+	accounts := genesis.DevAccounts()
+
+	// Assuming 10 private keys are defined in DevAccounts
+	expectedNumAccounts := 10
+	assert.Equal(t, expectedNumAccounts, len(accounts), "Incorrect number of dev accounts returned")
+
+	for _, account := range accounts {
+		assert.NotNil(t, account.PrivateKey, "Private key should not be nil")
+		assert.NotEqual(t, thor.Address{}, account.Address, "Account address should be valid")
+	}
+}
+
+// TestNewDevnet checks if NewDevnet function returns a correctly initialized Genesis object
+func TestNewDevnet(t *testing.T) {
+	genesisObj := genesis.NewDevnet()
+
+	assert.NotNil(t, genesisObj, "NewDevnet should return a non-nil Genesis object")
+	assert.NotEqual(t, thor.Bytes32{}, genesisObj.ID(), "Genesis ID should be valid")
+	assert.Equal(t, "devnet", genesisObj.Name(), "Genesis name should be 'devnet'")
+}
