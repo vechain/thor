a5d27def94cd0d20f91e85957a763c83d2d7c70e
Makis Christou <michalis.christou@protonmail.com>
2024-01-22 09:27:19 +0200

    Add test files for memory, noop, opcodes and stack

---
+++ vm/memory_test.go
@@ -0,0 +1,74 @@
+package vm
+
+import (
+	"testing"
+
+	"github.com/holiman/uint256"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewMemory(t *testing.T) {
+	mem := NewMemory()
+	assert.NotNil(t, mem)
+	mem.Set(1, 0, []byte{})
+
+	mem.Print()
+
+}
+
+func TestSet(t *testing.T) {
+	mem := NewMemory()
+	assert.NotNil(t, mem)
+
+	testData := []byte{1, 2, 3, 4}
+	mem.Resize(uint64(len(testData)))
+	mem.Set(0, uint64(len(testData)), testData)
+
+	retrievedData := mem.GetPtr(0, int64(len(testData)))
+	assert.Equal(t, testData, retrievedData)
+
+	mem.Print()
+
+	data := mem.Data()
+
+	assert.NotNil(t, data)
+}
+
+func TestSet32AndGetCopy(t *testing.T) {
+	mem := NewMemory()
+	assert.NotNil(t, mem)
+
+	// Create a uint256 value and set it in memory
+	testValue := uint256.NewInt(123).SetUint64(12345678)
+	mem.Resize(32) // Ensure there's enough space
+	mem.Set32(0, testValue)
+
+	// Retrieve the value using GetCopy
+	copiedData := mem.GetCopy(0, 32)
+	assert.NotNil(t, copiedData, "Copied data should not be nil")
+
+	// Convert copiedData back to uint256 for comparison
+	retrievedValue := new(uint256.Int).SetBytes(copiedData)
+	assert.Equal(t, testValue, retrievedValue, "Retrieved value should match the original")
+}
+
+func TestSetPanic(t *testing.T) {
+	mem := NewMemory()
+	assert.NotNil(t, mem)
+
+	// This should cause a panic because the memory store is not resized
+	assert.Panics(t, func() {
+		mem.Set(0, 10, []byte{1, 2, 3, 4}) // Trying to set 10 bytes in an empty store
+	}, "Set should panic when trying to set more data than the size of the store")
+}
+
+func TestSet32Panic(t *testing.T) {
+	mem := NewMemory()
+	assert.NotNil(t, mem)
+
+	// This should cause a panic because the memory store is not resized
+	testVal := uint256.NewInt(123).SetUint64(12345)
+	assert.Panics(t, func() {
+		mem.Set32(0, testVal) // Trying to set 32 bytes in an empty store
+	}, "Set32 should panic when trying to set 32 bytes in an empty store")
+}
---
+++ vm/stack_test.go
@@ -0,0 +1,75 @@
+package vm
+
+import (
+	"testing"
+
+	"github.com/holiman/uint256"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStackPushPop(t *testing.T) {
+	stack := newstack()
+	defer returnStack(stack)
+
+	val := uint256.NewInt(42)
+	stack.push(val)
+
+	assert.Equal(t, 1, stack.len(), "Stack should have one item after push")
+	assert.Equal(t, val, stack.peek(), "Top item should be the one that was pushed")
+
+	popped := stack.pop()
+	assert.Equal(t, val, &popped, "Popped item should be equal to pushed item")
+	assert.Equal(t, 0, stack.len(), "Stack should be empty after pop")
+}
+
+func TestStackSwap(t *testing.T) {
+	stack := newstack()
+	defer returnStack(stack)
+
+	first := uint256.NewInt(1)
+	second := uint256.NewInt(2)
+	stack.push(first)
+	stack.push(second)
+
+	stack.swap(2)
+	assert.Equal(t, first, stack.peek(), "Top item should be the first one after swap")
+}
+
+func TestStackDup(t *testing.T) {
+	stack := newstack()
+	defer returnStack(stack)
+
+	val := uint256.NewInt(42)
+	stack.push(val)
+	stack.dup(1)
+
+	assert.Equal(t, 2, stack.len(), "Stack should have two items after dup")
+	assert.Equal(t, val, stack.peek(), "Top item should be the same as the duplicated one")
+}
+
+func TestStackBack(t *testing.T) {
+	stack := newstack()
+	defer returnStack(stack)
+
+	first := uint256.NewInt(1)
+	second := uint256.NewInt(2)
+	stack.push(first)
+	stack.push(second)
+
+	back := stack.Back(1)
+	assert.Equal(t, first, back, "Back should return the first item")
+}
+
+func TestStackPrint(t *testing.T) {
+	stack := newstack()
+	defer returnStack(stack)
+
+	// Test printing an empty and a non-empty stack
+	// Since Print() doesn't return anything, we are just
+	// making sure it doesn't crash for now
+	stack.Print()
+
+	val := uint256.NewInt(42)
+	stack.push(val)
+	stack.Print()
+}
