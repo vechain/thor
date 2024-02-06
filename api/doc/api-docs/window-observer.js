b450c56c68b45bfdbec12ba146f1a5e673f38c67
Darren Kelly <107671032+darrenvechain@users.noreply.github.com>
2024-02-06 08:50:16 +0000

    Update api/doc/api-docs/window-observer.js
    Co-authored-by: libotony <liboliqi@gmail.com>

--- api/doc/api-docs/window-observer.js
+++ api/doc/api-docs/window-observer.js
@@ -3,17 +3,12 @@
  * @type {MutationObserver}
  */
 const mutationObserver = new MutationObserver(() => {
-
-
     if (window.location.hash?.includes("#/paths/subscriptions")) {
-
         const element = document.querySelector('[data-testid="two-column-right"]');
-
         if (element) {
             element.remove();
         }
     }
-
 })
 
 mutationObserver.observe(document, {attributes: false, childList: true, characterData: false, subtree:true});
