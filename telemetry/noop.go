dfd4946c271166653cca704adf236543ff5c42e1
otherview <otherview@gmail.com>
2024-02-26 15:33:46 +0000

    lint

--- telemetry/noop.go
+++ telemetry/noop.go
@@ -7,10 +7,6 @@ type noopTelemetry struct{}
 
 func defaultNoopTelemetry() Telemetry { return &noopTelemetry{} }
 
-func (n *noopTelemetry) startServer() error { return nil }
-
-func (n *noopTelemetry) stopServer() error { return nil }
-
 func (n *noopTelemetry) GetOrCreateHistogramMeter(string, []int64) HistogramMeter { return &noopMetric }
 
 func (n *noopTelemetry) GetOrCreateCountMeter(string) CountMeter { return &noopMetric }
