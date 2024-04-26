ea5447dba42e9596b80b3e43bea82055d633f70f
otherview <otherview@gmail.com>
2024-04-26 10:26:28 +0100

    updating txt description

--- cmd/thor/flags.go
+++ cmd/thor/flags.go
@@ -152,6 +152,6 @@ var (
 		Name:   "allowed-peers",
 		Value:  nil,
 		Hidden: true,
-		Usage:  "Set the enode of only nodes that are accepted in p2p. Ex: enode://797fdd968592ca3b59a143f1aa2f152913499d4bb469f2bd5b62dfb1257707b4cb0686563fe144ee2088b1cc4f174bd72df51dbeb7ec1c5b6a8d8599c756f38b@107.150.112.22:55555",
+		Usage:  "comma separated list of node IDs that can be connected to. Ex: enode://797fdd968592ca3b59a143f1aa2f152913499d4bb469f2bd5b62dfb1257707b4cb0686563fe144ee2088b1cc4f174bd72df51dbeb7ec1c5b6a8d8599c756f38b@107.150.112.22:55555",
 	}
 )
