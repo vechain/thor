af7d9d4d688e182d46af7412caadcbcbeeff95df
libotony <liboliqi@gmail.com>
2024-05-14 10:50:37 +0800

    Update thor/bytes32.go
    Co-authored-by: Pedro Gomes <otherview@gmail.com>

--- thor/bytes32.go
+++ thor/bytes32.go
@@ -45,6 +45,7 @@ func (b Bytes32) IsZero() bool {
 
 // MarshalJSON implements json.Marshaler.
 func (b Bytes32) MarshalJSON() ([]byte, error) {
+	// Bytes32 are represented as a hexadecimal string.
 	return json.Marshal(b.String())
 }
 
