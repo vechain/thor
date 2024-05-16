ebb28cbeab91b84caccd6a41f99f35a76f5b5911
Pedro Gomes <otherview@gmail.com>
2024-05-16 10:10:21 +0100

    Update p2psrv/metrics.go
    Co-authored-by: libotony <liboliqi@gmail.com>

--- p2psrv/metrics.go
+++ p2psrv/metrics.go
@@ -10,5 +10,5 @@ import "github.com/vechain/thor/v2/telemetry"
 var (
 	metricConnectedPeers  = telemetry.LazyLoadGauge("p2p_connected_peers_count")
 	metricDiscoveredNodes = telemetry.LazyLoadGauge("p2p_discovered_node_count")
-	metricDialNewNode     = telemetry.LazyLoadCounter("p2p_dial_new_node_count")
+	metricDialNewNode     = telemetry.LazyLoadCounter("p2p_dialed_new_node_count")
 )
