1e3fe350b2633cb81d6a6100c452e8102bf35101
Darren Kelly <107671032+darrenvechain@users.noreply.github.com>
2024-07-11 13:48:20 +0100

    Added revision next for optimizing call/estimate related endpoints (#776)
    * fix: using the best block + 1 when estimating
    
    * fix: update comment
    
    * remove duplicate statement
    
    * fix: update thor.yaml and change pending to next
    
    * refactor: update query param name
    
    * fix: use 'next' instead of pending
    
    * chore: add unit tests
    
    * fix: use next only if explicity defined
    
    * fix: update unit tests
    
    * new way for parsing revision next (#37)
    
    * Update api/doc/thor.yaml
    
    Co-authored-by: libotony <liboliqi@gmail.com>
    
    * chore: update doc.yaml
    
    * Update api/accounts/accounts_test.go
    
    Co-authored-by: Darren Kelly <107671032+darrenvechain@users.noreply.github.com>
    
    * Update api/debug/debug_test.go
    
    * getsummary and getsummaryAndState decoupling (#786)
    
    * getsummary and getsummaryAndState decoupling
    
    * restart build
    
    * switch table tests to array
    
    * chore: update thor.yaml and update e2e commit sha
    
    * chore: update e2e commit sha
    
    ---------
    
    Co-authored-by: libotony <liboliqi@gmail.com>
    Co-authored-by: otherview <otherview@gmail.com>

--- api/utils/revisions.go
+++ api/utils/revisions.go
@@ -6,53 +6,54 @@
 package utils
 
 import (
+	"errors"
 	"math"
 	"strconv"
 
-	"github.com/pkg/errors"
 	"github.com/vechain/thor/v2/bft"
+	"github.com/vechain/thor/v2/block"
 	"github.com/vechain/thor/v2/chain"
+	"github.com/vechain/thor/v2/state"
 	"github.com/vechain/thor/v2/thor"
 )
 
-type Revision = interface{}
+const (
+	revBest      int64 = -1
+	revFinalized int64 = -2
+	revNext      int64 = -3
+)
 
-func GetSummary(revision Revision, repo *chain.Repository, bft bft.Finalizer) (s *chain.BlockSummary, err error) {
-	var id thor.Bytes32
-	switch revision := revision.(type) {
-	case thor.Bytes32:
-		id = revision
-	case uint32:
-		id, err = repo.NewBestChain().GetBlockID(revision)
-		if err != nil {
-			return
-		}
-	case string:
-		id = bft.Finalized()
-	default:
-		id = repo.BestBlockSummary().Header.ID()
-	}
-	summary, err := repo.GetBlockSummary(id)
-	if err != nil {
-		return nil, err
-	}
-	return summary, nil
+type Revision struct {
+	val interface{}
+}
+
+func (rev *Revision) IsNext() bool {
+	return rev.val == revNext
 }
 
 // ParseRevision parses a query parameter into a block number or block ID.
-func ParseRevision(revision string) (Revision, error) {
+func ParseRevision(revision string, allowNext bool) (*Revision, error) {
 	if revision == "" || revision == "best" {
-		return nil, nil
+		return &Revision{revBest}, nil
 	}
+
 	if revision == "finalized" {
-		return revision, nil
+		return &Revision{revFinalized}, nil
 	}
+
+	if revision == "next" {
+		if !allowNext {
+			return nil, errors.New("invalid revision: next is not allowed")
+		}
+		return &Revision{revNext}, nil
+	}
+
 	if len(revision) == 66 || len(revision) == 64 {
 		blockID, err := thor.ParseBytes32(revision)
 		if err != nil {
 			return nil, err
 		}
-		return blockID, nil
+		return &Revision{blockID}, nil
 	}
 	n, err := strconv.ParseUint(revision, 0, 0)
 	if err != nil {
@@ -61,5 +62,86 @@ func ParseRevision(revision string) (Revision, error) {
 	if n > math.MaxUint32 {
 		return nil, errors.New("block number out of max uint32")
 	}
-	return uint32(n), err
+	return &Revision{uint32(n)}, err
+}
+
+// GetSummary returns the block summary for the given revision,
+// revision required to be a deterministic block other than "next".
+func GetSummary(rev *Revision, repo *chain.Repository, bft bft.Finalizer) (sum *chain.BlockSummary, err error) {
+	var id thor.Bytes32
+	switch rev := rev.val.(type) {
+	case thor.Bytes32:
+		id = rev
+	case uint32:
+		id, err = repo.NewBestChain().GetBlockID(rev)
+		if err != nil {
+			return
+		}
+	case int64:
+		switch rev {
+		case revBest:
+			id = repo.BestBlockSummary().Header.ID()
+		case revFinalized:
+			id = bft.Finalized()
+		}
+	}
+	if id.IsZero() {
+		return nil, errors.New("invalid revision")
+	}
+	summary, err := repo.GetBlockSummary(id)
+	if err != nil {
+		return nil, err
+	}
+	return summary, nil
+}
+
+// GetSummaryAndState returns the block summary and state for the given revision,
+// this function supports the "next" revision.
+func GetSummaryAndState(rev *Revision, repo *chain.Repository, bft bft.Finalizer, stater *state.Stater) (*chain.BlockSummary, *state.State, error) {
+	if rev.IsNext() {
+		best := repo.BestBlockSummary()
+
+		// here we create a fake(no signature) "next" block header which reused most part of the parent block
+		// but set the timestamp and number to the next block. The following parameters will be used in the evm
+		// number, timestamp, total score, gas limit, beneficiary and "signer"
+		// since the fake block is not signed, the signer is the zero address, it is important that the subsequent
+		// call to header.Signer(), the error should be ignored.
+		builder := new(block.Builder).
+			ParentID(best.Header.ID()).
+			Timestamp(best.Header.Timestamp() + thor.BlockInterval).
+			TotalScore(best.Header.TotalScore()).
+			GasLimit(best.Header.GasLimit()).
+			GasUsed(best.Header.GasUsed()).
+			Beneficiary(best.Header.Beneficiary()).
+			StateRoot(best.Header.StateRoot()).
+			ReceiptsRoot(best.Header.ReceiptsRoot()).
+			TransactionFeatures(best.Header.TxsFeatures()).
+			Alpha(best.Header.Alpha())
+
+		// here we skipped the block's tx list thus header.txRoot will be an empty root
+		// since txRoot won't be supplied into the evm, it's safe to skip it.
+		if best.Header.COM() {
+			builder.COM()
+		}
+		mocked := builder.Build()
+
+		// state is also reused from the parent block
+		st := stater.NewState(best.Header.StateRoot(), best.Header.Number(), best.Conflicts, best.SteadyNum)
+
+		// rebuild the block summary with the next header (mocked) AND the best block status
+		return &chain.BlockSummary{
+			Header:    mocked.Header(),
+			Txs:       best.Txs,
+			Size:      uint64(mocked.Size()),
+			Conflicts: best.Conflicts,
+			SteadyNum: best.SteadyNum,
+		}, st, nil
+	}
+	sum, err := GetSummary(rev, repo, bft)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	st := stater.NewState(sum.Header.StateRoot(), sum.Header.Number(), sum.Conflicts, sum.SteadyNum)
+	return sum, st, nil
 }
