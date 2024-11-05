ae23b6d7a762afcc6efe8795e1cf0901a978da95
tony <liboliqi@gmail.com>
2024-11-05 18:55:58 +0800

    update named store space prefix

--- muxdb/muxdb.go
+++ muxdb/muxdb.go
@@ -25,7 +25,7 @@ import (
 const (
 	trieHistSpace    = byte(0) // the key space for historical trie nodes.
 	trieDedupedSpace = byte(1) // the key space for deduped trie nodes.
-	namedStoreSpace  = byte(3) // the key space for named store.
+	namedStoreSpace  = byte(2) // the key space for named store.
 )
 
 const (
