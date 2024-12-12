c5784013800c4a31b5c3a02b412e7b3f5459014e
paologalligit <paolo.galli@vechain.org>
2024-12-12 14:56:07 +0100

    fix: replace header type in baseFee calc

--- consensus/fork/galactica.go
+++ consensus/fork/galactica.go
@@ -12,7 +12,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/vechain/thor/v2/block"
 	"github.com/vechain/thor/v2/thor"
 	"github.com/vechain/thor/v2/vm"
 )
@@ -20,24 +20,25 @@ import (
 // VerifyGalacticaHeader verifies some header attributes which were changed in Galactica fork,
 // - gas limit check
 // - basefee check
-func VerifyGalacticaHeader(config *vm.ChainConfig, parent, header *types.Header) error {
+func VerifyGalacticaHeader(config *vm.ChainConfig, parent, header *block.Header) error {
 	// Verify that the gas limit remains within allowed bounds
-	parentGasLimit := parent.GasLimit
-	if !config.IsGalactica(parent.Number) {
-		parentGasLimit = parent.GasLimit * thor.ElasticityMultiplier
+	parentBlockNum := big.NewInt(int64(parent.Number()))
+	parentGasLimit := parent.GasLimit()
+	if !config.IsGalactica(parentBlockNum) {
+		parentGasLimit = parent.GasLimit() * thor.ElasticityMultiplier
 	}
-	if err := VerifyGaslimit(parentGasLimit, header.GasLimit); err != nil {
+	if err := VerifyGaslimit(parentGasLimit, header.GasLimit()); err != nil {
 		return err
 	}
 	// Verify the header is not malformed
-	if header.BaseFee == nil {
+	if header.BaseFee() == nil {
 		return fmt.Errorf("header is missing baseFee")
 	}
 	// Verify the baseFee is correct based on the parent header.
 	expectedBaseFee := CalcBaseFee(config, parent)
-	if header.BaseFee.Cmp(expectedBaseFee) != 0 {
+	if header.BaseFee().Cmp(expectedBaseFee) != 0 {
 		return fmt.Errorf("invalid baseFee: have %s, want %s, parentBaseFee %s, parentGasUsed %d",
-			expectedBaseFee, header.BaseFee, parent.BaseFee, parent.GasUsed)
+			expectedBaseFee, header.BaseFee(), parent.BaseFee(), parent.GasUsed())
 	}
 	return nil
 }
@@ -61,41 +62,45 @@ func VerifyGaslimit(parentGasLimit, headerGasLimit uint64) error {
 }
 
 // CalcBaseFee calculates the basefee of the header.
-func CalcBaseFee(config *vm.ChainConfig, parent *types.Header) *big.Int {
+func CalcBaseFee(config *vm.ChainConfig, parent *block.Header) *big.Int {
 	// If the current block is the first Galactica block, return the InitialBaseFee.
-	if !config.IsGalactica(parent.Number) {
+	parentBlockNum := big.NewInt(int64(parent.Number()))
+	if !config.IsGalactica(parentBlockNum) {
 		return new(big.Int).SetUint64(thor.InitialBaseFee)
 	}
 
 	var (
-		parentGasTarget          = parent.GasLimit / thor.ElasticityMultiplier
+		parentGasTarget          = parent.GasLimit() / thor.ElasticityMultiplier
 		parentGasTargetBig       = new(big.Int).SetUint64(parentGasTarget)
 		baseFeeChangeDenominator = new(big.Int).SetUint64(thor.BaseFeeChangeDenominator)
 	)
+	parentGasUsed := parent.GasUsed()
+	parentBaseFee := parent.BaseFee()
+
 	// If the parent gasUsed is the same as the target, the baseFee remains unchanged.
-	if parent.GasUsed == parentGasTarget {
-		return new(big.Int).Set(parent.BaseFee)
+	if parentGasUsed == parentGasTarget {
+		return new(big.Int).Set(parentBaseFee)
 	}
-	if parent.GasUsed > parentGasTarget {
+	if parentGasUsed > parentGasTarget {
 		// If the parent block used more gas than its target, the baseFee should increase.
-		gasUsedDelta := new(big.Int).SetUint64(parent.GasUsed - parentGasTarget)
-		x := new(big.Int).Mul(parent.BaseFee, gasUsedDelta)
+		gasUsedDelta := new(big.Int).SetUint64(parentGasUsed - parentGasTarget)
+		x := new(big.Int).Mul(parentBaseFee, gasUsedDelta)
 		y := x.Div(x, parentGasTargetBig)
 		baseFeeDelta := math.BigMax(
 			x.Div(y, baseFeeChangeDenominator),
 			common.Big1,
 		)
 
-		return x.Add(parent.BaseFee, baseFeeDelta)
+		return x.Add(parentBaseFee, baseFeeDelta)
 	} else {
 		// Otherwise if the parent block used less gas than its target, the baseFee should decrease.
-		gasUsedDelta := new(big.Int).SetUint64(parentGasTarget - parent.GasUsed)
-		x := new(big.Int).Mul(parent.BaseFee, gasUsedDelta)
+		gasUsedDelta := new(big.Int).SetUint64(parentGasTarget - parentGasUsed)
+		x := new(big.Int).Mul(parentBaseFee, gasUsedDelta)
 		y := x.Div(x, parentGasTargetBig)
 		baseFeeDelta := x.Div(y, baseFeeChangeDenominator)
 
 		return math.BigMax(
-			x.Sub(parent.BaseFee, baseFeeDelta),
+			x.Sub(parentBaseFee, baseFeeDelta),
 			common.Big0,
 		)
 	}
