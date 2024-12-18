aec763034e19ea26ce75d4ba69a0e918fed4d14a
paologalligit <paolo.galli@vechain.org>
2024-12-18 15:24:02 +0100

    test: add more dynFee txs to tests

--- block/block_test.go
+++ block/block_test.go
@@ -19,7 +19,7 @@ import (
 
 func TestBlock(t *testing.T) {
 	tx1 := new(tx.LegacyBuilder).Clause(tx.NewClause(&thor.Address{})).Clause(tx.NewClause(&thor.Address{})).Build()
-	tx2 := new(tx.LegacyBuilder).Clause(tx.NewClause(nil)).Build()
+	tx2 := new(tx.DynFeeBuilder).Clause(tx.NewClause(nil)).Build()
 
 	privKey := string("dce1443bd2ef0c2631adc1c67e5c93f13dc23a41c18b536effbbdcbcdb96fb65")
 
