0baeeb0bc5cb75f3b54e323cf3df095e85332f3a
paologalligit <paolo.galli@vechain.org>
2025-01-14 16:36:21 +0100

    test: add validate block tests

--- consensus/validator.go
+++ consensus/validator.go
@@ -237,8 +237,8 @@ func (c *Consensus) validateBlockBody(blk *block.Block) error {
 		return consensusError(fmt.Sprintf("block txs root mismatch: want %v, have %v", header.TxsRoot(), txs.RootHash()))
 	}
 
-	for _, tx := range txs {
-		origin, err := tx.Origin()
+	for _, tr := range txs {
+		origin, err := tr.Origin()
 		if err != nil {
 			return consensusError(fmt.Sprintf("tx signer unavailable: %v", err))
 		}
@@ -248,15 +248,17 @@ func (c *Consensus) validateBlockBody(blk *block.Block) error {
 		}
 
 		switch {
-		case tx.ChainTag() != c.repo.ChainTag():
-			return consensusError(fmt.Sprintf("tx chain tag mismatch: want %v, have %v", c.repo.ChainTag(), tx.ChainTag()))
-		case header.Number() < tx.BlockRef().Number():
-			return consensusError(fmt.Sprintf("tx ref future block: ref %v, current %v", tx.BlockRef().Number(), header.Number()))
-		case tx.IsExpired(header.Number()):
-			return consensusError(fmt.Sprintf("tx expired: ref %v, current %v, expiration %v", tx.BlockRef().Number(), header.Number(), tx.Expiration()))
+		case tr.ChainTag() != c.repo.ChainTag():
+			return consensusError(fmt.Sprintf("tx chain tag mismatch: want %v, have %v", c.repo.ChainTag(), tr.ChainTag()))
+		case header.Number() < tr.BlockRef().Number():
+			return consensusError(fmt.Sprintf("tx ref future block: ref %v, current %v", tr.BlockRef().Number(), header.Number()))
+		case tr.IsExpired(header.Number()):
+			return consensusError(fmt.Sprintf("tx expired: ref %v, current %v, expiration %v", tr.BlockRef().Number(), header.Number(), tr.Expiration()))
+		case header.Number() < c.forkConfig.GALACTICA && tr.Type() != tx.LegacyTxType:
+			return consensusError(tx.ErrTxTypeNotSupported.Error())
 		}
 
-		if err := tx.TestFeatures(header.TxsFeatures()); err != nil {
+		if err := tr.TestFeatures(header.TxsFeatures()); err != nil {
 			return consensusError("invalid tx: " + err.Error())
 		}
 	}
