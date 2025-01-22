a782aa4836bf37ec4232c6a96bdefb64e9a8c53f
Miguel Angel Rojo Fernandez <miguel.rojo@vechain.org>
2025-01-22 15:56:11 +0000

    first commit with a bit of a skeleton

--- api/api.go
+++ api/api.go
@@ -98,7 +98,7 @@ func New(
 	subs := subscriptions.New(repo, origins, config.BacktraceLimit, txPool, config.EnableDeprecated)
 	subs.Mount(router, "/subscriptions")
 
-	fees.New(repo).Mount(router, "/fees")
+	fees.New(repo, bft).Mount(router, "/fees")
 
 	if config.PprofOn {
 		router.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
--- api/fees/fees.go
+++ api/fees/fees.go
@@ -4,28 +4,47 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/pkg/errors"
 	"github.com/vechain/thor/v2/api/utils"
+	"github.com/vechain/thor/v2/bft"
 	"github.com/vechain/thor/v2/chain"
 )
 
 type Fees struct {
 	repo *chain.Repository
+	bft  bft.Committer
 }
 
-func New(repo *chain.Repository) *Fees {
+func New(repo *chain.Repository, bft bft.Committer) *Fees {
 	return &Fees{
 		repo,
+		bft,
 	}
 }
 
-func (f *Fees) handlePostFeesHistory(w http.ResponseWriter, req *http.Request) error {
-	revision, err := utils.ParseRevision(mux.Vars(req)["revision"], false)
+func (f *Fees) handleGetFeesHistory(w http.ResponseWriter, req *http.Request) error {
+	newestBlock, err := utils.ParseRevision(req.URL.Query().Get("newestBlock"), false)
+	if err != nil {
+		return utils.BadRequest(errors.WithMessage(err, "newestBlock"))
+	}
+	//TODO: get the array of fees from the newest block to the blockCount
+	summary, err := utils.GetSummary(newestBlock, f.repo, f.bft)
+	if err != nil {
+		if f.repo.IsNotFound(err) {
+			return utils.BadRequest(errors.WithMessage(err, "newestBlock"))
+		}
+		return err
+	}
+
+	baseFee := summary.Header.BaseFee()
+
+	return utils.WriteJSON(w, baseFee)
 }
 
 func (f *Fees) Mount(root *mux.Router, pathPrefix string) {
 	sub := root.PathPrefix(pathPrefix).Subrouter()
 	sub.Path("/history").
 		Methods(http.MethodGet).
-		Name("POST /fees/history").
-		HandlerFunc(utils.WrapHandlerFunc(f.handlePostFeesHistory))
+		Name("GET /fees/history").
+		HandlerFunc(utils.WrapHandlerFunc(f.handleGetFeesHistory))
 }
