8ad6c4ca06b105ff406c4cc88bf08cfa239e13fb
yeahnotsewerside <dreenot@gmail.com>
2025-01-22 15:56:09 +0200

    bump up codecov

---
+++ muxdb/backend_test.go
@@ -0,0 +1,191 @@
+package muxdb
+
+import (
+	"bytes"
+	"context"
+	"encoding/binary"
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vechain/thor/v2/kv"
+	"github.com/vechain/thor/v2/trie"
+)
+
+type mockStoreBackend struct {
+	kv.Store
+	deleteRangeCalled bool
+	startKey          []byte
+	limitKey          []byte
+}
+
+func (m *mockStoreBackend) DeleteRange(ctx context.Context, r kv.Range) error {
+	m.deleteRangeCalled = true
+	m.startKey = r.Start
+	m.limitKey = r.Limit
+	return nil
+}
+
+func TestAppendHistNodeKey(t *testing.T) {
+	b := backend{
+		HistPtnFactor:    1000,
+		DedupedPtnFactor: 1000,
+		CachedNodeTTL:    100,
+	}
+
+	tests := []struct {
+		name     string
+		trieName string
+		path     []byte
+		ver      trie.Version
+	}{
+		{
+			"empty path",
+			"test",
+			[]byte{},
+			trie.Version{Major: 1, Minor: 0},
+		},
+		{
+			"with path",
+			"test",
+			[]byte{1, 2, 3},
+			trie.Version{Major: 1000, Minor: 1},
+		},
+		{
+			"max uint32 factor",
+			"test",
+			[]byte{1},
+			trie.Version{Major: 1, Minor: 0},
+		},
+		{
+			"with minor version",
+			"test",
+			[]byte{1},
+			trie.Version{Major: 1, Minor: 5},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var buf []byte
+			result := b.AppendHistNodeKey(buf, tt.trieName, tt.path, tt.ver)
+
+			assert.Equal(t, byte(trieHistSpace), result[0])
+
+			if b.HistPtnFactor != math.MaxUint32 {
+				ptnBytes := result[1:5]
+				ptn := binary.BigEndian.Uint32(ptnBytes)
+				assert.Equal(t, tt.ver.Major/b.HistPtnFactor, ptn)
+			}
+		})
+	}
+}
+
+func TestAppendDedupedNodeKey(t *testing.T) {
+	b := backend{
+		HistPtnFactor:    1000,
+		DedupedPtnFactor: 1000,
+		CachedNodeTTL:    100,
+	}
+
+	tests := []struct {
+		name     string
+		trieName string
+		path     []byte
+		ver      trie.Version
+	}{
+		{
+			"empty path",
+			"test",
+			[]byte{},
+			trie.Version{Major: 1, Minor: 0},
+		},
+		{
+			"with path",
+			"test",
+			[]byte{1, 2, 3},
+			trie.Version{Major: 1000, Minor: 1},
+		},
+		{
+			"large version",
+			"test",
+			[]byte{1},
+			trie.Version{Major: 999999, Minor: 0},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var buf []byte
+			result := b.AppendDedupedNodeKey(buf, tt.trieName, tt.path, tt.ver)
+
+			assert.Equal(t, byte(trieDedupedSpace), result[0])
+
+			if b.DedupedPtnFactor != math.MaxUint32 {
+				ptnBytes := result[1:5]
+				ptn := binary.BigEndian.Uint32(ptnBytes)
+				assert.Equal(t, tt.ver.Major/b.DedupedPtnFactor, ptn)
+			}
+		})
+	}
+}
+
+func TestDeleteHistoryNodes(t *testing.T) {
+	b := backend{
+		HistPtnFactor: 1000,
+		Store:         &mockStoreBackend{},
+	}
+
+	mockStore := b.Store.(*mockStoreBackend)
+
+	err := b.DeleteHistoryNodes(context.Background(), 1000, 2000)
+	assert.Nil(t, err)
+	assert.True(t, mockStore.deleteRangeCalled)
+
+	expectedStart := binary.BigEndian.AppendUint32([]byte{trieHistSpace}, 1)
+	expectedLimit := binary.BigEndian.AppendUint32([]byte{trieHistSpace}, 2)
+	assert.True(t, bytes.Equal(expectedStart, mockStore.startKey))
+	assert.True(t, bytes.Equal(expectedLimit, mockStore.limitKey))
+}
+
+func TestAppendNodePath(t *testing.T) {
+	tests := []struct {
+		name string
+		path []byte
+	}{
+		{"empty path", []byte{}},
+		{"single byte", []byte{0x12}},
+		{"two bytes", []byte{0x12, 0x34}},
+		{"three bytes", []byte{0x12, 0x34, 0x56}},
+		{"four bytes", []byte{0x12, 0x34, 0x56, 0x78}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var buf []byte
+			result := appendNodePath(buf, tt.path)
+
+			if len(tt.path) == 0 {
+				assert.Equal(t, []byte{0, 0}, result)
+				return
+			}
+
+			if len(tt.path) == 1 {
+				assert.Equal(t, tt.path[0], result[0])
+				assert.Equal(t, byte(1), result[1])
+				return
+			}
+
+			assert.True(t, len(result) > 0)
+
+			if len(tt.path) >= 2 {
+				if len(tt.path) > 2 {
+					assert.Equal(t, tt.path[0]|0x10, result[0])
+				} else {
+					assert.Equal(t, tt.path[0], result[0])
+				}
+				assert.Equal(t, (tt.path[1]<<4)|2, result[1])
+			}
+		})
+	}
+}
--- muxdb/trie_test.go
+++ muxdb/trie_test.go
@@ -6,6 +6,7 @@
 package muxdb
 
 import (
+	"context"
 	"encoding/binary"
 	"testing"
 
@@ -13,7 +14,6 @@ import (
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/storage"
 	"github.com/vechain/thor/v2/muxdb/engine"
-	"github.com/vechain/thor/v2/thor"
 	"github.com/vechain/thor/v2/trie"
 )
 
@@ -33,25 +33,55 @@ func newTestBackend() *backend {
 	}
 }
 
-func TestTrie(t *testing.T) {
+func TestTrieBasicOperations(t *testing.T) {
 	var (
-		name  = "the trie"
+		name = "test-trie"
+		back = newTestBackend()
+	)
+
+	tr := newTrie(name, back, trie.Root{})
+
+	key := []byte("key1")
+	value := []byte("value1")
+	meta := []byte("meta1")
+
+	err := tr.Update(key, value, meta)
+	assert.Nil(t, err)
+
+	gotValue, gotMeta, err := tr.Get(key)
+	assert.Nil(t, err)
+	assert.Equal(t, value, gotValue)
+	assert.Equal(t, meta, gotMeta)
+
+	err = tr.Update(key, nil, nil)
+	assert.Nil(t, err)
+
+	gotValue, gotMeta, err = tr.Get(key)
+	assert.Nil(t, err)
+	assert.Nil(t, gotValue)
+	assert.Nil(t, gotMeta)
+}
+
+func TestTrieVersioning(t *testing.T) {
+	var (
+		name  = "versioned-trie"
 		back  = newTestBackend()
-		round = uint32(200)
 		roots []trie.Root
 	)
 
-	for i := uint32(0); i < round; i++ {
+	for i := uint32(0); i < 5; i++ {
 		var root trie.Root
 		if len(roots) > 0 {
 			root = roots[len(roots)-1]
 		}
 
 		tr := newTrie(name, back, root)
-		key := thor.Blake2b(binary.BigEndian.AppendUint32(nil, i)).Bytes()
-		val := thor.Blake2b(key).Bytes()
-		meta := thor.Blake2b(val).Bytes()
-		err := tr.Update(key, val, meta)
+
+		key := make([]byte, 4)
+		binary.BigEndian.PutUint32(key, i)
+		value := []byte("value-" + string(key))
+
+		err := tr.Update(key, value, nil)
 		assert.Nil(t, err)
 
 		err = tr.Commit(trie.Version{Major: i}, false)
@@ -63,16 +93,151 @@ func TestTrie(t *testing.T) {
 		})
 	}
 
-	for _i, root := range roots {
+	for i, root := range roots {
 		tr := newTrie(name, back, root)
-		for i := uint32(0); i <= uint32(_i); i++ {
-			key := thor.Blake2b(binary.BigEndian.AppendUint32(nil, i)).Bytes()
-			val := thor.Blake2b(key).Bytes()
-			meta := thor.Blake2b(val).Bytes()
-			_val, _meta, err := tr.Get(key)
-			assert.Nil(t, err)
-			assert.Equal(t, val, _val)
-			assert.Equal(t, meta, _meta)
+		key := make([]byte, 4)
+		binary.BigEndian.PutUint32(key, uint32(i))
+		value := []byte("value-" + string(key))
+
+		gotValue, _, err := tr.Get(key)
+		assert.Nil(t, err)
+		assert.Equal(t, value, gotValue)
+	}
+}
+
+func TestTrieCaching(t *testing.T) {
+	var (
+		name = "cached-trie"
+		back = newTestBackend()
+	)
+
+	tr := newTrie(name, back, trie.Root{})
+
+	key := []byte("cached-key")
+	value := []byte("cached-value")
+
+	err := tr.Update(key, value, nil)
+	assert.Nil(t, err)
+
+	tr.SetNoFillCache(true)
+	key2 := []byte("uncached-key")
+	value2 := []byte("uncached-value")
+
+	err = tr.Update(key2, value2, nil)
+	assert.Nil(t, err)
+}
+
+func TestTrieCheckpoint(t *testing.T) {
+	var (
+		name = "checkpoint-trie"
+		back = newTestBackend()
+	)
+
+	tr := newTrie(name, back, trie.Root{})
+
+	for i := uint32(0); i < 3; i++ {
+		key := make([]byte, 4)
+		binary.BigEndian.PutUint32(key, i)
+		value := []byte("checkpoint-" + string(key))
+
+		err := tr.Update(key, value, nil)
+		assert.Nil(t, err)
+
+		err = tr.Commit(trie.Version{Major: i}, false)
+		assert.Nil(t, err)
+	}
+
+	leafCount := 0
+	leafHandler := func(leaf *trie.Leaf) {
+		leafCount++
+	}
+
+	err := tr.Checkpoint(context.Background(), 0, leafHandler)
+	assert.Nil(t, err)
+	assert.Greater(t, leafCount, 0)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	for i := uint32(0); i < 10000; i++ {
+		key := make([]byte, 4)
+		binary.BigEndian.PutUint32(key, i)
+		value := []byte("checkpoint-extra-" + string(key))
+
+		err := tr.Update(key, value, nil)
+		assert.Nil(t, err)
+	}
+	err = tr.Commit(trie.Version{Major: 10}, false)
+	assert.Nil(t, err)
+
+	err = tr.Checkpoint(ctx, 0, nil)
+	assert.Equal(t, context.Canceled, err)
+}
+
+func TestTrieNodeIterator(t *testing.T) {
+	var (
+		name = "iterator-trie"
+		back = newTestBackend()
+	)
+
+	tr := newTrie(name, back, trie.Root{})
+
+	for i := uint32(0); i < 5; i++ {
+		key := make([]byte, 4)
+		binary.BigEndian.PutUint32(key, i)
+		value := []byte("iter-" + string(key))
+
+		err := tr.Update(key, value, nil)
+		assert.Nil(t, err)
+	}
+
+	iter := tr.NodeIterator(nil, 0)
+	nodeCount := 0
+	for iter.Next(true) {
+		nodeCount++
+	}
+	assert.Greater(t, nodeCount, 0)
+	assert.Nil(t, iter.Error())
+}
+
+func TestTrieCopy(t *testing.T) {
+	var (
+		name = "copy-trie"
+		back = newTestBackend()
+	)
+
+	tr := newTrie(name, back, trie.Root{})
+
+	key := []byte("original")
+	value := []byte("value")
+	err := tr.Update(key, value, nil)
+	assert.Nil(t, err)
+
+	trCopy := tr.Copy()
+
+	gotValue, _, err := trCopy.Get(key)
+	assert.Nil(t, err)
+	assert.Equal(t, value, gotValue)
+
+	err = tr.Update(key, []byte("modified"), nil)
+	assert.Nil(t, err)
+
+	gotValue, _, err = trCopy.Get(key)
+	assert.Nil(t, err)
+	assert.Equal(t, value, gotValue)
+}
+
+func TestContextChecker(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	checker := newContextChecker(ctx, 1) // Set small debounce for faster testing
+	cancel()
+
+	for i := 0; i < 10; i++ {
+		err := checker()
+		if err != nil {
+			assert.Equal(t, context.Canceled, err)
+			return
 		}
 	}
+	t.Error("Expected context canceled error")
 }
