c33aceb5d6cacfe2ee31f8f49b9152af7bf1a1a4
Miguel Angel Rojo Fernandez <miguel.rojo@vechain.org>
2025-01-22 17:53:33 +0000

    first commit with a bit of a skeleton

--- api/fees/fees.go
+++ api/fees/fees.go
@@ -4,6 +4,7 @@ import (
 	"math/big"
 	"net/http"
 	"strconv"
+	"sync/atomic"
 
 	"github.com/gorilla/mux"
 	"github.com/pkg/errors"
@@ -24,35 +25,75 @@ func New(repo *chain.Repository, bft bft.Committer) *Fees {
 	}
 }
 
-func (f *Fees) handleGetFeesHistory(w http.ResponseWriter, req *http.Request) error {
+func validateGetFeesHistoryParams(w http.ResponseWriter, req *http.Request) (uint32, *utils.Revision, error) {
 	blockCountParam := req.URL.Query().Get("blockCount")
-	blockCount, err := strconv.Atoi(blockCountParam)
+	blockCount, err := strconv.ParseUint(blockCountParam, 10, 32)
 	if err != nil {
-		return utils.BadRequest(errors.WithMessage(err, "invalid blockCount, it should represent an integer"))
+		return 0, nil, utils.BadRequest(errors.WithMessage(err, "invalid blockCount, it should represent an integer"))
 	}
-	if blockCount < 1 || blockCount > 1024 {
-		return utils.BadRequest(errors.New("blockCount must be between 1 and 1024"))
+	if blockCount < 1 || blockCount > maxNumberOfBlocks {
+		return 0, nil, utils.BadRequest(errors.New("blockCount must be between 1 and 1024"))
 	}
-	newestBlock, err := utils.ParseRevision(req.URL.Query().Get("newestBlock"), false)
+	newestBlockRevision, err := utils.ParseRevision(req.URL.Query().Get("newestBlock"), false)
 	if err != nil {
-		return utils.BadRequest(errors.WithMessage(err, "newestBlock"))
+		return 0, nil, utils.BadRequest(errors.WithMessage(err, "newestBlock"))
 	}
-	//TODO: get the array of fees from the newest block to the blockCount
-	summary, err := utils.GetSummary(newestBlock, f.repo, f.bft)
+
+	return uint32(blockCount), newestBlockRevision, nil
+}
+
+func (f *Fees) handleGetFeesHistory(w http.ResponseWriter, req *http.Request) error {
+	blockCount, newestBlockRevision, error := validateGetFeesHistoryParams(w, req)
+	if error != nil {
+		return error
+	}
+	newestBlockSummary, err := utils.GetSummary(newestBlockRevision, f.repo, f.bft)
 	if err != nil {
 		if f.repo.IsNotFound(err) {
 			return utils.BadRequest(errors.WithMessage(err, "newestBlock"))
 		}
 		return err
 	}
+	lastBlock := newestBlockSummary.Header.Number()
+	oldestBlock := lastBlock + 1 - blockCount
+	var next atomic.Uint32
+	next.Store(oldestBlock)
 
-	baseFees := make([]*big.Int, blockCount)
-	baseFees[0] = summary.Header.BaseFee()
+	blockDataChan := make(chan *blockData, blockCount)
+
+	for i := 0; i < maxBlockFetchers && i < int(blockCount); i++ {
+		go func() {
+			for {
+				blockNumber := next.Add(1) - 1
+				if blockNumber > lastBlock {
+					return
+				}
+				blockFee := &blockData{}
+				blockFee.blockRevision, blockFee.err = utils.ParseRevision(strconv.FormatUint(uint64(blockNumber), 10), false)
+				if blockFee.err != nil {
+					blockFee.blockSummary, blockFee.err = utils.GetSummary(blockFee.blockRevision, f.repo, f.bft)
+				}
+				blockDataChan <- blockFee
+			}
+		}()
+	}
+
+	var (
+		baseFees      = make([]*big.Int, blockCount)
+		gasUsedRatios = make([]float64, blockCount)
+	)
+
+	// Collect results from the channel
+	for i := 0; i < int(blockCount); i++ {
+		blockData := <-blockDataChan
+		baseFees[i] = blockData.blockSummary.Header.BaseFee()
+		gasUsedRatios[i] = float64(blockData.blockSummary.Header.GasUsed()) / float64(blockData.blockSummary.Header.GasLimit())
+	}
 
 	return utils.WriteJSON(w, &GetFeesHistory{
-		OldestBlock:  nil,
-		BaseFees:     baseFees,
-		GasUsedRatio: nil,
+		OldestBlock:   new(big.Int).SetUint64(uint64(oldestBlock)),
+		BaseFees:      baseFees,
+		GasUsedRatios: gasUsedRatios,
 	})
 }
 
