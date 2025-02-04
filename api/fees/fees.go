801e1fead747815cacf2c3b6bd74a0de41a301cf
Miguel Angel Rojo Fernandez <miguel.rojo@vechain.org>
2025-02-04 19:05:18 +0000

    using data after code review comments (tests fixed)

--- api/fees/fees.go
+++ api/fees/fees.go
@@ -54,7 +54,6 @@ func (f *Fees) validateGetFeesHistoryParams(req *http.Request) (uint32, *chain.B
 	}
 
 	//newestBlock validation
-
 	newestBlock, err := utils.ParseRevision(req.URL.Query().Get("newestBlock"), false)
 	if err != nil {
 		return 0, nil, nil, utils.BadRequest(errors.WithMessage(err, "newestBlock"))
