de0990d7d968e22bda6495a26af59f31785ece63
Paolo Galli <paolo.galli@vechain.org>
2025-03-14 17:59:36 +0100

    Add more baseFee and validation tests (#1010)
    * test: add more baseFee and validation tests

--- consensus/consensus_test.go
+++ consensus/consensus_test.go
@@ -461,6 +461,35 @@ func TestValidateBlockHeader(t *testing.T) {
 	}
 }
 
+func TestValidateBlockHeaderWithBadBaseFee(t *testing.T) {
+	forkConfig := testchain.DefaultForkConfig
+	forkConfig.GALACTICA = 1
+	forkConfig.VIP214 = 2
+
+	chain, err := testchain.NewWithFork(forkConfig)
+	assert.NoError(t, err)
+
+	con := New(chain.Repo(), chain.Stater(), forkConfig)
+
+	best, err := chain.BestBlock()
+	assert.NoError(t, err)
+
+	var sig [65]byte
+	rand.Read(sig[:])
+	newBlock := new(block.Builder).
+		ParentID(best.Header().ID()).
+		Timestamp(best.Header().Timestamp() + thor.BlockInterval).
+		TotalScore(best.Header().TotalScore() + 1).
+		BaseFee(big.NewInt(thor.InitialBaseFee * 123)).
+		TransactionFeatures(1).
+		GasLimit(thor.InitialGasLimit).
+		Build().
+		WithSignature(sig[:])
+
+	_, _, err = con.Process(con.repo.BestBlockSummary(), newBlock, newBlock.Header().Timestamp(), 0)
+	assert.Contains(t, err.Error(), "block header invalid: invalid baseFee: have 10000000000000, want 1230000000000000")
+}
+
 func TestVerifyBlock(t *testing.T) {
 	tc, err := newTestConsensus()
 	if err != nil {
