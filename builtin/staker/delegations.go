25be2ce7ee5e803eb4e74aff9ef2a8031cfb7742
Darren Kelly <darren.kelly@vechain.org>
2025-05-30 19:04:17 +0100

    chore: remove logs

--- builtin/staker/delegations.go
+++ builtin/staker/delegations.go
@@ -6,7 +6,6 @@
 package staker
 
 import (
-	"log/slog"
 	"math/big"
 
 	"github.com/pkg/errors"
@@ -133,12 +132,6 @@ func (d *delegations) DisableAutoRenew(delegationID thor.Bytes32) error {
 
 	weight := delegation.Weight()
 
-	slog.Info("Disabling auto-renew for delegation",
-		"pending-cooldown-vet", aggregation.PendingCooldownVET,
-		"pending-cooldown-weight", aggregation.PendingCooldownWeight,
-		"pending-locked-vet", aggregation.PendingLockedVET,
-		"pending-locked-weight", aggregation.PendingLockedWeight)
-
 	// the delegation's funds have already been locked, so we need to move them to cooldown
 	if delegation.IsLocked(validator) {
 		// move the delegation's portion of locked to cooldown.
@@ -163,12 +156,6 @@ func (d *delegations) DisableAutoRenew(delegationID thor.Bytes32) error {
 	lastIteration := validator.CurrentIteration() + 1
 	delegation.LastIteration = &lastIteration
 	delegation.AutoRenew = false
-
-	slog.Info("Disabling auto-renew for delegation",
-		"pending-cooldown-vet", aggregation.PendingCooldownVET,
-		"pending-cooldown-weight", aggregation.PendingCooldownWeight,
-		"pending-locked-vet", aggregation.PendingLockedVET,
-		"pending-locked-weight", aggregation.PendingLockedWeight)
 
 	if err := d.storage.SetDelegation(delegationID, delegation); err != nil {
 		return err
