ab303eca1612476a6d15d71217ab642b37e356b4
Darren Kelly <darren.kelly@vechain.org>
2025-07-14 09:40:37 +0100

    chore: add license header

--- thorclient/wsclient/types.go
+++ thorclient/wsclient/types.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2025 The VeChainThor developers
+//
+// Distributed under the GNU Lesser General Public License v3.0 software license, see the accompanying
+// file LICENSE or <https://www.gnu.org/licenses/lgpl-3.0.html>
+
 package wsclient
 
 // EventWrapper is used to return errors from the websocket alongside the data
