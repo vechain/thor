c019c18622ed4d9b521d8177f1c5fd55a2241bcd
libotony <tony.li@vechain.org>
2025-07-21 18:10:04 +0800

    Update builtin/gen/staker.sol
    Co-authored-by: Pedro Gomes <otherview@gmail.com>

--- builtin/gen/staker.sol
+++ builtin/gen/staker.sol
@@ -289,7 +289,7 @@ contract Staker {
     }
 
     /**
-     * @dev getWithdraw returns the amount of a validator's withdrawal.
+     * @dev getWithdrawable returns the amount of a validator's withdrawal.
      */
     function getWithdrawable(bytes32 id) public view returns (uint256) {
         (uint256 withdrawal, string memory error) = StakerNative(address(this))
