30b64c6c8c14f2ddd4db599d5d775249e7fcd7f1
paologalligit <paolo.galli@vechain.org>
2025-07-22 15:12:53 +0200

    reset the RewardRatio to 30% when Hayabusa activates

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -145,6 +145,9 @@ func New(
 		if err := builtin.Params.Native(state).Set(thor.KeyCurveFactor, thor.CurveFactor); err != nil {
 			panic(err)
 		}
+		if err := builtin.Params.Native(state).Set(thor.KeyRewardRatio, thor.InitialRewardRatio); err != nil {
+			panic(err)
+		}
 	}
 
 	rt := Runtime{
