82da05328703c6044aee4c50bf5019af50f8bb30
Darren Kelly <darren.kelly@vechain.org>
2025-08-01 12:41:24 +0100

    chore: fix lint issues

--- cmd/thor/solo/core.go
+++ cmd/thor/solo/core.go
@@ -71,7 +71,7 @@ func (c *Core) Pack(pendingTxs tx.Transactions, onDemand bool) ([]*tx.Transactio
 
 	// If on-demand and now equals the best timestamp, this will create blocks with future timestamps
 	// Otherwise, new blocks have the same timestamp as the best block
-	if  c.options.OnDemand {
+	if c.options.OnDemand {
 		now = best.Header.Timestamp() + c.options.BlockInterval
 	}
 
