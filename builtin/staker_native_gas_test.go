7d156184216df9b72aae973d7a91a6506773b52c
libotony <tony.li@vechain.org>
2025-09-01 19:14:55 +0800

    update gas usage of raw (#1303)

--- builtin/staker_native_gas_test.go
+++ builtin/staker_native_gas_test.go
@@ -108,7 +108,7 @@ func TestStakerNativeGasCosts(t *testing.T) {
 		},
 		{
 			function:    "native_addValidation",
-			expectedGas: 61200,
+			expectedGas: 76200,
 			args:        []any{account1, account1, thor.LowStakingPeriod(), staker.MinStake},
 			description: "Add a new validator (not implemented yet)",
 		},
