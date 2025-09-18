031281257564f3ae70d7e049e20ba3c6d35d9979
Darren Kelly <darren.kelly@vechain.org>
2025-09-18 07:50:41 +0100

    chore: add bandwidth metrics

--- cmd/thor/bandwidth/metrics.go
+++ cmd/thor/bandwidth/metrics.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2025 The VeChainThor developers
+//
+// Distributed under the GNU Lesser General Public License v3.0 software license, see the accompanying
+// file LICENSE or <https://www.gnu.org/licenses/lgpl-3.0.html>
+
 package bandwidth
 
 import "github.com/vechain/thor/v2/metrics"
