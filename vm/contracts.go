68a6b09faed9c7c797714053aa19654625647c37
Paolo Galli <paolo.galli@vechain.org>
2025-11-04 10:10:15 +0100

    Update vm/contracts.go
    Co-authored-by: libotony <tony.li@vechain.org>

--- vm/contracts.go
+++ vm/contracts.go
@@ -560,6 +560,7 @@ var (
 )
 
 // #nosec G602
+// false alert from gosec as input length is checked in L565
 func (c *blake2F) Run(input []byte) ([]byte, error) {
 	// Make sure the input is valid (correct length and final flag)
 	if len(input) != blake2FInputLength {
