33ad208f3ff1b01c389c1fe9021c2d9c083f873c
vanjatomic <vanja.tomic@vechain.org>
2025-11-19 17:06:23 +0000

    Enhance logger

--- api/middleware/request_logger.go
+++ api/middleware/request_logger.go
@@ -16,10 +16,11 @@ import (
 )
 
 // RequestLoggerMiddleware returns a middleware to ensure requests are syphoned into the writer
-func RequestLoggerMiddleware(logger log.Logger, enabled *atomic.Bool) func(http.Handler) http.Handler {
+func RequestLoggerMiddleware(logger log.Logger, enabled *atomic.Bool, slowQueriesThreshold time.Duration) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if !enabled.Load() {
+			// If api logging is not enabled and slow queries threshold is set to 0, api logs won't be recorded
+			if !enabled.Load() && slowQueriesThreshold == time.Duration(0) {
 				next.ServeHTTP(w, r)
 				return
 			}
@@ -36,15 +37,20 @@ func RequestLoggerMiddleware(logger log.Logger, enabled *atomic.Bool) func(http.
 				r.Body = io.NopCloser(io.Reader(bytes.NewReader(bodyBytes)))
 			}
 
-			logger.Info("API Request",
-				"timestamp", time.Now().Unix(),
-				"URI", r.URL.String(),
-				"Method", r.Method,
-				"Body", string(bodyBytes),
-			)
-
+			start := time.Now()
 			// call the original http.Handler we're wrapping
 			next.ServeHTTP(w, r)
+
+			duration := time.Since(start)
+			if enabled.Load() || duration > slowQueriesThreshold {
+				logger.Info("API Request",
+					"DurationMs", duration.Milliseconds(),
+					"Timestamp", time.Now().Unix(),
+					"URI", r.URL.String(),
+					"Method", r.Method,
+					"Body", string(bodyBytes),
+				)
+			}
 		})
 	}
 }
