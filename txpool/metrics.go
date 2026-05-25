0ea89ae6aae96c43270a87523d6c74314aeb115a
tony <liboliqi@gmail.com>
2026-05-25 16:13:49 +0800

    txpool: add gauge for total pool size after wash

--- txpool/metrics.go
+++ txpool/metrics.go
@@ -13,5 +13,6 @@ var (
 	metricTxPoolGauge            = metrics.LazyLoadGaugeVec("txpool_current_tx_count", []string{"source", "type"})
 	metricBadTxGauge             = metrics.LazyLoadGaugeVec("bad_tx_count", []string{"source"})
 	metricTxPoolExecutablesGauge = metrics.LazyLoadGauge("txpool_executable_tx_count")
+	metricTxPoolAllGauge         = metrics.LazyLoadGauge("txpool_all_tx_count")
 	metricAccountQuotaExceeded   = metrics.LazyLoadCounterVec("account_quota_exceeded", []string{"type"})
 )
